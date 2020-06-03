0146f54a022a5b821baf1b9d418317b038a20db1
sevenhhe <sevenhhe@goodow.com>
2020-06-03 14:11:48 +0800

    智能配网逻辑优化 1、剔除不必要的日志打印 2、检测 wifi 频率，非 2.4GHz 快速失败
    Change-Id: Ieb34ad423cc734529d2a6c2e40a50d333fc2cee8

---
+++ tenextllapp/app/src/main/java/com/kitlink/util/WifiUtil.java
@@ -0,0 +1,32 @@
+package com.kitlink.util;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+
+public class WifiUtil {
+
+    private static int getFrequency(Context context) {
+        WifiManager wifi = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        return wifi.getConnectionInfo().getFrequency();
+    }
+
+    /**
+     * 判断是否是 2.4GHz 的 wifi
+     * @param context 上下文
+     * @return true：wifi 是 2.4GHz， false：wifi 不是 2.4GHz
+     */
+    public static boolean is24GHz(Context context) {
+        int freq = getFrequency(context);
+        return freq > 2400 && freq < 2500;
+    }
+
+    /**
+     * 判断是否是 5GHz 的 wifi
+     * @param context 上下文
+     * @return true：wifi 是 5GHz， false：wifi 不是 5GHz
+     */
+    public static boolean is5GHz(Context context) {
+        int freq = getFrequency(context);
+        return freq > 4900 && freq < 5900;
+    }
+}
