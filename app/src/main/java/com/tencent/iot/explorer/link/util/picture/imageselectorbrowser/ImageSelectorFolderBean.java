19624123c0bdb69f184d97e73c475a363f7890a0
archurtan <archurspace@gmail.com>
2020-06-16 19:52:55 +0800

    将picture模块移到app的util下
    Change-Id: I7cb54af114e427da4a2f20b42e3d38e2861cf429

---
+++ app/src/main/java/com/tencent/iot/explorer/link/util/picture/imageselectorbrowser/ImageSelectorFolderBean.java
@@ -0,0 +1,62 @@
+package com.tencent.iot.explorer.link.util.picture.imageselectorbrowser;
+
+import java.util.List;
+
+
+/**  
+ * ClassName: ImageSelectorFolderBean   
+ * Description: 单个目录的实体类    
+ * @author wuqionghai  
+ * @version 1.0  2015-9-17
+ */
+public class ImageSelectorFolderBean {
+
+	private String mFileDirectoryName;
+    private String mFristImagePath;
+    private ImageSelectorImageBean mImageSelectorImageBean;
+    private List<ImageSelectorImageBean> mImages;
+
+    public String getFileDirectoryName() {
+		return mFileDirectoryName;
+	}
+
+	public void setFileDirectoryName(String mFileDirectoryName) {
+		this.mFileDirectoryName = mFileDirectoryName;
+	}
+
+	public String getFristImagePath() {
+		return mFristImagePath;
+	}
+
+	public void setFristImagePath(String mFristImagePath) {
+		this.mFristImagePath = mFristImagePath;
+	}
+
+	public ImageSelectorImageBean getImageSelectorImageBean() {
+		return mImageSelectorImageBean;
+	}
+
+	public void setImageSelectorImageBean(
+			ImageSelectorImageBean mImageSelectorImageBean) {
+		this.mImageSelectorImageBean = mImageSelectorImageBean;
+	}
+
+	public List<ImageSelectorImageBean> getImages() {
+		return mImages;
+	}
+	
+	public void setImages(List<ImageSelectorImageBean> mImages) {
+		this.mImages = mImages;
+	}
+	
+    @Override
+    public boolean equals(Object o) {
+        try {
+            ImageSelectorFolderBean other = (ImageSelectorFolderBean) o;
+            return this.mFristImagePath.equalsIgnoreCase(other.mFristImagePath);
+        }catch (ClassCastException e){
+            e.printStackTrace();
+        }
+        return super.equals(o);
+    }
+}
