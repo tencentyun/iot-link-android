d1145535befabbe37f834e8ffbb9a7442a81ad61
archurtan <archurspace@gmail.com>
2020-06-19 10:42:49 +0800

    将view-release.aar替换成源码
    Change-Id: I4ae542803913e26e9e901d25f8f5b8953d3eb698

---
+++ app/src/main/java/com/tencent/iot/explorer/link/customview/home/BottomItemEntity.java
@@ -0,0 +1,28 @@
+package com.view.bottom;
+
+import android.support.annotation.ColorInt;
+import android.support.annotation.DrawableRes;
+
+/**
+ * 底部菜单实体类
+ * <p>
+ * Created by lurensheng on 2018/4/25 0025.
+ */
+
+public class BottomItemEntity {
+
+    protected int normalColor;
+    protected int hoverColor;
+    protected String title;
+    protected int normalSrc;
+    protected int hoverSrc;
+
+    public BottomItemEntity(String title, @ColorInt int normalColor, @ColorInt int hoverColor,
+                            @DrawableRes int normalSrc, @DrawableRes int hoverSrc) {
+        this.normalColor = normalColor;
+        this.hoverColor = hoverColor;
+        this.title = title;
+        this.normalSrc = normalSrc;
+        this.hoverSrc = hoverSrc;
+    }
+}
