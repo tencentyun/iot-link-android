7ff0ee20509e71b54f7f113ddf56cffa1d962566
sevenhhe <sevenhhe@goodow.com>
2020-06-29 20:26:40 +0800

    当使用过期的 token 发起请求时，检测到服务器返回 “token 无效”，跳转至登陆界面
    Change-Id: I46c391c112f69115a966663c64a852f6d7ef7c32

---
+++ app/src/main/java/com/tencent/iot/explorer/link/ErrorCode.java
@@ -0,0 +1,10 @@
+package com.tencent.iot.explorer.link;
+
+public class ErrorCode {
+    public final static int REQ_ERROR_CODE = -1000;
+
+
+    public static class DATA_MSG {
+        public final static String ACCESS_TOKEN_ERR = "InvalidParameterValue.InvalidAccessToken";
+    }
+}
