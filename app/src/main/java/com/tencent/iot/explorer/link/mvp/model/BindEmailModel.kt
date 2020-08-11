43dce44f4ba37646e74281716ab305f2bdff1b03
archurtan <archurspace@gmail.com>
2020-08-11 20:04:33 +0800

    修复产品提出的连连体验问题
    Change-Id: Icf1d292c5544faf21bcacbede81c4ef9c15ae76e

--- app/src/main/java/com/tencent/iot/explorer/link/mvp/model/BindEmailModel.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/mvp/model/BindEmailModel.kt
@@ -1,11 +1,14 @@
 package com.tencent.iot.explorer.link.mvp.model
 
+import com.tencent.iot.explorer.link.ErrorMessage
 import com.tencent.iot.explorer.link.kitlink.consts.SocketConstants
 import com.tencent.iot.explorer.link.kitlink.response.BaseResponse
 import com.tencent.iot.explorer.link.kitlink.util.HttpRequest
 import com.tencent.iot.explorer.link.kitlink.util.MyCallback
+import com.tencent.iot.explorer.link.kitlink.util.RequestCode
 import com.tencent.iot.explorer.link.mvp.ParentModel
 import com.tencent.iot.explorer.link.mvp.view.BindEmailView
+import com.tencent.iot.explorer.link.util.T
 
 class BindEmailModel(view: BindEmailView) : ParentModel<BindEmailView>(view), MyCallback {
 
@@ -22,11 +25,27 @@ class BindEmailModel(view: BindEmailView) : ParentModel<BindEmailView>(view), My
     }
 
     override fun fail(msg: String?, reqCode: Int) {
-        TODO("Not yet implemented")
+        T.show(msg)
     }
 
     override fun success(response: BaseResponse, reqCode: Int) {
-        if (response.isSuccess()) {
+        when (reqCode) {
+            RequestCode.send_email_code -> {// 发送邮箱验证码
+                if (response.isSuccess()) {
+                    view?.sendVerifyCodeSuccess()
+                } else {
+                    val errMsg = ErrorMessage.parseErrorMessage(response.data.toString())
+                    view?.sendVerifyCodeFail(errMsg)
+                }
+            }
+            RequestCode.update_user_info -> {// 绑定邮箱响应
+                if (response.isSuccess()) {
+                    view?.bindSuccess()
+                } else {
+                    val errMsg = ErrorMessage.parseErrorMessage(response.data.toString())
+                    view?.bindFail(errMsg)
+                }
+            }
         }
     }
 }
