37354a7c412afb5821df347dc19a67d53b1cb359
archurtan <archurspace@gmail.com>
2020-08-11 12:20:59 +0800

    账号绑定逻辑完善
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192858966531
    
    Change-Id: Ic7b481a60cc4dfffdd95d91520349f4cf6cc2784

--- app/src/main/java/com/tencent/iot/explorer/link/mvp/view/ModifyEmailView.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/mvp/view/ModifyEmailView.kt
@@ -1,6 +1,11 @@
 package com.tencent.iot.explorer.link.mvp.view
 
+import com.tencent.iot.explorer.link.ErrorMessage
 import com.tencent.iot.explorer.link.mvp.ParentView
 
 interface ModifyEmailView : ParentView {
+    fun sendVerifyCodeFail(msg: ErrorMessage)
+    fun sendVerifyCodeSuccess()
+    fun updateEmailFail(msg: ErrorMessage)
+    fun updateEmailSuccess()
 }
