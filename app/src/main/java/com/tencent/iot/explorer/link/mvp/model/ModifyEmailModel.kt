9b341d9c7ff0a6a6470aa1281bca04ce9a1a2b49
archurtan <archurspace@gmail.com>
2020-08-11 12:20:59 +0800

    账号绑定逻辑完善
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192858966531
    
    Change-Id: Ic7b481a60cc4dfffdd95d91520349f4cf6cc2784

--- app/src/main/java/com/tencent/iot/explorer/link/mvp/model/ModifyEmailModel.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/mvp/model/ModifyEmailModel.kt
@@ -1,9 +1,11 @@
 package com.tencent.iot.explorer.link.mvp.model
 
+import com.tencent.iot.explorer.link.ErrorMessage
 import com.tencent.iot.explorer.link.kitlink.consts.SocketConstants
 import com.tencent.iot.explorer.link.kitlink.response.BaseResponse
 import com.tencent.iot.explorer.link.kitlink.util.HttpRequest
 import com.tencent.iot.explorer.link.kitlink.util.MyCallback
+import com.tencent.iot.explorer.link.kitlink.util.RequestCode
 import com.tencent.iot.explorer.link.mvp.ParentModel
 import com.tencent.iot.explorer.link.mvp.view.ModifyEmailView
 import com.tencent.iot.explorer.link.util.T
@@ -22,11 +24,27 @@ class ModifyEmailModel(view: ModifyEmailView) : ParentModel<ModifyEmailView>(vie
     }
 
     override fun fail(msg: String?, reqCode: Int) {
-
+        T.show(msg)
     }
 
     override fun success(response: BaseResponse, reqCode: Int) {
-        if (response.isSuccess()) {
+        when (reqCode) {
+            RequestCode.send_email_code -> {// 发送验证码
+                if (!response.isSuccess()) {
+                    val errMsg = ErrorMessage.parseErrorMessage(response.data.toString())
+                    view?.sendVerifyCodeFail(errMsg)
+                } else {
+                    view?.sendVerifyCodeSuccess()
+                }
+            }
+            RequestCode.update_user_info -> {// 更新邮箱号
+                if (!response.isSuccess()) {
+                    val errMsg = ErrorMessage.parseErrorMessage(response.data.toString())
+                    view?.updateEmailFail(errMsg)
+                } else {
+                    view?.updateEmailSuccess()
+                }
+            }
         }
     }
 }
