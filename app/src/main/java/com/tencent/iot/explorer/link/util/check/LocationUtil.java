9a12a05d03784fba80bfbe6761bc08fbbdd44b71
sevenhhe <sevenhhe@goodow.com>
2020-08-17 19:33:35 +0800

    APP配网优化体验问题记录 1.WiFi获取不到没显示未知wifi 2.一键配网和热点配网连接家庭wifi后，下一步报错，不能进行下一步 3.配网失败后，点击查看更多失败原因没有响应 4.一键配网进度卡在当前页面，状态无变化 5.设备热点密码框文案
    https://docs.qq.com/doc/DYVBKWXRKaWhjY3Vh
    
    Change-Id: I14362675aec145ad81135635a4ff798906be9bfa

--- app/src/main/java/com/tencent/iot/explorer/link/util/check/LocationUtil.java
+++ app/src/main/java/com/tencent/iot/explorer/link/util/check/LocationUtil.java
@@ -7,6 +7,7 @@ import android.location.LocationManager;
  * 位置相关的工具类
  */
 public class LocationUtil {
+
     public static boolean isLocationServiceEnable(Context context) {
         if (context != null) {
             LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
