9b220a5d47e7c775668ad179e3516ac591dd1e54
archurtan <archurspace@gmail.com>
2020-08-17 17:47:44 +0800

    用户体系账号支持国际化（添加设置温度单位的接口）
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192859002033
    
    Change-Id: Id086af163088bd1983bee13fc885d659531fa6ec

---
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/response/UserSettingResponse.kt
@@ -0,0 +1,7 @@
+package com.tencent.iot.explorer.link.kitlink.response
+
+import com.tencent.iot.explorer.link.kitlink.entity.UserSetting
+
+class UserSettingResponse {
+    var UserSetting: UserSetting? = null
+}
