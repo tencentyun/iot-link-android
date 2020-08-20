9bcfb9ddb52e1c1b179f853b626063c56c977228
archurtan <archurspace@gmail.com>
2020-08-20 14:24:21 +0800

    用户体系账号支持国际化
    1、对拉取的地区列表按照英文或中文首字母排序并显示对应索引字母
    2、从静态环境拉去区域列表并调整解析逻辑
    
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192859002033
    
    Change-Id: I96808f332d231e839b03ef40a7d3192895a49e3a

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/entity/RegionEntity.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/entity/RegionEntity.kt
@@ -1,10 +1,20 @@
 package com.tencent.iot.explorer.link.kitlink.entity
 
+import com.tencent.iot.explorer.link.kitlink.util.CommonUtils
+import com.tencent.iot.explorer.link.util.T
+import java.util.*
+
 class RegionEntity : Comparable<RegionEntity>{
     var Title = ""
+    var TitleEN = ""
     var RegionID = ""
     var Region = ""
+    var CountryCode = ""
     override fun compareTo(other: RegionEntity): Int {
-        return Region.compareTo(other.Region)
+        return if (CommonUtils.isChineseSystem()) {
+            -Title.compareTo(other.Title)
+        } else {
+            TitleEN.compareTo(other.TitleEN)
+        }
     }
 }
