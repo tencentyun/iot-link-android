0ff9e42792e95ca63db7a5ba7b8399509b554389
sevenhhe <sevenhhe@goodow.com>
2020-09-24 17:54:03 +0800

    [腾讯连连APP]APP h5嵌入&控制台APP开发 页面支持动态加载内容
    http://tapd.oa.com/20393192/prong/stories/view/1020393192859862773
    
    Change-Id: Id06c2eb0013d2c492bc61f451d0373c91d59cc57

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/fragment/SCStepFragment.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/fragment/SCStepFragment.kt
@@ -1,16 +1,47 @@
 package com.tencent.iot.explorer.link.kitlink.fragment
 
+import android.text.TextUtils
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.alibaba.fastjson.JSON
+import com.alibaba.fastjson.JSONObject
+import com.squareup.picasso.Picasso
+import com.tencent.iot.explorer.link.App
 import com.tencent.iot.explorer.link.R
+import com.tencent.iot.explorer.link.kitlink.consts.CommonField
+import com.tencent.iot.explorer.link.kitlink.consts.LoadViewTxtType
+import com.tencent.iot.explorer.link.kitlink.entity.HardwareGuide
+import com.tencent.iot.explorer.link.kitlink.entity.ProdConfigDetailEntity
+import com.tencent.iot.explorer.link.kitlink.response.BaseResponse
+import com.tencent.iot.explorer.link.kitlink.response.ProductsConfigResponse
+import com.tencent.iot.explorer.link.kitlink.util.HttpRequest
+import com.tencent.iot.explorer.link.kitlink.util.JsonManager
+import com.tencent.iot.explorer.link.kitlink.util.MyCallback
 import com.tencent.iot.explorer.link.mvp.IPresenter
+import com.tencent.iot.explorer.link.util.T
+import kotlinx.android.synthetic.main.item_device.view.*
 import kotlinx.android.synthetic.main.smart_config_first.*
 
 /**
  * SmartConfig步骤展示
  */
-class SCStepFragment : BaseFragment() {
+class SCStepFragment(type: Int, productId: String) : BaseFragment() {
 
     var onNextListener: OnNextListener? = null
+    private var tipContent: TextView? = null
+    private var pic: ImageView? = null
+    private var nextBtn: TextView? = null
+
+    private var type: Int = type
+    private var productId = ""
+
+    init {
+        this.productId = productId
+    }
+
+    constructor() : this( LoadViewTxtType.LoadLocalViewTxt.ordinal, "") {}
 
     override fun getPresenter(): IPresenter? {
         return null
@@ -21,8 +52,61 @@ class SCStepFragment : BaseFragment() {
     }
 
     override fun startHere(view: View) {
+        tipContent = view.findViewById(R.id.tv_smart_config_hint)
+        pic = view.findViewById(R.id.iv_smart_config)
+        nextBtn = view.findViewById(R.id.tv_smart_first_commit)
 
+        // 加载本地文案
+        if (type == LoadViewTxtType.LoadLocalViewTxt.ordinal) {
+            loadViewStandradInfo()
+        } else {    // 加载远端配置的文案
+            loadViewInfo()
+        }
         tv_smart_first_commit.setOnClickListener { onNextListener?.onNext() }
+    }
+
+    // 网络请求成功且返回自定义的内容，调用页面内容加载方法，网络请求失败无需调用
+    private fun loadViewInfo() {
+        if (TextUtils.isEmpty(productId)) {
+            return
+        }
+
+        val productsList  = arrayListOf<String>()
+        productsList.add(productId)
+        HttpRequest.instance.getProductsConfig(productsList, object: MyCallback{
+            override fun fail(msg: String?, reqCode: Int) {
+                T.show(msg)
+            }
+
+            override fun success(response: BaseResponse, reqCode: Int) {
+                response.parse(ProductsConfigResponse::class.java)?.run {
+                    val config = JsonManager.parseJson(Data[0].Config, ProdConfigDetailEntity::class.java)
+
+                    if (TextUtils.isEmpty(config.WifiSmartConfig)) {
+                        return
+                    }
+
+                    var json = JSONObject.parseObject(config.WifiSmartConfig)
+                    if (json.containsKey(CommonField.HARD_WARE_GUIDE)) {
+                        var hardwareGuide = JSONObject.parseObject(json.getString(CommonField.HARD_WARE_GUIDE), HardwareGuide::class.java)
+                        nextBtn?.setText(hardwareGuide.btnText)
+                        tipContent?.setText(hardwareGuide.message)
+                        Picasso.get().load(hardwareGuide.bgImg).placeholder(R.drawable.imageselector_default_error)
+                            .resize(App.data.screenWith / 5, App.data.screenWith / 5).centerCrop()
+                            .into(pic)
+                    }
+                }
+
+            }
+
+        })
+    }
+
+    // 网络请求成功且返回标准内容
+    private fun loadViewStandradInfo() {
+        nextBtn?.setText(R.string.smart_config_first_title)
+        tipContent?.setText(R.string.smart_config_first_hint2)
+        pic?.setImageResource(R.mipmap.image_smart_config)
     }
 
     interface OnNextListener {
