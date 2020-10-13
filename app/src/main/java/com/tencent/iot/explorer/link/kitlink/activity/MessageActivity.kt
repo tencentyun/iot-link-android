afc006e3a62bca00848c117ec8d57daf19a6db1f
sevenhhe <sevenhhe@goodow.com>
2020-10-13 16:40:59 +0800

    [腾讯连连APP]登录与消息界面点触优化（需求分支 3 支持三个 fragment 横向滑动，同时支持纵向的下拉刷新）
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192860120143
    
    Change-Id: Ibb7c6a8e4f55d42d9b4b18de510ee13edecebd81

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/activity/MessageActivity.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/activity/MessageActivity.kt
@@ -1,11 +1,14 @@
 package com.tencent.iot.explorer.link.kitlink.activity
 
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import com.tencent.iot.explorer.link.R
+import com.tencent.iot.explorer.link.customview.PageAdapter
 import com.tencent.iot.explorer.link.kitlink.fragment.MessageFragment
 import kotlinx.android.synthetic.main.activity_message.*
 import kotlinx.android.synthetic.main.menu_back_layout.*
+import java.util.*
 
 
 /**
@@ -15,24 +18,29 @@ class MessageActivity : BaseActivity() {
 
     private var previousPosition = 0
     private val fragments = arrayListOf<Fragment>()
+    private val page1 = MessageFragment(1)
+    private val page2 = MessageFragment(2)
+    private val page3 = MessageFragment(3)
+    private val mPageList: MutableList<MessageFragment> = ArrayList()
+    private lateinit var mAdapter :PageAdapter
+
 
     override fun getContentView(): Int {
         return R.layout.activity_message
     }
 
-
     override fun initView() {
         iv_back.setColorFilter(resources.getColor(R.color.black_333333))
         tv_title.text = getString(R.string.message_notification)
 
-        fragments.clear()
-        fragments.add(MessageFragment(1))
-        fragments.add(MessageFragment(2))
-        fragments.add(MessageFragment(3))
-        this.supportFragmentManager.beginTransaction()
-            .add(R.id.message_container, fragments[0])
-            .show(fragments[0])
-            .commit()
+        mPageList.add(page1)
+        mPageList.add(page2)
+        mPageList.add(page3)
+        mAdapter = PageAdapter(this.supportFragmentManager, mPageList)
+        fragment_pager.setAdapter(mAdapter)
+        fragment_pager.setPagingEnabled(true)
+        fragment_pager.setOffscreenPageLimit(3)
+        fragment_pager.setCurrentItem(0)
     }
 
     override fun setListener() {
@@ -45,21 +53,26 @@ class MessageActivity : BaseActivity() {
             }
 
             override fun onTabSelected(tab: TabLayout.Tab) {
-                showFragment(tab.position)
+                if (fragment_pager != null) {
+                    fragment_pager.setCurrentItem(tab.position)
+                }
             }
         })
+
+        fragment_pager.setOnPageChangeListener(pageSelectListener)
     }
 
-    private fun showFragment(position: Int) {
-        val transaction = this.supportFragmentManager.beginTransaction()
-        if (fragments[position].isAdded) {
-            transaction.show(fragments[position]).hide(fragments[previousPosition]).commit()
-        } else {
-            transaction.add(R.id.message_container, fragments[position])
-                .show(fragments[position]).hide(fragments[previousPosition])
-                .commit()
+    private var pageSelectListener = object :ViewPager.OnPageChangeListener{
+        override fun onPageScrollStateChanged(state: Int) {}
+
+        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            if (tab_message != null) {
+                tab_message.setScrollPosition(position, positionOffset, true)
+            }
         }
-        previousPosition = position
+
+        override fun onPageSelected(position: Int) {}
+
     }
 
 }
