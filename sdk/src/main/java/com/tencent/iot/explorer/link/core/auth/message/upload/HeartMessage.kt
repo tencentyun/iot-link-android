09459ee40ff61c31822a8a001beff20f950d8317
archurtan <archurspace@gmail.com>
2020-10-19 16:48:52 +0800

    Link auth模块去重
    Change-Id: I8a18835080885711f4fcea59253627cb7280e3dc

--- sdk/src/main/java/com/tencent/iot/explorer/link/core/auth/message/upload/HeartMessage.kt
+++ sdk/src/main/java/com/tencent/iot/explorer/link/core/auth/message/upload/HeartMessage.kt
@@ -1,5 +1,7 @@
 package com.tencent.iot.explorer.link.core.auth.message.upload
 
+import com.tencent.iot.explorer.link.core.auth.message.MessageConst
+
 
 /**
  * 解绑设备
@@ -14,7 +16,7 @@ class HeartMessage(deviceIds: ArrayString) : YunMessage() {
 
     override fun toString(): String {
         reqId = 2
-        addValue("DeviceIds", deviceIds)
+        addValue(MessageConst.DEVICE_IDS, deviceIds)
         return super.toString()
     }
 
