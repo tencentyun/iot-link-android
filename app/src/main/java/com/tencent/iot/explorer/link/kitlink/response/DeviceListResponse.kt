24dd44cc8ef5ce58b6b5631fbc4304e2749d713f
archurtan <archurspace@gmail.com>
2020-10-21 19:34:42 +0800

    Link entity模块去重
    Change-Id: I780251de06c032c7a30799571d1a91493f58c504

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/response/DeviceListResponse.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/response/DeviceListResponse.kt
@@ -1,6 +1,6 @@
 package com.tencent.iot.explorer.link.kitlink.response
 
-import com.tencent.iot.explorer.link.kitlink.entity.DeviceEntity
+import com.tencent.iot.explorer.link.core.auth.entity.DeviceEntity
 
 class DeviceListResponse {
     var Total = 0
--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/response/ShareDeviceListRespone.kt
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/response/ShareDeviceListRespone.kt
@@ -1,11 +1,11 @@
 package com.tencent.iot.explorer.link.core.demo.response
 
-import com.tencent.iot.explorer.link.core.auth.entity.Device
+import com.tencent.iot.explorer.link.core.auth.entity.DeviceEntity
 
 class ShareDeviceListRespone {
 
     var Total = 0
     var RequestId = ""
-    var ShareDevices = ArrayList<Device>()
+    var ShareDevices = ArrayList<DeviceEntity>()
 
 }
