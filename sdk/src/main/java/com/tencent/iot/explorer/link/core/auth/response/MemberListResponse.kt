ff89e7c16c5c0eae8c20926471072e0d9e159726
archurtan <archurspace@gmail.com>
2020-10-22 19:03:45 +0800

    Link response模块去重
    Change-Id: I0b90b5197ddcfcff78ab6f219e09c27461ceebdb

--- sdk/src/main/java/com/tencent/iot/explorer/link/core/auth/response/MemberListResponse.kt
+++ sdk/src/main/java/com/tencent/iot/explorer/link/core/auth/response/MemberListResponse.kt
@@ -1,6 +1,6 @@
-package com.tencent.iot.explorer.link.kitlink.response
+package com.tencent.iot.explorer.link.core.auth.response
 
-import com.tencent.iot.explorer.link.kitlink.entity.MemberEntity
+import com.tencent.iot.explorer.link.core.link.entity.MemberEntity
 
 /**
  * 家庭成员列表响应实体
--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/response/ShareUserResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.tencent.iot.explorer.link.core.demo.response
-
-import com.tencent.iot.explorer.link.core.demo.entity.ShareUser
-
-class ShareUserResponse {
-    var Users: List<ShareUser>? = null
-    var RequestId = ""
-    var Total = 0
-}
