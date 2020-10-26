8a6a0cec24cd7bd656aa9627adf67c13719ba664
sevenhhe <sevenhhe@goodow.com>
2020-10-26 14:04:55 +0800

    [腾讯连连APP]新增智能场景联动
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192859882785
    
    Change-Id: I06e2ea628c5e2e35b606d2b3448e0e13d73980b8

--- app/src/main/java/com/tencent/iot/explorer/link/customview/PageAdapter.java
+++ app/src/main/java/com/tencent/iot/explorer/link/customview/PageAdapter.java
@@ -3,20 +3,21 @@ package com.tencent.iot.explorer.link.customview;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.tencent.iot.explorer.link.kitlink.fragment.BaseFragment;
 import com.tencent.iot.explorer.link.kitlink.fragment.MessageFragment;
 
 import java.util.List;
 
 public class PageAdapter extends FragmentPagerAdapter {
-    List<MessageFragment> mPages;
+    List<BaseFragment> mPages;
 
-    public PageAdapter(FragmentManager fm, List<MessageFragment> pages) {
+    public PageAdapter(FragmentManager fm, List<BaseFragment> pages) {
         super(fm);
         mPages = pages;
     }
 
     @Override
-    public MessageFragment getItem(int arg0) {
+    public BaseFragment getItem(int arg0) {
         try {
             return mPages.get(arg0);
         } catch (Exception e) {
