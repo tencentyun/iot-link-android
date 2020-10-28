d2f00529740a78b9e4a70b8d7875a0360a67c5cf
v_vweisun(孙伟) <532125505@qq.com>
2020-10-28 09:30:32 +0800

    【腾讯连连APP】安卓配网视觉与ios同步需求
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192860444833
    
    Change-Id: Ia04a9616bf5bd9413cabb141f855856370d38db3

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/fragment/WifiFragment.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/fragment/WifiFragment.kt
@@ -5,32 +5,38 @@ import android.content.Intent
 import android.net.wifi.WifiInfo
 import android.net.wifi.WifiManager
 import android.text.Editable
+import android.text.TextUtils
 import android.text.TextWatcher
 import android.view.View
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import com.tencent.iot.explorer.link.R
+import com.tencent.iot.explorer.link.T
 import com.tencent.iot.explorer.link.customview.dialog.WifiHelperDialog
 import com.tencent.iot.explorer.link.mvp.IPresenter
 import com.tencent.iot.explorer.link.core.utils.LocationUtil
 import com.tencent.iot.explorer.link.core.utils.KeyBoardUtils
+import com.tencent.iot.explorer.link.kitlink.consts.CommonField
 import kotlinx.android.synthetic.main.fragment_wifi.*
 import kotlinx.android.synthetic.main.smart_config_second.*
 
 /**
  * 输入wifi密码
  */
-class WifiFragment(): BaseFragment() {
+class WifiFragment() : BaseFragment() {
 
     private var type = 0
     private var wifiInfo: WifiInfo? = null
-    private var showPwd = false
+    private var bssid = ""
+    var showTipTag = false
+
+    constructor(type: Int, showTag: Boolean) : this() {
+        this.type = type
+        showTipTag = showTag
+    }
 
     var onCommitWifiListener: OnCommitWifiListener? = null
     var openWifiDialog: WifiHelperDialog? = null
     var openLocationServiceDialog: WifiHelperDialog? = null
-
-    constructor(type: Int):this(){
-       this.type = type
-    }
 
     companion object {
         const val smart_config = 0
@@ -46,9 +52,9 @@ class WifiFragment(): BaseFragment() {
      */
     private fun showWifiInfo() {
         context?.let {
-            val wifiManager =
-                it.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+            val wifiManager = it.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
             wifiInfo = wifiManager.connectionInfo
+
             if (wifiInfo == null || wifiInfo!!.bssid == null) {
                 tv_select_wifi.hint = getString(R.string.not_network)
                 tv_wifi_commit.isEnabled = false
@@ -62,9 +68,24 @@ class WifiFragment(): BaseFragment() {
                     openLocationServiceDialog?.show()
                 }
                 tv_select_wifi.setText(ssid2Set)
-
+                if (tv_select_wifi.text.contains(CommonField.SSID_UNKNOWN)) {
+                    T.show(getString(R.string.open_location_tip))
+                }
+                bssid = wifiInfo!!.bssid
             }
-            tv_select_wifi.isEnabled = false
+            tv_select_wifi.isEnabled = type == soft_ap
+
+            isNextClickable()
+
+            if (showTipTag) {
+                tv_method.visibility = View.VISIBLE
+                tv_method_tip.visibility = View.VISIBLE
+                tv_tip_wifi.setText(R.string.connect_dev_wifi)
+            } else {
+                tv_method.visibility = View.GONE
+                tv_method_tip.visibility = View.GONE
+                tv_tip_wifi.setText(R.string.input_wifi_pwd)
+            }
         }
     }
 
@@ -87,7 +108,6 @@ class WifiFragment(): BaseFragment() {
             override fun onOkClicked() {
                 startActivity(Intent(android.provider.Settings.ACTION_WIFI_SETTINGS))
             }
-
             override fun onCancelClicked() {}
         })
         openLocationServiceDialog = WifiHelperDialog(context, getString(R.string.please_open_location_service))
@@ -95,23 +115,14 @@ class WifiFragment(): BaseFragment() {
             override fun onOkClicked() {
                 startActivity(Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS))
             }
-
             override fun onCancelClicked() {}
         })
         showWifiInfo()
         et_select_wifi_pwd.addClearImage(iv_wifi_eye_clear)
-        et_select_wifi_pwd.addShowImage(
-            iv_wifi_eye,
-            R.mipmap.icon_visible,
-            R.mipmap.icon_invisible
-        )
+        et_select_wifi_pwd.addShowImage(iv_wifi_eye, R.mipmap.icon_visible, R.mipmap.icon_invisible)
 
         tv_select_wifi.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                s?.let {
-                    tv_wifi_commit.isEnabled = it.isNotEmpty()
-                }
-            }
+            override fun afterTextChanged(s: Editable?) {}
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
@@ -132,32 +143,28 @@ class WifiFragment(): BaseFragment() {
                 et_select_wifi_pwd
             )
         }
-        /*iv_wifi_eye.setOnClickListener {
-            showPwd = !showPwd
-            et_select_wifi_pwd.inputType = if (showPwd) {
-                InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
-            } else {
-                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
-            }
-            iv_wifi_eye.setImageResource(
-                if (showPwd) {
-                    R.mipmap.icon_invisible
-                } else {
-                    R.mipmap.icon_visible
-                }
-            )
-            et_select_wifi_pwd.setSelection(et_select_wifi_pwd.length())
-        }*/
+
         container_wifi.setOnClickListener {
-            KeyBoardUtils.hideKeyBoard(
-                context,
-                et_select_wifi_pwd
-            )
+            KeyBoardUtils.hideKeyBoard(context, et_select_wifi_pwd)
         }
+
         iv_select_wifi.setOnClickListener {
             startActivity(Intent(android.provider.Settings.ACTION_WIFI_SETTINGS))
         }
     }
+
+    private fun isNextClickable() {
+        if (tv_select_wifi.text != null && (TextUtils.isEmpty(tv_select_wifi.text.toString())) ||
+            tv_select_wifi.text.toString().equals(CommonField.SSID_UNKNOWN)) {
+            tv_wifi_commit.isClickable = false
+            tv_wifi_commit.background = getDrawable(context!!, R.drawable.bg_edit)
+            return
+        }
+
+        tv_wifi_commit.isClickable = true
+        tv_wifi_commit.background = getDrawable(context!!, R.drawable.btn_bg)
+    }
+
 
     interface OnCommitWifiListener {
         fun commitWifi(ssid: String, bssid: String?, password: String)
