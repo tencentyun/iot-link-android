a63b35224673606d94744bfe14406cc197cce570
sevenhhe <sevenhhe@goodow.com>
2020-11-02 15:14:06 +0800

    [腾讯连连APP]新增智能场景联动
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192859882785
    
    Change-Id: I06e2ea628c5e2e35b606d2b3448e0e13d73980b8

---
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/entity/Automation.kt
@@ -0,0 +1,10 @@
+package com.tencent.iot.explorer.link.kitlink.entity
+
+class Automation {
+    var AutomationId = ""
+    var Icon = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603883255486&di=c20471422b0eef469d01145f3da7fd47&imgtype=0&src=http%3A%2F%2Fa0.att.hudong.com%2F56%2F12%2F01300000164151121576126282411.jpg"
+    var Name = "1111"
+    var Status = 0
+    var type = 0 // 0 手动   1 自动
+    var desc = "xxxxxxxxxxx" // x 个设备
+}
