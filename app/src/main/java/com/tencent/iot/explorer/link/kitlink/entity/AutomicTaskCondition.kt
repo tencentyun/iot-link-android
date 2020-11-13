b21a7fe1189d841ee968352cd9b67fa7e0963a86
sevenhhe <sevenhhe@goodow.com>
2020-11-13 10:02:46 +0800

    [腾讯连连APP]新增智能场景联动（自动智能）
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192859882785
    
    Change-Id: I35fa0bc2966f198ad2eedc4076b53f624ff80a49

---
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/entity/AutomicTaskCondition.kt
@@ -0,0 +1,16 @@
+package com.tencent.iot.explorer.link.kitlink.entity
+
+class AutomicTaskCondition {
+    var type = 0   // 0 定时  1 条件变化
+//    var devName = "" // 用于界面显示的设备别名
+//    var deviceName = "" // 真正的不可设置的设备名
+//    var taskTip = ""
+//    var task = ""
+//    var taskKey = ""
+//    var hour = 0 // 当 type 为 0 时，该字段生效
+//    var min = 0 // 当 type 为 0 时，该字段生效
+//    var pos = -1
+//    var iconUrl = ""
+//    var productId = ""
+//    var actionId = ""
+}
