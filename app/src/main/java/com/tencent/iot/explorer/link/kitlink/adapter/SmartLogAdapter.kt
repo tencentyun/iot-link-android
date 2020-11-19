4e9a44e60df694f9ddb89e4fe75c73581cf6376d
sevenhhe <sevenhhe@goodow.com>
2020-11-19 10:05:01 +0800

    [腾讯连连APP]新增智能场景联动（执行日志页面以及接口调试）
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192859882785
    
    Change-Id: Idf96d52162bcdabcb6b2c33f871f4b23a6ca2db1

---
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/adapter/SmartLogAdapter.kt
@@ -0,0 +1,125 @@
+package com.tencent.iot.explorer.link.kitlink.adapter
+
+import android.content.Context
+import android.text.TextUtils
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ListView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.chauthai.swipereveallayout.SwipeRevealLayout
+import com.squareup.picasso.Picasso
+import com.tencent.iot.explorer.link.App
+import com.tencent.iot.explorer.link.R
+import com.tencent.iot.explorer.link.T
+import com.tencent.iot.explorer.link.kitlink.entity.LogMessage
+import com.tencent.iot.explorer.link.kitlink.entity.ManualTask
+import kotlinx.android.synthetic.main.activity_complete_task_info.*
+import java.util.*
+
+class SmartLogAdapter(context: Context, list: MutableList<LogMessage>) : RecyclerView.Adapter<SmartLogAdapter.ViewHolder>() {
+    var list: MutableList<LogMessage> = LinkedList()
+    var index = -1;
+    var context: Context? = null
+
+    init {
+        this.list = list
+        this.context = context
+    }
+
+    class ViewHolder(layoutView: View) : RecyclerView.ViewHolder(layoutView) {
+        var allLayout: View
+        var dateTipLayout: ConstraintLayout
+        var runSuccessStaus: ImageView
+        var day: TextView
+        var mouth: TextView
+        var taskName: TextView
+        var taskDesc: TextView
+        var moreBtn: ImageView
+        var moreInfo: ConstraintLayout
+        var failedDetail: ListView
+
+        init {
+            allLayout = layoutView
+            dateTipLayout = layoutView.findViewById(R.id.title_layout)
+            day = layoutView.findViewById(R.id.tv_day)
+            mouth = layoutView.findViewById(R.id.tv_mouth)
+            runSuccessStaus = layoutView.findViewById(R.id.iv_status)
+            taskName = layoutView.findViewById(R.id.tv_task_name)
+            taskDesc = layoutView.findViewById(R.id.tv_run_desc)
+            moreBtn = layoutView.findViewById(R.id.iv_more)
+            moreInfo = layoutView.findViewById(R.id.more_layout)
+            failedDetail = layoutView.findViewById(R.id.lv_failed_detail)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_run_task_log, parent, false)
+        val holder = ViewHolder(view)
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        if (position == 0) {
+            holder.dateTipLayout.visibility = View.VISIBLE
+        } else if (list.get(position).mouth == list.get(position - 1).mouth &&
+                list.get(position).day == list.get(position - 1).day) {
+            holder.dateTipLayout.visibility = View.GONE
+        } else {
+            holder.dateTipLayout.visibility = View.VISIBLE
+        }
+        holder.mouth.setText(list.get(position).mouth + context?.getString(R.string.unit_mouth))
+        holder.day.setText(list.get(position).day)
+
+        if (list.get(position).resultCode == 0) {
+            holder.runSuccessStaus.setImageResource(R.mipmap.right)
+            holder.moreBtn.visibility = View.GONE
+            holder.moreInfo.visibility = View.GONE
+        } else {
+            holder.runSuccessStaus.setImageResource(R.mipmap.warning)
+            holder.moreBtn.visibility = View.VISIBLE
+            holder.moreInfo.visibility = View.GONE
+        }
+
+        if (TextUtils.isEmpty(list.get(position).automationName)) {
+            holder.taskName.setText(list.get(position).sceneName)
+        } else {
+            holder.taskName.setText(list.get(position).automationName)
+        }
+
+        holder.taskDesc.setText(list.get(position).time + " " + list.get(position).result)
+        holder.failedDetail.adapter = ActionResultAdapter(context, list.get(position).actionResults)
+
+        holder.allLayout.setOnClickListener{
+            if (list.get(position).resultCode != 0 && holder.moreInfo.visibility == View.VISIBLE) {
+                holder.moreBtn.rotation = -90F
+                holder.moreInfo.visibility = View.GONE
+            } else if (list.get(position).resultCode != 0 && holder.moreInfo.visibility == View.GONE) {
+                holder.moreBtn.rotation = 90F
+                holder.moreInfo.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        if (list == null) {
+            return 0
+        }
+        return list.size
+    }
+
+    interface OnItemClicked {
+        fun onItemClicked(pos: Int, logMessage: LogMessage)
+    }
+
+    private var onItemClicked: OnItemClicked? = null
+
+    fun setOnItemClicked(onItemClicked: OnItemClicked?) {
+        this.onItemClicked = onItemClicked
+    }
+}
--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/fragment/SmartLogFragment.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/fragment/SmartLogFragment.kt
@@ -1,14 +1,36 @@
 package com.tencent.iot.explorer.link.kitlink.fragment
 
+import android.util.Log
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.alibaba.fastjson.JSON
+import com.alibaba.fastjson.JSONObject
+import com.scwang.smart.refresh.footer.ClassicsFooter
+import com.scwang.smart.refresh.header.ClassicsHeader
+import com.scwang.smart.refresh.layout.listener.OnLoadMoreListener
+import com.scwang.smart.refresh.layout.listener.OnRefreshListener
+import com.tencent.iot.explorer.link.App
 import com.tencent.iot.explorer.link.R
+import com.tencent.iot.explorer.link.T
+import com.tencent.iot.explorer.link.core.auth.response.BaseResponse
+import com.tencent.iot.explorer.link.kitlink.adapter.SmartLogAdapter
+import com.tencent.iot.explorer.link.kitlink.entity.LogMessage
+import com.tencent.iot.explorer.link.kitlink.entity.LogResponesData
+import com.tencent.iot.explorer.link.kitlink.util.HttpRequest
+import com.tencent.iot.explorer.link.kitlink.util.MyCallback
+import com.tencent.iot.explorer.link.kitlink.util.RequestCode
 import com.tencent.iot.explorer.link.mvp.IPresenter
+import kotlinx.android.synthetic.main.fragment_smart_log.*
+import java.util.*
 
 /**
  * 智能日志页面
  */
-class SmartLogFragment() : BaseFragment() {
+class SmartLogFragment() : BaseFragment(), MyCallback {
 
+    private var msgId = ""
+    var logs: MutableList<LogMessage> = LinkedList()
+    var adapter: SmartLogAdapter? = null
 
     override fun getPresenter(): IPresenter? {
         return null
@@ -19,7 +41,67 @@ class SmartLogFragment() : BaseFragment() {
     }
 
     override fun startHere(view: View) {
-
+        initView()
     }
 
+    fun initView() {
+        val layoutManager = LinearLayoutManager(context)
+        lv_all_log.setLayoutManager(layoutManager)
+        adapter = SmartLogAdapter(context!!, logs)
+        lv_all_log.adapter = adapter
+        //下拉刷新及上拉加载
+        log_refreshLayout.setEnableRefresh(true)  // 禁止上拉刷新
+        log_refreshLayout.setRefreshHeader(ClassicsHeader(context))
+        log_refreshLayout.setEnableLoadMore(true)
+        log_refreshLayout.setRefreshFooter(ClassicsFooter(context))
+        log_refreshLayout.setOnRefreshListener(onRefreshListener)
+        log_refreshLayout.setOnLoadMoreListener(onLoadMoreListener)
+    }
+
+    private var onRefreshListener = OnRefreshListener {
+        reloadData()
+    }
+
+    private var onLoadMoreListener = OnLoadMoreListener {
+        loadMore()
+    }
+
+    private fun loadMore() {
+        HttpRequest.instance.getTaskRunLog(msgId, App.data.getCurrentFamily().FamilyId, this)
+    }
+
+    private fun reloadData() {
+        logs.clear()
+        msgId = ""
+        HttpRequest.instance.getTaskRunLog(msgId, App.data.getCurrentFamily().FamilyId, this)
+    }
+
+    override fun fail(msg: String?, reqCode: Int) {
+        log_refreshLayout.finishRefresh()
+        log_refreshLayout.finishLoadMore()
+        T.show(msg)
+    }
+
+    override fun success(response: BaseResponse, reqCode: Int) {
+        when (reqCode) {
+            RequestCode.get_run_task_log -> {
+                if (response.isSuccess()) {
+                    log_refreshLayout.finishRefresh()
+                    log_refreshLayout.finishLoadMore()
+                    var dataJson = response.data as JSONObject
+                    var logResponesData = JSON.parseObject(dataJson.getString("Data"), LogResponesData::class.java)
+                    Log.e("XXX", "log " + JSON.toJSONString(logResponesData))
+                    for (i in 0 until logResponesData.msgs!!.size) {
+                        logs.add(logResponesData.msgs!!.get(i))
+                    }
+                    msgId = logs.get(logs.lastIndex).msgId
+                    adapter?.notifyDataSetChanged()
+                } else {
+                    log_refreshLayout.finishRefresh()
+                    log_refreshLayout.finishLoadMore()
+                    T.show(response.msg)
+                }
+            }
+        }
+    }
 }
