443c8f8ecb1ef7689e78b3bce7c168a11d9f6ab4
v_vweisun(孙伟) <532125505@qq.com>
2020-11-19 09:15:39 +0800

    【ID860753847】 IoT Explorer TRTC V1.0版本，复用TRTC Demo语音呼叫和视频呼叫页面
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192860753847
    
    Change-Id: I7dae71b11ff1b621317e110370bd7d76605898cf

---
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/trtc/liveroom/model/impl/base/TXCallback.java
@@ -0,0 +1,5 @@
+package com.tencent.iot.explorer.link.core.demo.trtc.liveroom.model.impl.base;
+
+public interface TXCallback {
+    void onCallback(int code, String msg);
+}
---
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/trtc/liveroom/model/impl/base/TXRoomInfoListCallback.java
@@ -0,0 +1,8 @@
+package com.tencent.iot.explorer.link.core.demo.trtc.liveroom.model.impl.base;
+
+
+import java.util.List;
+
+public interface TXRoomInfoListCallback {
+    void onCallback(int code, String msg, List<TXRoomInfo> list);
+}
