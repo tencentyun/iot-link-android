374527e8f2264461d9f20a9cdd74fbf0d8c4cb96
sevenhhe <sevenhhe@goodow.com>
2020-11-20 11:44:51 +0800

    [腾讯连连APP]新增智能场景联动（修复条件和设备控制本地判断问题，通过接口返回筛选条件和设备控制）
    Change-Id: I437a3a8bc0e7532f6d050c4cd0bf49fbf40b415e

---
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/entity/AppAutomation.kt
@@ -0,0 +1,6 @@
+package com.tencent.iot.explorer.link.kitlink.entity
+
+class AppAutomation {
+    var actions: MutableList<String> = ArrayList()
+    var conditions: MutableList<String> = ArrayList()
+}
