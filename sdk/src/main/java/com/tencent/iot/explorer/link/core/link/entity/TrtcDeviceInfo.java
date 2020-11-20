7666a5f02258465b9e309ef74871a2979a97ee91
archurtan <archurspace@gmail.com>
2020-11-20 09:38:49 +0800

    【ID860753847】 IoT Explorer TRTC V1.0版本, 增加蓝牙绑定设备接口
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192860753847
    
    Change-Id: I86ea908eec4144bcfb0408be19ec1b0cf8f29eb6

---
+++ sdk/src/main/java/com/tencent/iot/explorer/link/core/link/entity/TrtcDeviceInfo.java
@@ -0,0 +1,31 @@
+package com.tencent.iot.explorer.link.core.link.entity;
+
+public class TrtcDeviceInfo {
+
+    private String mProductId;
+    private String mDeviceName;
+    private String mConnId;
+    private long mTimestamp;
+    private String mSignMethod;
+    private String mSignature;
+
+    public TrtcDeviceInfo(String deviceInfo) {
+        //${product_id};${device_name};${random};${timestamp};hmacsha256;sign
+        String[] items = deviceInfo.split(";");
+        if (items.length == 6) {
+            mProductId = items[0];
+            mDeviceName = items[1];
+            mConnId = items[2];
+            mTimestamp = Long.parseLong(items[3]);
+            mSignMethod = items[4];
+            mSignature = items[5];
+        }
+    }
+
+    public String getProductId() { return mProductId; }
+    public String getDeviceName() { return mDeviceName; }
+    public String getConnId() { return mConnId; }
+    public long getTimestamp() { return mTimestamp; }
+    public String getSignMethod() { return mSignMethod; }
+    public String getSignature() { return mSignature; }
+}
