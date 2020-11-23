390f3547b28b1ef10c896ef4d7a5af696bc6d986
sevenhhe <sevenhhe@goodow.com>
2020-11-23 16:16:17 +0800

    修复手动智能/自动智能选择设备页面，因为某一个设备名过长造成最后一行信息显示不全的问题 修复因为绑定的设备过多，超过分页的 20 的限制，无法显示数据的问题
    Change-Id: I28de908efa68b6b16dfa224b13274e4115eebee0

---
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/adapter/DeviceAdapter.kt
@@ -0,0 +1,69 @@
+package com.tencent.iot.explorer.link.kitlink.adapter
+
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import com.tencent.iot.explorer.link.R
+import com.tencent.iot.explorer.link.core.auth.entity.DeviceEntity
+import java.util.*
+
+class DeviceAdapter(list: MutableList<DeviceEntity>) : RecyclerView.Adapter<DeviceAdapter.ViewHolder>() {
+    var list: MutableList<DeviceEntity> = LinkedList()
+
+    init {
+        this.list = list
+    }
+
+    class ViewHolder(layoutView: View) : RecyclerView.ViewHolder(layoutView) {
+        var image: ImageView
+        var text: TextView
+
+        init {
+            image = layoutView.findViewById(R.id.iv_device_icon)
+            text = layoutView.findViewById(R.id.tv_device_name)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.device_item, parent, false)
+        val holder = ViewHolder(view)
+        view.setOnClickListener {
+            val position = holder.adapterPosition
+            if (onItemClicked != null) {
+                onItemClicked!!.onItemClicked(position, list[position])
+            }
+        }
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (TextUtils.isEmpty(list.get(position)?.IconUrl)) {
+            Picasso.get().load(R.drawable.device_placeholder).into(holder.image)
+        } else {
+            Picasso.get().load(list.get(position)?.IconUrl).into(holder.image)
+        }
+        holder.text.setText(list.get(position)?.getAlias())
+    }
+
+    override fun getItemCount(): Int {
+        if (list == null) {
+            return 0
+        }
+        return list.size
+    }
+
+    interface OnItemClicked {
+        fun onItemClicked(pos: Int, dev: DeviceEntity)
+    }
+
+    private var onItemClicked: OnItemClicked? = null
+
+    fun setOnItemClicked(onItemClicked: OnItemClicked?) {
+        this.onItemClicked = onItemClicked
+    }
+}
