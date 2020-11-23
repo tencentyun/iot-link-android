afcbdbef1e507f0a52da73b47e86a26fbfe9a291
archurtan <archurspace@gmail.com>
2020-11-23 16:40:23 +0800

    添加trtc设备控制面板逻辑
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192860753847
    
    Change-Id: Ie2c69da7e3c760b1ec51a1de90570547ed3a7733

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/holder/ControlSimpleLongHolder.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/holder/ControlSimpleLongHolder.kt
@@ -1,6 +1,7 @@
 package com.tencent.iot.explorer.link.kitlink.holder
 
 import android.content.Context
+import android.view.View
 import android.view.ViewGroup
 import com.tencent.iot.explorer.link.R
 import com.tencent.iot.explorer.link.kitlink.entity.DevicePropertyEntity
@@ -18,10 +19,14 @@ class ControlSimpleLongHolder : CRecyclerView.CViewHolder<DevicePropertyEntity> 
         entity?.run {
             itemView.tv_simple_long_name.text = name
             itemView.tv_simple_long_value.text = getValueText()
-            when (id) {
-                "color" -> itemView.iv_simple_long.setImageResource(R.mipmap.icon_control_color)
-                "power_switch" -> itemView.iv_simple_long.setImageResource(R.mipmap.icon_control_switch)
-                else -> itemView.iv_simple_long.setImageResource(R.mipmap.icon_control_brightness)
+            if (trtc) {
+                // 调整UI
+            } else {
+                when (id) {
+                    "color" -> itemView.iv_simple_long.setImageResource(R.mipmap.icon_control_color)
+                    "power_switch" -> itemView.iv_simple_long.setImageResource(R.mipmap.icon_control_switch)
+                    else -> itemView.iv_simple_long.setImageResource(R.mipmap.icon_control_brightness)
+                }
             }
         }
         itemView.setOnClickListener { recyclerItemView?.doAction(this, it, position) }
