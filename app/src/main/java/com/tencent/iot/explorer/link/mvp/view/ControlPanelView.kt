4c5e715c4fc9b707e6a45c0b1c37d2ea966afd12
archurtan <archurspace@gmail.com>
2020-11-26 20:45:27 +0800

    移动websocket监听至首页以及添加轮询逻辑
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192860753847
    
    Change-Id: Ie9d3e84395e5156193eb328b0c0a1a39e11ebfcf

--- app/src/main/java/com/tencent/iot/explorer/link/mvp/view/ControlPanelView.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/mvp/view/ControlPanelView.kt
@@ -2,9 +2,11 @@ package com.tencent.iot.explorer.link.mvp.view
 
 import com.tencent.iot.explorer.link.mvp.ParentView
 import com.tencent.iot.explorer.link.core.auth.entity.NavBar
+import com.tencent.iot.explorer.trtc.model.RoomKey
 
 interface ControlPanelView : ParentView {
 
     fun showControlPanel(navBar: NavBar?, timingProject: Boolean)
 
+    fun enterRoom(room: RoomKey)
 }
