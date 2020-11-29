d225f51f4ba9eb147ae728eafbfd18b71eebbdf7
sevenhhe <sevenhhe@goodow.com>
2020-11-29 16:50:11 +0800

    通过 zzconfig 获取联动图片的 url
    Change-Id: I8c9c0f489a5666f02c2fe4ce801eeee01fb53bd5

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/activity/SelectTaskPicActivity.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/activity/SelectTaskPicActivity.kt
@@ -1,31 +1,20 @@
 package com.tencent.iot.explorer.link.kitlink.activity
 
 import android.content.Intent
-import android.util.Log
+import android.text.TextUtils
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.alibaba.fastjson.JSON
+import com.alibaba.fastjson.JSONArray
 import com.tencent.iot.explorer.link.R
-import com.tencent.iot.explorer.link.kitlink.adapter.ManualTaskAdapter
+import com.tencent.iot.explorer.link.T
 import com.tencent.iot.explorer.link.kitlink.adapter.SelectPicAdapter
 import com.tencent.iot.explorer.link.kitlink.consts.CommonField
-import com.tencent.iot.explorer.link.kitlink.entity.ManualTask
-import kotlinx.android.synthetic.main.activity_add_manual_task.*
-import kotlinx.android.synthetic.main.activity_delay_time.*
+import com.tencent.iot.explorer.link.kitlink.util.HttpRequest
+import com.tencent.iot.explorer.link.kitlink.util.MyCustomCallBack
+import com.tencent.iot.explorer.link.kitlink.util.RequestCode
 import kotlinx.android.synthetic.main.activity_select_smart_pic.*
 import kotlinx.android.synthetic.main.menu_back_layout.*
 
-
 class SelectTaskPicActivity : BaseActivity() {
-    val array = arrayOf("https://main.qcloudimg.com/raw/c05e0ef33ff62962a089649800cd5ce9/scene1.jpg",
-        "https://main.qcloudimg.com/raw/a699919a2d7df048757facf781f9449e/scene2.jpg",
-        "https://main.qcloudimg.com/raw/41a727f20f200a1100c6e5cf7ac40088/scene3.jpg",
-        "https://main.qcloudimg.com/raw/493cd8e417bb990c3662f5689bf32074/scene4.jpg",
-        "https://main.qcloudimg.com/raw/a383821b3bf8eab99ccc4e51935bbf95/scene5.jpg",
-        "https://main.qcloudimg.com/raw/9c04afe82f2d18448efa45e239ee1244/scene6.jpg",
-        "https://main.qcloudimg.com/raw/4aa0aff9c1f0f67df0d0ad7e906d736a/scene7.jpg",
-        "https://main.qcloudimg.com/raw/0a5e2254ef293ef32a4749e77c4e73fa/scene8.jpg")
-
     private var listUrl = ArrayList<String>()
     private var adapter: SelectPicAdapter? = SelectPicAdapter(listUrl)
     @Volatile
@@ -42,15 +31,42 @@ class SelectTaskPicActivity : BaseActivity() {
         gv_pic.setLayoutManager(layoutManager)
         adapter?.setOnItemClicked(onItemClicked)
         gv_pic.setAdapter(adapter)
-        listUrl.addAll(array)
-        adapter?.index = 0
-        for (i in 0 until listUrl.size) {
-            if (listUrl.get(i) == picUrl) {
-                adapter?.index = i
-            }
+
+        HttpRequest.instance.getTaskPicList(CommonField.TASK_PIC_LIST_URL, loadPicUrlListener, RequestCode.get_tasl_pic_list)
+    }
+
+    var loadPicUrlListener = object: MyCustomCallBack {
+        override fun fail(msg: String?, reqCode: Int) {
+            T.show(msg)
         }
-        adapter?.notifyDataSetChanged()
-        picUrl = listUrl.get(adapter!!.index)
+
+        override fun success(str: String, reqCode: Int) {
+            var json = convertArr(str)
+            if (json != null && json.size > 0) {
+                for(i in 0 until json.size) {
+                    listUrl.add(json.getString(i))
+                }
+            }
+            adapter?.index = 0
+            for (i in 0 until listUrl.size) {
+                if (listUrl.get(i) == picUrl) {
+                    adapter?.index = i
+                }
+            }
+            adapter?.notifyDataSetChanged()
+            picUrl = listUrl.get(adapter!!.index)
+        }
+    }
+
+    private fun convertArr(str: String) :JSONArray {
+        var arr = JSONArray()
+        val start = str.indexOf('[')
+        val end = str.indexOf(']')
+        val urlArray = str.substring(start, end + 1)
+        if (!TextUtils.isEmpty(urlArray)) {
+            arr = JSONArray.parseArray(urlArray)
+        }
+        return arr
     }
 
     var onItemClicked = object: SelectPicAdapter.OnItemClicked{
