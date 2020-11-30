b10d4588d13b8aa3665ffa49fb1ab02c80a17b49
sevenhhe <sevenhhe@goodow.com>
2020-11-30 18:29:40 +0800

    自动智能选择条件支持大于/等于/小于
    Change-Id: Ife264ae50dc47c8bf03ff71dd3541029db2e9700

---
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/entity/OpValue.java
@@ -0,0 +1,8 @@
+package com.tencent.iot.explorer.link.kitlink.entity;
+
+public class OpValue {
+    public static String OP_GR = "gr";
+    public static String OP_EQ = "eq";
+    public static String OP_LT = "lt";
+
+}
