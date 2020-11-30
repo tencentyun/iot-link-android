17cbfc59825d644419ede39203bee8e00849a841
archurtan <archurspace@gmail.com>
2020-11-30 15:11:34 +0800

    添加信鸽拉起页面的逻辑
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192860753847
    
    Change-Id: I36f8af726b8ddc578536ac2eda78b6e0ecd416c5

---
+++ sdk/src/main/java/com/tencent/iot/explorer/link/core/link/entity/TRTCParamsEntity.kt
@@ -0,0 +1,9 @@
+package com.tencent.iot.explorer.link.core.link.entity
+
+class TRTCParamsEntity {
+    var PrivateKey = ""
+    var UserSig = ""
+    var UserId = ""
+    var StrRoomId = ""
+    var SdkAppId = 0
+}
