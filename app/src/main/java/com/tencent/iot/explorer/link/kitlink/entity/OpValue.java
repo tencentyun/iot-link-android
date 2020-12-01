31bfcf0c190c889e1fca8a02eab3a170c4d74ea3
sevenhhe <sevenhhe@goodow.com>
2020-12-01 14:11:48 +0800

    修复大于 value 值错误的问题
    Change-Id: Ia7f7d6fe20e8f10563d4ca6447f351dca138d1a8

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/entity/OpValue.java
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/entity/OpValue.java
@@ -1,7 +1,7 @@
 package com.tencent.iot.explorer.link.kitlink.entity;
 
 public class OpValue {
-    public static String OP_GR = "gr";
+    public static String OP_GR = "gt";
     public static String OP_EQ = "eq";
     public static String OP_LT = "lt";
 
