cbd8c3e1cb83bbcabb987a074b8012f4150875aa
sevenhhe <sevenhhe@goodow.com>
2020-12-01 20:33:01 +0800

    修复进度选择对话框，支持整数小数类型的选择
    Change-Id: Icdad3266cd3b0ef066b6a3a14c1ebd30917f1065

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/entity/ModeInt.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/entity/ModeInt.kt
@@ -2,11 +2,12 @@ package com.tencent.iot.explorer.link.kitlink.entity
 
 class ModeInt {
     var type = ""
-    var min = 0
-    var max = 0
-    var start = 0
-    var step = 0
+    var min = 0.0F
+    var max = 0.0F
+    var start = 0.0F
+    var step = 0.0F
     var unit = ""
     var showOp = false
+    var ifInteger = true
     var op = OpValue.OP_EQ //  eq 等于  ne 不等于  gt 大于  lt 小于  ge 大等于  le 小等于
 }
