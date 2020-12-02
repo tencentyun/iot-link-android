e3b82e8a2246ae0d45c5689e0ab8cde4b8b21ec8
v_vweisun(孙伟) <532125505@qq.com>
2020-12-02 11:21:13 +0800

    修改被呼叫时提前进入房间的问题
    Change-Id: I29e9260c4613f41114c28dd7f8681471383459b8

---
+++ trtc/src/main/java/com/tencent/iot/explorer/trtc/model/TRTCUIManager.java
@@ -0,0 +1,39 @@
+package com.tencent.iot.explorer.trtc.model;
+
+public class TRTCUIManager {
+
+    private static TRTCUIManager instance;
+
+    private TRTCCallingParamsCallback callingParamsCallback = null;
+
+    private TRTCSessionManager sessionManager = null;
+
+    public synchronized static TRTCUIManager getInstance() {
+        if (instance == null) {
+            instance = new TRTCUIManager();
+        }
+        return instance;
+    }
+
+    public void setSessionManager(TRTCSessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    public void addCallingParamsCallback(TRTCCallingParamsCallback callingParamsCallback) {
+        this.callingParamsCallback = callingParamsCallback;
+    }
+
+    public void removeCallingParamsCallback() {
+        this.callingParamsCallback = null;
+    }
+
+    public void didAcceptJoinRoom(Integer callingType, String deviceId) {
+        sessionManager.joinRoom(callingType, deviceId);
+    }
+
+    public void joinRoom(Integer callingType, String deviceId, RoomKey roomKey) {
+        if (callingParamsCallback != null) {
+            callingParamsCallback.joinRoom(callingType, deviceId, roomKey);
+        }
+    }
+}
