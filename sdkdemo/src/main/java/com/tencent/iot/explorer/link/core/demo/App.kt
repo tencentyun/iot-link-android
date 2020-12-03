5a67e11ebb6b4a9d22e69d595032f905e8958b0c
archurtan <archurspace@gmail.com>
2020-12-03 13:15:28 +0800

    SDK Demo 增加被设备音视频呼叫功能
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192860753847
    
    Change-Id: I55f73f6655c8cf9a2a700d210f6bf6d1d6962d2f

--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/App.kt
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/App.kt
@@ -5,12 +5,33 @@ import androidx.multidex.MultiDex
 import com.tencent.iot.explorer.link.core.auth.IoTAuth
 import com.tencent.iot.explorer.link.core.auth.entity.User
 import com.tencent.iot.explorer.link.core.auth.listener.LoginExpiredListener
+import com.tencent.iot.explorer.link.core.auth.message.MessageConst
+import com.tencent.iot.explorer.link.core.auth.message.payload.Payload
+import com.tencent.iot.explorer.link.core.auth.socket.callback.PayloadMessageCallback
+import com.tencent.iot.explorer.link.core.auth.util.Weak
+import com.tencent.iot.explorer.link.core.demo.activity.BaseActivity
 import com.tencent.iot.explorer.link.core.demo.log.L
+import com.tencent.iot.explorer.trtc.model.RoomKey
+import com.tencent.iot.explorer.trtc.model.TRTCCalling
+import com.tencent.iot.explorer.trtc.model.TRTCUIManager
+import com.tencent.iot.explorer.trtc.ui.audiocall.TRTCAudioCallActivity
+import com.tencent.iot.explorer.trtc.ui.videocall.TRTCVideoCallActivity
 
-class App : Application() {
+class App : Application(), PayloadMessageCallback {
 
     companion object {
         val data = AppData.instance
+        var activity by Weak<BaseActivity>()
+        fun appStartBeingCall(callingType: Int, deviceId: String) {
+            TRTCUIManager.getInstance().setSessionManager(TRTCSdkDemoSessionManager())
+            if (callingType == TRTCCalling.TYPE_VIDEO_CALL) {
+                TRTCUIManager.getInstance().isCalling = true
+                TRTCVideoCallActivity.startBeingCall(activity, RoomKey(), deviceId)
+            } else if (callingType == TRTCCalling.TYPE_AUDIO_CALL) {
+                TRTCUIManager.getInstance().isCalling = true
+                TRTCAudioCallActivity.startBeingCall(activity, RoomKey(), deviceId)
+            }
+        }
     }
 
     private val APP_KEY = BuildConfig.TencentIotLinkSDKDemoAppkey
@@ -32,10 +53,49 @@ class App : Application() {
         })
         IoTAuth.registerSharedBugly(this) //接入共享式bugly
         MultiDex.install(this)
+        IoTAuth.addEnterRoomCallback(this)
     }
 
     override fun onTerminate() {
         IoTAuth.destroy()
         super.onTerminate()
+    }
+
+    fun startBeingCall(callingType: Int, deviceId: String) {
+        appStartBeingCall(callingType, deviceId)
+    }
+
+    override fun payloadMessage(payload: Payload) {
+        var jsonObject = org.json.JSONObject(payload.json)
+        val action = jsonObject.getString(MessageConst.MODULE_ACTION);
+        if (action == MessageConst.DEVICE_CHANGE) { //收到了设备属性改变的wss消息
+            var paramsObject = jsonObject.getJSONObject(MessageConst.PARAM) as org.json.JSONObject
+            val subType = paramsObject.getString(MessageConst.SUB_TYPE)
+            if (subType == MessageConst.REPORT) { //收到了设备端属性状态改变的wss消息
+
+                var payloadParamsObject = org.json.JSONObject(payload.payload)
+                val payloadParamsJson = payloadParamsObject.getJSONObject(MessageConst.PARAM)
+                var videoCallStatus = -1
+                if (payloadParamsJson.has(MessageConst.TRTC_VIDEO_CALL_STATUS)) {
+                    videoCallStatus = payloadParamsJson.getInt(MessageConst.TRTC_VIDEO_CALL_STATUS)
+                }
+                var audioCallStatus = -1
+                if (payloadParamsJson.has(MessageConst.TRTC_AUDIO_CALL_STATUS)) {
+                    audioCallStatus = payloadParamsJson.getInt(MessageConst.TRTC_AUDIO_CALL_STATUS)
+                }
+
+                var deviceId = ""
+                if (payloadParamsJson.has(MessageConst.USERID)) {
+                    deviceId = payloadParamsJson.getString(MessageConst.USERID)
+                }
+
+                // 判断payload中是否包含设备的video_call_status, audio_call_status字段以及是否等于1，若等于1，就调用CallDevice接口, 主动拨打
+                if (videoCallStatus == 1) {
+                    startBeingCall(2, deviceId)
+                } else if (audioCallStatus == 1) {
+                    startBeingCall(1, deviceId)
+                }
+            }
+        }
     }
 }
