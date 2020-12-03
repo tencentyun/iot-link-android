1b12ae0b8ee4b12e9da2ef9ecef19cb452ebfdad
v_vweisun(孙伟) <532125505@qq.com>
2020-12-03 17:35:29 +0800

    修改体验问题2：视频呼叫按钮和音频呼叫按钮按产品要求，仅保留音频呼叫和视频呼叫，点击直接进入通话页面。不显示枚举页面
    Change-Id: I82ba06837363eb9b55de5423fbd7c2555789c3a5

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/holder/ControlSimpleLongHolder.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/holder/ControlSimpleLongHolder.kt
@@ -1,8 +1,10 @@
 package com.tencent.iot.explorer.link.kitlink.holder
 
 import android.content.Context
+import android.view.View
 import android.view.ViewGroup
 import com.tencent.iot.explorer.link.R
+import com.tencent.iot.explorer.link.core.auth.message.MessageConst
 import com.tencent.iot.explorer.link.kitlink.entity.DevicePropertyEntity
 import com.tencent.iot.explorer.link.customview.recyclerview.CRecyclerView
 import kotlinx.android.synthetic.main.control_simple_long.view.*
@@ -18,6 +20,16 @@ class ControlSimpleLongHolder : CRecyclerView.CViewHolder<DevicePropertyEntity> 
         entity?.run {
             itemView.tv_simple_long_name.text = name
             itemView.tv_simple_long_value.text = getValueText()
+            //特殊处理，当设备为trtc设备时。虽然call_status是枚举类型，但产品要求只显示语音呼叫和视频呼叫，隐藏值和箭头，点击即拨打语音或视频通话。
+            if (id == MessageConst.TRTC_AUDIO_CALL_STATUS || id == MessageConst.TRTC_VIDEO_CALL_STATUS) {
+                itemView.tv_simple_long_value.visibility = View.GONE
+                itemView.iv_simple_long_arrow.visibility = View.GONE
+                var text = name
+                if (text.length > 2) {
+                    text = text.substring(0, text.length - 2)
+                    itemView.tv_simple_long_name.text = text
+                }
+            }
             when (id) {
                 "color" -> itemView.iv_simple_long.setImageResource(R.mipmap.icon_control_color)
                 "power_switch" -> itemView.iv_simple_long.setImageResource(R.mipmap.icon_control_switch)
