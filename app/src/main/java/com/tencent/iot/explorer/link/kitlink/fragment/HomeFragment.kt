d5c032d3f6599027d8cebeb9c21483aec1559aef
v_vweisun(孙伟) <532125505@qq.com>
2020-12-03 21:05:26 +0800

    设备注册websocket监听时机前移至首页。
    Change-Id: I72606e3721ecf0dea18a8d0aeaef92332dcb80ca

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/fragment/HomeFragment.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/fragment/HomeFragment.kt
@@ -18,6 +18,7 @@ import com.tencent.iot.explorer.link.mvp.view.HomeFragmentView
 import com.scwang.smart.refresh.layout.api.RefreshLayout
 import com.scwang.smart.refresh.layout.listener.OnRefreshLoadMoreListener
 import com.tencent.iot.explorer.link.T
+import com.tencent.iot.explorer.link.core.auth.IoTAuth
 import com.tencent.iot.explorer.link.core.auth.entity.DeviceEntity
 import com.tencent.iot.explorer.link.core.auth.entity.FamilyEntity
 import com.tencent.iot.explorer.link.core.auth.response.BaseResponse
@@ -30,6 +31,7 @@ import com.tencent.iot.explorer.link.kitlink.entity.ProdConfigDetailEntity
 import com.tencent.iot.explorer.link.kitlink.response.ProductsConfigResponse
 import com.tencent.iot.explorer.link.kitlink.util.HttpRequest
 import com.tencent.iot.explorer.link.core.auth.callback.MyCallback
+import com.tencent.iot.explorer.link.core.auth.message.upload.ArrayString
 import com.tencent.iot.explorer.link.kitlink.util.RequestCode
 import com.tencent.iot.explorer.trtc.model.RoomKey
 import com.tencent.iot.explorer.trtc.model.TRTCCalling
