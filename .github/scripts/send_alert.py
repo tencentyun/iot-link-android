857ba60960de16d3120fec3fec246318c7f3cab7
archurtan <archurspace@gmail.com>
2020-12-14 15:38:31 +0800

    ci test9
    Change-Id: I8a68f2b21d0cd19b0e07b5754684b91198889069

--- .github/scripts/send_alert.py
+++ .github/scripts/send_alert.py
@@ -15,6 +15,7 @@ logging.basicConfig(level=logging.INFO,
 
 
 def send(labels, url=None, annotations=None):
+  print("========= begin send alert =========")
   alert = {
       'generatorURL':
           url,  # 格式不对会报错, 可为 null or ''
@@ -32,28 +33,32 @@ def send(labels, url=None, annotations=None):
       'Authorization': f'Basic {os.environ["ALERTMANANGER_BASIC_AUTH"]}'
   }
   data = [alert]
-  # req = urllib.request.Request('https://alert.goodow.com/api/v2/alerts',
-  #                              data=json.dumps(data).encode('utf-8'),
-  #                              headers=headers)
-  headers['Host'] = 'alert.goodow.com'
-  req = urllib.request.Request('https://119.29.125.143:443/api/v2/alerts',
+  req = urllib.request.Request('https://alert.goodow.com/api/v2/alerts',
                                data=json.dumps(data).encode('utf-8'),
                                headers=headers)
-  ctx = ssl.create_default_context()
-  ctx.check_hostname = False
-  ctx.verify_mode = ssl.CERT_NONE
-  resp = urllib.request.urlopen(req, context=ctx)
-  # try:
-  #   resp = urllib.request.urlopen(req)
-  # except URLError as e:
-  #   logging.info(f'retry, {type(e.reason)}({e.reason})')
-  #   # [Errno 104] Connection reset by peer
-  #   # [Errno 110] Connection timed out
-  #   # if isinstance(e.reason, ConnectionResetError):
+  try:
+    resp = urllib.request.urlopen(req)
+    print('========= urlopen 1')
+  except URLError as e:
+    logging.info(f'retry, {type(e.reason)}({e.reason})')
+    # [Errno 104] Connection reset by peer
+    # [Errno 110] Connection timed out
+    # if isinstance(e.reason, ConnectionResetError):
+    headers['Host'] = 'alert.goodow.com'
+    req = urllib.request.Request('https://119.29.125.143:443/api/v2/alerts',
+                                 data=json.dumps(data).encode('utf-8'),
+                                 headers=headers)
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    resp = urllib.request.urlopen(req, context=ctx)
+    print('========= urlopen 2')
 
   if resp.status != 200:
     print('发送 Alert 出错', resp.status, resp.reason)
     raise Exception(f'{resp.status} {resp.reason}')
+  print("========= end send alert =========")
+
 
 
 def short_sha():
@@ -77,5 +82,6 @@ if __name__ == '__main__':
   head_commit = github['event']['head_commit']
   url = f'{head_commit["url"]}/checks'
   annotations = {'commit': head_commit['message'], 'receivers': '25088358528,ArchurSpace'}
-
+  print('========= start ===========')
   send(labels, url=url, annotations=annotations)
+  print('========= stop ===========')
