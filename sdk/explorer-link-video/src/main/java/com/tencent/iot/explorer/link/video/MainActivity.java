99fbdd24700598043ffc2471be3f319ec7f82b10
archurtan <archurspace@gmail.com>
2020-12-15 19:34:15 +0800

    微信演示demo
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192861563823
    
    Change-Id: Ibe7f50cbc3dbcd827fa5bd1beeb458223bb75015

---
+++ sdk/explorer-link-video/src/main/java/com/tencent/iot/explorer/link/video/MainActivity.java
@@ -0,0 +1,96 @@
+package com.tencent.iot.explorer.link.video;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SurfaceHolder.Callback{
+
+    private MediaPlayer mPlayer = null;
+    private SurfaceView sfv_show;
+    private SurfaceHolder surfaceHolder;
+    private Button btn_start;
+    private Button btn_pause;
+    private Button btn_stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindViews();
+
+        mPlayer = new MediaPlayer();
+
+        try {
+            mPlayer.setDataSource("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+    }
+
+    private void bindViews() {
+        sfv_show = (SurfaceView) findViewById(R.id.sfv_show);
+        btn_start = (Button) findViewById(R.id.btn_start);
+        btn_pause = (Button) findViewById(R.id.btn_pause);
+        btn_stop = (Button) findViewById(R.id.btn_stop);
+
+        btn_start.setOnClickListener(this);
+        btn_pause.setOnClickListener(this);
+        btn_stop.setOnClickListener(this);
+
+        //初始化SurfaceHolder类，SurfaceView的控制器
+        surfaceHolder = sfv_show.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setFixedSize(320, 220);   //显示的分辨率,不设置为视频默认
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_start:
+                mPlayer.start();
+                break;
+            case R.id.btn_pause:
+                mPlayer.pause();
+                break;
+            case R.id.btn_stop:
+                mPlayer.stop();
+                break;
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mPlayer.setDisplay(holder);
+        mPlayer.prepareAsync();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {}
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mPlayer.isPlaying()) {
+            mPlayer.stop();
+        }
+        mPlayer.release();
+    }
+}
