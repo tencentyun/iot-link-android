47116434bb7018cf260d75ec102919f61aac4add
archurtan <archurspace@gmail.com>
2020-12-15 11:56:35 +0800

    重构日志类、调整日志级别、规范存量代码日志类的使用
    Change-Id: I7eb28dc6f03c86dc73e7b92ccc511db85d892046

--- sdk/explorer-link-android/src/main/java/com/tencent/iot/explorer/link/core/log/L.kt
+++ sdk/explorer-link-android/src/main/java/com/tencent/iot/explorer/link/core/log/L.kt
@@ -4,38 +4,49 @@ import android.util.Log
 
 object L {
 
-    var isLog:Boolean = false
-
     private const val DEFAULT_TAG = "TenextIoT"
 
-    fun e(msg: String) {
-        e(DEFAULT_TAG, msg)
-    }
+    var isLog:Boolean = false
 
-    fun e(tag: String, msg: String) {
-        if (isLog) {
-            Log.e(tag, msg)
+    var LOG_LEVEL     = 5 // 允许输出log的级别（0：不输出， 5：输出所有日志）
+    var LEVEL_VERBOSE = 1 // 日志输出级别 V
+    var LEVEL_DEBUG   = 2 // 日志输出级别 D
+    var LEVEL_INFO    = 3 // 日志输出级别 I
+    var LEVEL_WARN    = 4 // 日志输出级别 W
+    var LEVEL_ERROR   = 5 // 日志输出级别 E
+
+    fun v(msg: String) { v(DEFAULT_TAG, msg) }
+    fun v(tag: String, msg: String) {
+        if (isLog && LEVEL_VERBOSE >= LOG_LEVEL) {
+            Log.v(tag, msg)
         }
     }
 
-    fun d(msg: String) {
-        d(DEFAULT_TAG, msg)
-    }
-
+    fun d(msg: String) { d(DEFAULT_TAG, msg) }
     fun d(tag: String, msg: String) {
-        if (isLog) {
+        if (isLog && LEVEL_DEBUG >= LOG_LEVEL) {
             Log.d(tag, msg)
         }
     }
 
-    fun i(msg: String) {
-        i(DEFAULT_TAG, msg)
-    }
-
+    fun i(msg: String) { i(DEFAULT_TAG, msg) }
     fun i(tag: String, msg: String) {
-        if (isLog) {
+        if (isLog && LEVEL_INFO  >= LOG_LEVEL) {
             Log.i(tag, msg)
         }
     }
 
+    fun w(msg: String) { i(DEFAULT_TAG, msg) }
+    fun w(tag: String, msg: String) {
+        if (isLog && LEVEL_WARN  >= LOG_LEVEL) {
+            Log.w(tag, msg)
+        }
+    }
+
+    fun e(msg: String) { e(DEFAULT_TAG, msg) }
+    fun e(tag: String, msg: String) {
+        if (isLog && LEVEL_ERROR >= LOG_LEVEL) {
+            Log.e(tag, msg)
+        }
+    }
 }
