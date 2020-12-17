3cd1eee5ceeebe04ef1db3a341c2cbbd700fd919
sevenhhe <sevenhhe@goodow.com>
2020-12-17 18:57:51 +0800

    Video 扫码配网 sdk + demo
    https://cloud.tencent.com/document/product/1131/42246
    
    Change-Id: Id9cfe1c2bd75353f3bf6f55842bce7f50038e54b

---
+++ sdk/explorer-link-android/src/main/java/com/tencent/iot/explorer/link/core/link/configNetwork/QrcodeConfig.kt
@@ -0,0 +1,10 @@
+package com.tencent.iot.explorer.link.core.link.configNetwork
+
+class QrcodeConfig {
+    var ssid = ""
+    var bssid = ""
+    var wifiPwd = ""
+    var token = ""
+    var width = 200  // 默认位图宽度
+    var height = 200  // 默认位图高度
+}
