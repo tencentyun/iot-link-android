aeaed7848d7092dd1ffe5bfe34f5e34b05fb56ea
v_vweisun(孙伟) <532125505@qq.com>
2020-12-18 00:22:28 +0800

    TRTC V1.1 边界问题和异常处理优化-Case1：用户1和用户2（不同账号）同时呼叫设备A。检查设备callstatus状态，不为0，则提示用户对方正忙... 边界问题和异常处理优化-Case2：用户1和用户2（相同账号）同时呼叫设备A。查看websocket中收到的_sys_userid不为自己的userid则被其他用户抢先呼叫设备了，提示用户 对方正忙...
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192860753847
    
    Change-Id: Ice4fc9a0e066437278af4b14532625e5ea4b5241

--- sdk/explorer-link-trtc/src/main/java/com/tencent/iot/explorer/trtc/model/TRTCCallingParamsCallback.java
+++ sdk/explorer-link-trtc/src/main/java/com/tencent/iot/explorer/trtc/model/TRTCCallingParamsCallback.java
@@ -3,4 +3,6 @@ package com.tencent.iot.explorer.trtc.model;
 public interface TRTCCallingParamsCallback {
 
     void joinRoom(Integer callingType, String deviceId, RoomKey roomKey);
+
+    void exitRoom();
 }
