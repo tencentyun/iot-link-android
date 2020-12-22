24a0c54e5130d6301e0b9f16e15cfbb263fc57df
v_vweisun(孙伟) <532125505@qq.com>
2020-12-22 10:19:02 +0800

    TRTC V1.1 应用端 边界问题和异常处理优化-Case4&Case5：自己已进入房间15秒内对方没有进入房间 则显示对方已挂断，并主动退出。呼叫了60秒，对方未接听 显示对方无人接听，并退出。
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192860753847
    
    Change-Id: I0d3143393a0b3c627bc76e0b97109c133264e3b6

--- sdk/explorer-link-rtc/src/main/java/com/tencent/iot/explorer/trtc/ui/videocall/TRTCVideoCallActivity.java
+++ sdk/explorer-link-rtc/src/main/java/com/tencent/iot/explorer/trtc/ui/videocall/TRTCVideoCallActivity.java
@@ -13,6 +13,7 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -38,6 +39,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * 用于展示视频通话的主界面，通话的接听和拒绝就是在这个界面中完成的。
@@ -89,6 +92,9 @@ public class TRTCVideoCallActivity extends AppCompatActivity {
     private boolean               isHandsFree       = true;
     private boolean               isMuteMic         = false;
 
+    private TimerTask otherEnterRoomTask = null;
+    private TimerTask enterRoomTask = null;
+
     /**
      * 拨号的回调
      */
@@ -114,6 +120,7 @@ public class TRTCVideoCallActivity extends AppCompatActivity {
                 @Override
                 public void run() {
                     showCallingView();
+                    TRTCUIManager.getInstance().otherEnterRoom = true;
                     //1.先造一个虚拟的用户添加到屏幕上
                     UserInfo model = new UserInfo();
                     model.setUserId(userId);
@@ -297,6 +304,42 @@ public class TRTCVideoCallActivity extends AppCompatActivity {
         context.startActivity(starter);
     }
 
+    private void checkoutOtherIsEnterRoom15seconds() {
+        otherEnterRoomTask = new TimerTask(){
+            public void run(){
+                if (!TRTCUIManager.getInstance().otherEnterRoom) { //自己已进入房间15秒内对方没有进入房间 则显示对方已挂断，并主动退出
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "对方已挂断", Toast.LENGTH_LONG).show();
+                            stopCameraAndFinish();
+                        }
+                    });
+                }
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(otherEnterRoomTask, 15000);
+    }
+
+    private void checkoutIsEnterRoom60seconds() {
+        enterRoomTask = new TimerTask(){
+            public void run(){
+                if (!TRTCUIManager.getInstance().enterRoom) { //呼叫了60秒，对方未接听 显示对方无人接听，并退出
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "对方无人接听", Toast.LENGTH_LONG).show();
+                            stopCameraAndFinish();
+                        }
+                    });
+                }
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(enterRoomTask, 60000);
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -313,7 +356,11 @@ public class TRTCVideoCallActivity extends AppCompatActivity {
 //                mTRTCCalling.accept();
 //                mTRTCCalling.enterTRTCRoom();
                 startInviting(roomKey);
+                if (roomKey != null) {
+                    TRTCUIManager.getInstance().enterRoom = true;
+                }
                 showCallingView();
+                checkoutOtherIsEnterRoom15seconds();
             }
 
             @Override
@@ -325,6 +372,7 @@ public class TRTCVideoCallActivity extends AppCompatActivity {
         initView();
         initData();
         initListener();
+        checkoutIsEnterRoom60seconds();
     }
 
     @Override
@@ -341,8 +389,18 @@ public class TRTCVideoCallActivity extends AppCompatActivity {
 //        mTRTCCalling.removeDelegate(mTRTCCallingDelegate);
         finish();
         TRTCUIManager.getInstance().isCalling = false;
+        TRTCUIManager.getInstance().enterRoom = false;
+        TRTCUIManager.getInstance().otherEnterRoom = false;
         TRTCUIManager.getInstance().deviceId = "";
         TRTCUIManager.getInstance().removeCallingParamsCallback();
+        if (otherEnterRoomTask != null) {
+            otherEnterRoomTask.cancel();
+            otherEnterRoomTask = null;
+        }
+        if (enterRoomTask != null) {
+            enterRoomTask.cancel();
+            enterRoomTask = null;
+        }
     }
 
     @Override
