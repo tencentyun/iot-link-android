caada2567ba32858753ae6dab57622408c4cc7cc
v_vweisun(孙伟) <532125505@qq.com>
2021-01-07 12:03:24 +0800

    Video V2.0 摄像头列表UI搭建，列表GetDeviceList接口调试，添加鉴权输入信息页面。
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192861722531
    
    Change-Id: Ibdf59f98005a0a298e8d2b4fb8fc8d6cc662c70e

---
+++ sdk/video-link-android/src/main/java/com/tencent/iot/video/link/consts/VideoRequestCode.kt
@@ -0,0 +1,9 @@
+package com.tencent.iot.video.link.consts
+
+object VideoRequestCode {
+
+    /*************video接口开始**************/
+    const val video_describe_devices = 20000
+
+    /*************video接口结束**************/
+}
