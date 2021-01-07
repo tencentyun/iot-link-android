681cc734726734c378e1cd004e54cf9e9bc821c6
v_vweisun(孙伟) <532125505@qq.com>
2021-01-07 12:03:24 +0800

    Video V2.0 摄像头列表UI搭建，列表GetDeviceList接口调试，添加鉴权输入信息页面。
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192861722531
    
    Change-Id: Ibdf59f98005a0a298e8d2b4fb8fc8d6cc662c70e

---
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/activity/InputAuthorizeActivity.kt
@@ -0,0 +1,70 @@
+package com.tencent.iot.explorer.link.core.demo.activity
+
+import android.widget.Toast
+import com.tencent.iot.explorer.link.core.auth.message.MessageConst
+import com.tencent.iot.explorer.link.core.demo.R
+import com.tencent.iot.explorer.link.core.utils.SharePreferenceUtil
+import com.tencent.iot.video.link.consts.VideoConst
+import kotlinx.android.synthetic.main.activity_input_authorize.*
+
+class InputAuthorizeActivity : BaseActivity() {
+
+    override fun getContentView(): Int {
+        return R.layout.activity_input_authorize
+    }
+
+    override fun initView() {
+        val secretId = SharePreferenceUtil.getString(this, VideoConst.VIDEO_CONFIG, VideoConst.VIDEO_SECRET_ID)
+        val secretKey = SharePreferenceUtil.getString(this, VideoConst.VIDEO_CONFIG, VideoConst.VIDEO_SECRET_KEY)
+        val productId = SharePreferenceUtil.getString(this, VideoConst.VIDEO_CONFIG, VideoConst.VIDEO_PRODUCT_ID)
+        if (secretId != null) {
+            et_secretid.setText(secretId)
+        }
+        if (secretKey != null) {
+            et_secretkey.setText(secretKey)
+        }
+        if (productId != null) {
+            et_productid.setText(productId)
+        }
+    }
+
+    override fun setListener() {
+        btn_1_vedio.setOnClickListener {
+            if (checkInput()) {
+                jumpActivity(VideoMessageActivity::class.java)
+            }
+        }
+
+    }
+
+    private fun checkInput(): Boolean {
+        val inputSecretId: String = et_secretid.text.toString()
+        if (inputSecretId == "") {
+            val toast =
+                Toast.makeText(applicationContext, "请输入SecretId", Toast.LENGTH_LONG)
+            toast.show()
+            return false
+        }
+        val inputSecretKey: String = et_secretkey.text.toString()
+        if (inputSecretKey == "") {
+            val toast =
+                Toast.makeText(applicationContext, "请输入SecretKey", Toast.LENGTH_LONG)
+            toast.show()
+            return false
+        }
+        val inputProductId: String = et_productid.text.toString()
+        if (inputProductId == "") {
+            val toast =
+                Toast.makeText(applicationContext, "请输入productId", Toast.LENGTH_LONG)
+            toast.show()
+            return false
+        }
+
+        SharePreferenceUtil.saveString(this, VideoConst.VIDEO_CONFIG, VideoConst.VIDEO_SECRET_ID, inputSecretId)
+        SharePreferenceUtil.saveString(this, VideoConst.VIDEO_CONFIG, VideoConst.VIDEO_SECRET_KEY, inputSecretKey)
+        SharePreferenceUtil.saveString(this, VideoConst.VIDEO_CONFIG, VideoConst.VIDEO_PRODUCT_ID, inputProductId)
+
+        return true
+    }
+
+}
