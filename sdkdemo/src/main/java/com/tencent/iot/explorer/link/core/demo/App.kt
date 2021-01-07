0d3a5d29459b295e5a6a1fd071c771ecc6bfaf17
v_vweisun(孙伟) <532125505@qq.com>
2021-01-07 12:03:24 +0800

    Video V2.0 摄像头列表UI搭建，列表鉴权接口调试。
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192861722531
    
    Change-Id: Ibdf59f98005a0a298e8d2b4fb8fc8d6cc662c70e

--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/App.kt
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/App.kt
@@ -57,7 +57,7 @@ class App : Application(), PayloadMessageCallback {
          * 此处仅供参考, 需自建服务接入物联网平台服务，以免 App Secret 泄露
          * 自建服务可参考此处 https://cloud.tencent.com/document/product/1081/45901#.E6.90.AD.E5.BB.BA.E5.90.8E.E5.8F.B0.E6.9C.8D.E5.8A.A1.2C-.E5.B0.86-app-api-.E8.B0.83.E7.94.A8.E7.94.B1.E8.AE.BE.E5.A4.87.E7.AB.AF.E5.8F.91.E8.B5.B7.E5.88.87.E6.8D.A2.E4.B8.BA.E7.94.B1.E8.87.AA.E5.BB.BA.E5.90.8E.E5.8F.B0.E6.9C.8D.E5.8A.A1.E5.8F.91.E8.B5.B7
          */
-        IoTAuth.init(APP_KEY, APP_SECRET)
+        IoTAuth.init(APP_KEY, APP_SECRET, "", "")
         IoTAuth.addLoginExpiredListener(object : LoginExpiredListener {
             override fun expired(user: User) {
                 L.d("用户登录过期")
---
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/entity/VideoMessageEntity.kt
@@ -0,0 +1,14 @@
+package com.tencent.iot.explorer.link.core.demo.entity
+
+/**
+ * video事件实体
+ */
+class VideoMessageEntity {
+
+    var VideoMessageId = ""
+    var CreateTime = 0L
+    var VideoTitle = ""
+    var VideoTip = ""
+    var VideoImageUrl = ""
+
+}
