17fd73f3db11222350e078781a59d0a26f6f742f
v_vweisun(孙伟) <532125505@qq.com>
2021-01-12 19:38:29 +0800

    Video V2.0 摄像头列表item上的按钮对应的点击响应。
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192861722531
    
    Change-Id: If404eebc3d0c2fdef331bf7a91b40641c7976322

--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/adapter/VideoMessageAdapter.kt
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/adapter/VideoMessageAdapter.kt
@@ -1,15 +1,49 @@
 package com.tencent.iot.explorer.link.core.demo.adapter
 
 import android.content.Context
+import android.view.View
 import android.view.ViewGroup
-import com.tencent.iot.explorer.link.core.demo.entity.VideoMessageEntity
 import com.tencent.iot.explorer.link.core.demo.R
+import com.tencent.iot.explorer.link.core.demo.entity.VideoMessageEntity
 import com.tencent.iot.explorer.link.core.demo.holder.BaseHolder
 import com.tencent.iot.explorer.link.core.demo.holder.VideoMessageHolder
 
 class VideoMessageAdapter(context: Context, list: List<VideoMessageEntity>) : BaseAdapter(context, list) {
 
+    private var mButtonInterface: ButtonInterface? = null
+
     override fun createHolder(parent: ViewGroup, viewType: Int): BaseHolder<*> {
         return VideoMessageHolder(mContext, parent, R.layout.item_video_message)
     }
+
+    /**
+     * 按钮点击事件需要的方法
+     */
+    fun buttonSetOnclick(buttonInterface: ButtonInterface) {
+        mButtonInterface = buttonInterface
+    }
+
+    /**
+     * 调用点击
+     */
+    fun onClickButton(holder: BaseHolder<*>, clickView: View, position: Int, tag: Int) {
+        when (tag) {
+            0 -> {
+                mButtonInterface?.onRealtimeMonitorButtonClick(holder, clickView, position)
+            }
+            1 -> {
+                mButtonInterface?.onLocalPlaybackButtonClick(holder, clickView, position)
+            }
+            2 -> {
+                mButtonInterface?.onCloudSaveButtonClick(holder, clickView, position)
+            }
+        }
+    }
+
+}
+
+interface ButtonInterface {
+    fun onRealtimeMonitorButtonClick(holder: BaseHolder<*>, clickView: View, position: Int)
+    fun onLocalPlaybackButtonClick(holder: BaseHolder<*>, clickView: View, position: Int)
+    fun onCloudSaveButtonClick(holder: BaseHolder<*>, clickView: View, position: Int)
 }
