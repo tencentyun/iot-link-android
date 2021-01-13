3c02f0925af83f4d1e6800409db3222790c3d8a9
archurtan <archurspace@gmail.com>
2021-01-13 20:03:16 +0800

    将实时监控和本地回放与现有入口代码整合，同时优化语音对讲的代码逻辑
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192861471117?url_cache_key=e79c8d589e6ce8f8fef299933dcc92fc&action_entry_type=story_tree_list
    
    Change-Id: I442018f0063006c23df9598a4a430b0dcf10d30d

--- sdk/video-link-android/src/main/cpp/include/httpProxy.h
+++ sdk/video-link-android/src/main/cpp/include/httpProxy.h
@@ -6,8 +6,12 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <string>
 
+__attribute__ ((visibility ("default")))
 int proxy_init(std::string peername, uint16_t *port);
+
+__attribute__ ((visibility ("default")))
 void proxy_deinit();
 
 //#ifdef __cplusplus
