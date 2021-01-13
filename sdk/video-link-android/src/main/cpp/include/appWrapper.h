ee03a36563fd043b75710e518190f22f1f321d40
archurtan <archurspace@gmail.com>
2021-01-13 20:03:16 +0800

    将实时监控和本地回放与现有入口代码整合，同时优化语音对讲的代码逻辑
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192861471117?url_cache_key=e79c8d589e6ce8f8fef299933dcc92fc&action_entry_type=story_tree_list
    
    Change-Id: I442018f0063006c23df9598a4a430b0dcf10d30d

--- sdk/video-link-android/src/main/cpp/include/appWrapper.h
+++ sdk/video-link-android/src/main/cpp/include/appWrapper.h
@@ -3,13 +3,37 @@
 //#ifdef __cplusplus
 //extern "C" {
 //#endif
-#define _GLIBCXX_USE_CXX11_ABI 0
 
 #include <stddef.h>
 #include <stdint.h>
+#include <string>
 
-int startServiceWithPeername(std::string peername);
+__attribute__ ((visibility ("default")))
+int startServiceWithXp2pInfo(std::string peername);
+
+__attribute__ ((visibility ("default")))
 std::string delegateHttpFlv();
+
+__attribute__ ((visibility ("default")))
+void *runSendService();
+
+__attribute__ ((visibility ("default")))
+//int stopSendService();
+int stopSendService(void *req);
+
+__attribute__ ((visibility ("default")))
+int setQcloudApiCred(const char *sec_id, const char *sec_key);
+
+__attribute__ ((visibility ("default")))
+int setDeviceInfo(const char *pro_id, const char *dev_name);
+
+__attribute__ ((visibility ("default")))
+int setXp2pInfoAttributes(const char *attr);
+
+__attribute__ ((visibility ("default")))
+int dataSend(uint8_t *data, size_t len);
+
+__attribute__ ((visibility ("default")))
 void stopService();
 
 //#ifdef __cplusplus
--- sdk/video-link-android/src/main/cpp/native-lib.cpp
+++ sdk/video-link-android/src/main/cpp/native-lib.cpp
@@ -12,17 +12,72 @@
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_tencent_iot_video_link_XP2P_startServiceWithPeername(JNIEnv *env, jobject thiz,
+Java_com_tencent_iot_video_link_XP2P_startServiceWithXp2pInfo(JNIEnv *env, jclass clazz,
                                                               jstring peername) {
     const char *pname = env->GetStringUTFChars(peername, 0);
-    LOGI("------------ startServiceWithPeername start------------\n");
-    startServiceWithPeername(pname);
-    LOGI("------------ startServiceWithPeername end------------");
+    LOGI("------------ startServiceWithXp2pInfo Start------------\n");
+    startServiceWithXp2pInfo(pname);
+    LOGI("------------ startServiceWithXp2pInfo End------------");
 }
 
 extern "C"
 JNIEXPORT jstring JNICALL
-Java_com_tencent_iot_video_link_XP2P_delegateHttpFlv(JNIEnv *env, jobject thiz) {
-    LOGI("------------ delegateHttpFlv start------------\n");
+Java_com_tencent_iot_video_link_XP2P_delegateHttpFlv(JNIEnv *env, jclass clazz) {
+    LOGI("------------ DelegateHttpFlv ------------\n");
     return env->NewStringUTF(delegateHttpFlv().c_str());
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_tencent_iot_video_link_XP2P_setDeviceInfo(JNIEnv *env, jclass clazz, jstring pro_id, jstring dev_name) {
+    const char *id = env->GetStringUTFChars(pro_id, 0);
+    const char *name = env->GetStringUTFChars(dev_name, 0);
+    int rc = setDeviceInfo(id, name);
+    LOGI("------------ SetDeviceInfo End:%d------------", rc);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_tencent_iot_video_link_XP2P_setQcloudApiCred(JNIEnv *env, jclass clazz, jstring api_id, jstring api_key) {
+    const char *id = env->GetStringUTFChars(api_id, 0);
+    const char *key = env->GetStringUTFChars(api_key, 0);
+    int rc = setQcloudApiCred(id, key);
+    LOGI("------------ SetQcloudApiCred End:%d------------", rc);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_tencent_iot_video_link_XP2P_runSendService(JNIEnv *env, jclass clazz) {
+    runSendService();
+    LOGI("------------ RunSendService End------------");
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_tencent_iot_video_link_XP2P_dataSend(JNIEnv *env, jclass clazz, jbyteArray data, jint len) {
+    jbyte* dataPtr = env->GetByteArrayElements(data, 0);
+    int rc = dataSend((uint8_t *)dataPtr, (size_t)len);
+    LOGI("------------ DataSend End:%d------------", rc);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_tencent_iot_video_link_XP2P_stopService(JNIEnv *env, jclass clazz) {
+    stopService();
+    LOGI("------------ StopService End------------");
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_tencent_iot_video_link_XP2P_setXp2pInfoAttributes(JNIEnv *env, jclass clazz, jstring attributes) {
+    const char *_attributes = env->GetStringUTFChars(attributes, 0);
+    setXp2pInfoAttributes(_attributes);
+    LOGI("------------ SetXp2pInfoAttributes End------------");
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_tencent_iot_video_link_XP2P_stopSendService(JNIEnv *env, jclass clazz, jbyteArray data) {
+    stopSendService(nullptr);
+    LOGI("------------ StopSendService End------------");
 }
