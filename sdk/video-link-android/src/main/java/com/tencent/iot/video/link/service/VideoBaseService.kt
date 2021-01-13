423db67d4e9a87b207f5bf676d6dae78a396be84
v_vweisun(孙伟) <532125505@qq.com>
2021-01-13 16:29:57 +0800

    Video V2.0 区分video和explorer和鉴权有关的接口修改。
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192861722531
    
    Change-Id: I07116eb2bc32b66132249f344cb19feccc200faf

--- sdk/video-link-android/src/main/java/com/tencent/iot/video/link/service/VideoBaseService.kt
+++ sdk/video-link-android/src/main/java/com/tencent/iot/video/link/service/VideoBaseService.kt
@@ -34,25 +34,44 @@ open class VideoBaseService(secretId: String, secretKey: String) {
     }
 
     /**
-     * 获取设备信息列表   DescribeDevices
+     * explorer获取设备信息列表   GetDeviceList
      */
-    fun describeDevices(
+    fun getDeviceList(
         productId: String, callback: VideoCallback
     ) {
-        var headerParams = commonHeaderParams("GetDeviceList")
+        var headerParams = explorerCommonHeaderParams("GetDeviceList")
         val param = TreeMap<String, Any>()
         param["ProductId"] = productId
-        val authorization = sign(headerParams, param)
+        val authorization = sign(VideoHttpUtil.EXPLORER_SERVICE, headerParams, param)
         if (authorization != null) {
             headerParams["Authorization"] = authorization
         }
-        basePost(param, headerParams, callback, VideoRequestCode.video_describe_devices)
+        basePost(VideoHttpUtil.EXPLORER_SERVICE+VideoHttpUtil.REST_HOST_URL, param, headerParams, callback, VideoRequestCode.video_describe_devices)
     }
 
     /**
-     * 未登录接口公共参数
+     * video获取设备信息列表   DescribeDevices
      */
-    fun commonHeaderParams(action: String): HashMap<String, String> {
+    fun describeDevices(
+        productId: String, returnModel: Boolean, limit: Int, offset: Int, callback: VideoCallback
+    ) {
+        var headerParams = videoCommonHeaderParams("DescribeDevices")
+        val param = TreeMap<String, Any>()
+        param["Limit"] = limit
+        param["Offset"] = offset
+        param["ProductId"] = productId
+        param["ReturnModel"] = returnModel
+        val authorization = sign(VideoHttpUtil.VIDEO_SERVICE, headerParams, param)
+        if (authorization != null) {
+            headerParams["Authorization"] = authorization
+        }
+        basePost(VideoHttpUtil.VIDEO_SERVICE+VideoHttpUtil.REST_HOST_URL, param, headerParams, callback, VideoRequestCode.video_describe_devices)
+    }
+
+    /**
+     * explorer header接口公共参数
+     */
+    fun explorerCommonHeaderParams(action: String): HashMap<String, String> {
         val param = HashMap<String, String>()
         param["X-TC-Action"] = action
         param["X-TC-Version"] = "2019-04-23"
@@ -61,11 +80,22 @@ open class VideoBaseService(secretId: String, secretKey: String) {
         return param
     }
 
+    /**
+     * video header接口公共参数
+     */
+    fun videoCommonHeaderParams(action: String): HashMap<String, String> {
+        val param = HashMap<String, String>()
+        param["X-TC-Action"] = action
+        param["X-TC-Version"] = "2019-11-26"
+        param["X-TC-Timestamp"] = (System.currentTimeMillis() / 1000).toString()
+        return param
+    }
+
     open fun sign(
+        service: String,
         headers: Map<String, String>,
         param: TreeMap<String, Any>
     ): String? {
-        val service: String = "iotexplorer"
         val algorithm = "TC3-HMAC-SHA256"
         val timestamp: String = headers?.get("X-TC-Timestamp") as String
         val sdf = SimpleDateFormat("yyyy-MM-dd")
@@ -78,7 +108,7 @@ open class VideoBaseService(secretId: String, secretKey: String) {
         val httpRequestMethod = "POST"
         val canonicalUri = "/"
         val canonicalQueryString = ""
-        val canonicalHeadersBuilder = "content-type:application/json; charset=utf-8\nhost:${VideoHttpUtil.VIDEO_API}\n"
+        val canonicalHeadersBuilder = "content-type:application/json; charset=utf-8\nhost:${service}${VideoHttpUtil.REST_HOST_URL}\n"
         val signedHeadersBuilder = "content-type;host"
         val canonicalHeaders = canonicalHeadersBuilder
         val signedHeaders = signedHeadersBuilder.toLowerCase()
@@ -167,8 +197,8 @@ open class VideoBaseService(secretId: String, secretKey: String) {
     /**
      * base请求
      */
-    fun basePost(param: TreeMap<String, Any>, headerParams: Map<String, Any>, callback: VideoCallback, reqCode: Int) {
-        VideoHttpUtil.post(param, headerParams, object :
+    fun basePost(url: String, param: TreeMap<String, Any>, headerParams: Map<String, Any>, callback: VideoCallback, reqCode: Int) {
+        VideoHttpUtil.post(url, param, headerParams, object :
             HttpCallBack {
             override fun onSuccess(response: String) {
                 Log.d("响应${headerParams["X-TC-Action"]}", response)
