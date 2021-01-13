68f57da206357d84d25a0ac327431f1d0612d8df
archurtan <archurspace@gmail.com>
2021-01-13 20:03:16 +0800

    将实时监控和本地回放与现有入口代码整合，同时优化语音对讲的代码逻辑
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192861471117?url_cache_key=e79c8d589e6ce8f8fef299933dcc92fc&action_entry_type=story_tree_list
    
    Change-Id: I442018f0063006c23df9598a4a430b0dcf10d30d

--- sdk/video-link-android/src/main/java/com/tencent/iot/video/link/XP2P.java
+++ sdk/video-link-android/src/main/java/com/tencent/iot/video/link/XP2P.java
@@ -6,14 +6,15 @@ public class XP2P {
         System.loadLibrary("native-lib");
     }
 
-    public void openP2PChannel(String peername) {
-        startServiceWithPeername(peername);
-    }
+    public static native void startServiceWithXp2pInfo(String peername);
+    public static native void stopService();
+    public static native String delegateHttpFlv();
+    public static native void setDeviceInfo(String id, String name);
+    public static native void setQcloudApiCred(String id, String key);
+    public static native void runSendService();
+    public static native void stopSendService(byte[] data);
+    public static native void dataSend(byte[] data, int len);
+    public static native void setXp2pInfoAttributes(String attributes);
 
-    public String getHttpFlvUrl() {
-        return delegateHttpFlv();
-    }
 
-    public native void startServiceWithPeername(String peername);
-    public native String delegateHttpFlv();
 }
