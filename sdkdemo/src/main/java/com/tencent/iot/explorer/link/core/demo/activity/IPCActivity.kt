89df59767c9e69a812b3165fe85a50db52a2ea5d
heyawei <379135467@qq.com>
2021-01-15 12:53:27 +0800

    Video（云存储，后台接口联调）
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192861722531
    
    Change-Id: Ib4be5fa6ebd7c46cec17e020bf6b02f91101fe59

--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/activity/IPCActivity.kt
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/activity/IPCActivity.kt
@@ -1,35 +1,21 @@
 package com.tencent.iot.explorer.link.core.demo.activity
 
-import android.content.Intent
 import android.net.Uri
-import android.text.TextUtils
 import android.widget.MediaController
 import com.tencent.iot.explorer.link.core.demo.R
 import kotlinx.android.synthetic.main.activity_ipc.*
 
 class IPCActivity : BaseActivity() {
-    var url = ""
 
     override fun getContentView(): Int {
         return R.layout.activity_ipc
     }
 
     override fun initView() {
-        url = intent.getStringExtra(URL)
-        if (!TextUtils.isEmpty(url)) {
-//            url = "http://devimages.apple.com/iphone/samples/bipbop/gear1/prog_index.m3u8"
-            playVideo(url)
-        }
-
-        palyback_info.setText("xxxxxxxxxxx")
     }
 
     override fun setListener() {
-        test_btn.setOnClickListener {
-            var intent = Intent(this@IPCActivity, DateIPCActivity::class.java)
-            intent.putExtra(IPCActivity.URL, "")
-            startActivity(intent)
-        }
+        test_btn.setOnClickListener {}
     }
 
     private fun playVideo(url: String) {
@@ -42,10 +28,6 @@ class IPCActivity : BaseActivity() {
 
     private fun stopPlay() {
         vedio_player.stopPlayback()
-    }
-
-    companion object {
-        const val URL = "extraUrl"
     }
 
 }
