dd26af7f4c8dfe8d41b5a377b4a4da0886b45e1b
archurtan <archurspace@gmail.com>
2021-01-20 17:02:12 +0800

    统一xp2p日志tag
    Change-Id: I0fc268eb7d8f631c1646b43934915b30d4b39321

--- sdk/video-link-android/src/main/cpp/native-lib.cpp
+++ sdk/video-link-android/src/main/cpp/native-lib.cpp
@@ -4,7 +4,7 @@
 #include "httpProxy.h"
 
 #include  <android/log.h>
-#define  TAG    "ARCHURLOG"
+#define  TAG    "XP2P-LOG-TAG"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG,__VA_ARGS__)
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG,__VA_ARGS__)
