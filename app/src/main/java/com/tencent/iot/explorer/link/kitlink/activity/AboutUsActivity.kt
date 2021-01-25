23455a3e1eaabc3d773a9201e9078999e53d3911
v_vweisun(孙伟) <532125505@qq.com>
2021-01-25 09:37:41 +0800

    【ID861493671】新增开源信息展示入口
    http://tapd.oa.com/20393192/prong/stories/view/1020393192861493671
    
    Change-Id: Id6d232718a24d268a170a561ba685ec5aef6a32c

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/activity/AboutUsActivity.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/activity/AboutUsActivity.kt
@@ -1,26 +1,29 @@
 package com.tencent.iot.explorer.link.kitlink.activity
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Build
 import android.view.View
 import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintSet
 import com.alibaba.fastjson.JSONObject
 import com.tencent.iot.explorer.link.App
 import com.tencent.iot.explorer.link.BuildConfig
 import com.tencent.iot.explorer.link.R
+import com.tencent.iot.explorer.link.T
+import com.tencent.iot.explorer.link.core.auth.callback.MyCallback
+import com.tencent.iot.explorer.link.core.auth.response.BaseResponse
+import com.tencent.iot.explorer.link.core.utils.FileUtils
+import com.tencent.iot.explorer.link.core.utils.Utils
+import com.tencent.iot.explorer.link.customview.ConstraintUtil
+import com.tencent.iot.explorer.link.customview.ConstraintUtil.ConstraintBegin
 import com.tencent.iot.explorer.link.customview.dialog.ProgressDialog
 import com.tencent.iot.explorer.link.customview.dialog.UpgradeDialog
 import com.tencent.iot.explorer.link.customview.dialog.UpgradeInfo
 import com.tencent.iot.explorer.link.kitlink.consts.CommonField
-import com.tencent.iot.explorer.link.core.utils.FileUtils
 import com.tencent.iot.explorer.link.kitlink.util.HttpRequest
-import com.tencent.iot.explorer.link.core.auth.callback.MyCallback
-import com.tencent.iot.explorer.link.T
-import com.tencent.iot.explorer.link.core.auth.response.BaseResponse
-import com.tencent.iot.explorer.link.core.utils.Utils
 import kotlinx.android.synthetic.main.activity_about_us.*
 import kotlinx.android.synthetic.main.menu_back_layout.*
+
 
 /**
  * 关于我们
@@ -37,6 +40,23 @@ class AboutUsActivity : BaseActivity() {
         iv_back.setColorFilter(resources.getColor(R.color.black_333333))
         tv_title.text = getString(R.string.about_me)
         tv_about_app_version.text = resources.getString(R.string.current_version) + BuildConfig.VERSION_NAME
+
+        if (App.data.regionId == "1") { //国内
+            tv_title_opensource.visibility = View.INVISIBLE
+            iv_opensource_arrow.visibility = View.INVISIBLE
+            line_opensource.visibility = View.INVISIBLE
+            var constraintUtil = ConstraintUtil(about_us_constraintLayout)
+            val begin: ConstraintBegin = constraintUtil.beginWithAnim()
+            begin.clear(R.id.tv_title_version)
+            begin.TopToBottomOf(R.id.tv_title_version, R.id.line_user_agreement)
+            begin.setWidth(R.id.tv_title_version, ConstraintSet.PARENT_ID);
+            begin.setHeight(R.id.tv_title_version, 200)
+            begin.commit()
+        } else {
+            tv_title_opensource.visibility = View.VISIBLE
+            iv_opensource_arrow.visibility = View.VISIBLE
+            line_opensource.visibility = View.VISIBLE
+        }
     }
 
     override fun setListener() {
@@ -44,6 +64,7 @@ class AboutUsActivity : BaseActivity() {
         tv_title_privacy_policy.setOnClickListener(listener)
         tv_title_user_agreement.setOnClickListener(listener)
         tv_about_app_version.setOnClickListener(listener)
+        tv_title_opensource.setOnClickListener(listener)
     }
 
     private var listener = object : View.OnClickListener {
@@ -51,25 +72,52 @@ class AboutUsActivity : BaseActivity() {
         override fun onClick(v: View?) {
             when (v) {
                 tv_title_user_agreement -> {
-                    val intent = Intent(this@AboutUsActivity, WebActivity::class.java)
-                    intent.putExtra(CommonField.EXTRA_TITLE, getString(R.string.register_agree_2))
-                    var url = CommonField.POLICY_PREFIX
-                    url += "?uin=$ANDROID_ID"
-                    url += CommonField.SERVICE_POLICY_SUFFIX
-                    intent.putExtra(CommonField.EXTRA_TEXT, url)
-                    startActivity(intent)
+
+                    if (App.data.regionId == "1") { //国内
+
+                        val intent = Intent(this@AboutUsActivity, WebActivity::class.java)
+                        intent.putExtra(CommonField.EXTRA_TITLE, getString(R.string.register_agree_2))
+                        var url = CommonField.POLICY_PREFIX
+                        url += "?uin=$ANDROID_ID"
+                        url += CommonField.SERVICE_POLICY_SUFFIX
+                        intent.putExtra(CommonField.EXTRA_TEXT, url)
+                        startActivity(intent)
+                    } else {
+
+                        val intent = Intent(this@AboutUsActivity, OpensourceLicenseActivity::class.java)
+                        intent.putExtra(CommonField.EXTRA_TITLE, getString(R.string.register_agree_2))
+                        intent.putExtra(CommonField.EXTRA_TEXT, CommonField.SERVICE_AGREEMENT_URL)
+                        startActivity(intent)
+                    }
                 }
 
                 tv_title_privacy_policy -> {
-                    val intent = Intent(this@AboutUsActivity, WebActivity::class.java)
-                    intent.putExtra(CommonField.EXTRA_TITLE, getString(R.string.register_agree_4))
-                    var url = CommonField.POLICY_PREFIX
-                    url += "?uin=$ANDROID_ID"
-                    url += CommonField.PRIVACY_POLICY_SUFFIX
-                    intent.putExtra(CommonField.EXTRA_TEXT, url)
-                    startActivity(intent)
+
+                    if (App.data.regionId == "1") { //国内
+
+                        val intent = Intent(this@AboutUsActivity, WebActivity::class.java)
+                        intent.putExtra(CommonField.EXTRA_TITLE, getString(R.string.register_agree_4))
+                        var url = CommonField.POLICY_PREFIX
+                        url += "?uin=$ANDROID_ID"
+                        url += CommonField.PRIVACY_POLICY_SUFFIX
+                        intent.putExtra(CommonField.EXTRA_TEXT, url)
+                        startActivity(intent)
+                    } else {
+
+                        val intent = Intent(this@AboutUsActivity, OpensourceLicenseActivity::class.java)
+                        intent.putExtra(CommonField.EXTRA_TITLE, getString(R.string.register_agree_4))
+                        intent.putExtra(CommonField.EXTRA_TEXT, CommonField.PRIVACY_POLICY_URL)
+                        startActivity(intent)
+                    }
                 }
 
+                tv_title_opensource -> {
+                    val intent = Intent(this@AboutUsActivity, OpensourceLicenseActivity::class.java)
+                    intent.putExtra(CommonField.EXTRA_TITLE, getString(R.string.register_agree_5))
+                    intent.putExtra(CommonField.EXTRA_TEXT, CommonField.OPENSOURCE_LICENSE_URL)
+                    startActivity(intent)
+                }
+                
                 tv_about_app_version -> {
                     startUpdateApp()
                 }
