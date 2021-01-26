0c1642f3ccebd65349640356213a42f80baa9ffa
archurtan <archurspace@gmail.com>
2021-01-26 18:44:01 +0800

    添加解析录像列表功能
    Change-Id: I734e81f7bacccfaeeeb16a5914a7a8c904f51b05

---
+++ sdk/video-link-android/src/main/java/com/tencent/iot/video/link/entity/PlaybackVideoEntity.kt
@@ -0,0 +1,7 @@
+package com.tencent.iot.video.link.entity
+
+class PlaybackVideoEntity {
+    var file_name = ""
+    var start_time = ""
+    var end_time = ""
+}
--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/activity/PlaybackVideoActivity.kt
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/activity/PlaybackVideoActivity.kt
@@ -1,66 +1,72 @@
 package com.tencent.iot.explorer.link.core.demo.activity
 
-import android.Manifest
 import android.os.Handler
 import android.os.Looper
+import android.text.TextUtils
 import android.view.SurfaceHolder
 import android.view.View
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tencent.iot.explorer.link.core.demo.R
+import com.tencent.iot.explorer.link.core.demo.adapter.OnItemListener
+import com.tencent.iot.explorer.link.core.demo.adapter.VideoListAdapter
+import com.tencent.iot.explorer.link.core.demo.holder.BaseHolder
 import com.tencent.iot.explorer.link.core.demo.log.L
-import com.tencent.iot.video.link.util.audio.AudioRecordUtil
+import com.tencent.iot.explorer.link.core.demo.view.MyDivider
 import com.tencent.iot.video.link.consts.VideoConst
+import com.tencent.iot.video.link.entity.PlaybackVideoEntity
+import com.tencent.iot.video.link.util.JsonManager
 import com.tencent.xnet.XP2P
 import com.tencent.xnet.XP2PCallback
-import kotlinx.android.synthetic.main.activity_video.*
+import kotlinx.android.synthetic.main.activity_playback_video.*
 import tv.danmaku.ijk.media.player.IjkMediaPlayer
 import java.text.SimpleDateFormat
 import java.util.*
 
-class VideoActivity : BaseActivity(), View.OnClickListener, SurfaceHolder.Callback, XP2PCallback {
+class PlaybackVideoActivity  : BaseActivity(), View.OnClickListener, SurfaceHolder.Callback,
+    XP2PCallback {
 
     private lateinit var secretId: String
     private lateinit var secretKey: String
     private lateinit var productId: String
     private lateinit var deviceName: String
-    private var isSpeaking: Boolean = false
     private var isPlaying: Boolean = true
     private var isP2PChannelAvailable: Boolean = false
 
+    private var videoList = arrayListOf<PlaybackVideoEntity>()
+    private lateinit var adapter: VideoListAdapter
+
     private lateinit var mPlayer: IjkMediaPlayer
-    private lateinit var audioRecordUtil: AudioRecordUtil
     private val mHandler = Handler(Looper.getMainLooper())
 
-    private var permissions = arrayOf(
-        Manifest.permission.RECORD_AUDIO
-    )
-
     override fun getContentView(): Int {
-        return R.layout.activity_video
+        return R.layout.activity_playback_video
     }
 
     override fun initView() {
-        requestPermission(permissions)
         enableSaveLog()
+        adapter = VideoListAdapter(this, videoList)
+        playback_video_list.addItemDecoration(MyDivider(dp2px(16), dp2px(16), dp2px(16)))
+        playback_video_list.layoutManager = LinearLayoutManager(this)
+        playback_video_list.adapter = adapter
+
         val bundle = this.intent.extras
         secretId = bundle?.get(VideoConst.VIDEO_SECRET_ID) as String
         secretKey = bundle.get(VideoConst.VIDEO_SECRET_KEY) as String
         productId = bundle.get(VideoConst.VIDEO_PRODUCT_ID) as String
         deviceName = bundle.get(VideoConst.VIDEO_DEVICE_NAME) as String
 
-        audioRecordUtil = AudioRecordUtil()
-
-        video_view.holder.addCallback(this)
+        playback_video_view.holder.addCallback(this)
         mPlayer = IjkMediaPlayer()
         mPlayer.setOnPreparedListener {
             mHandler.post {
-                val viewWidth = video_view.width
+                val viewWidth = playback_video_view.width
                 val videoWidth = mPlayer.videoWidth
                 val videoHeight = mPlayer.videoHeight
-                val lp = video_view.layoutParams
+                val lp = playback_video_view.layoutParams
                 lp.width = viewWidth
                 lp.height = (videoHeight.toFloat() * viewWidth.toFloat() / videoWidth.toFloat()).toInt()
-                video_view.layoutParams = lp
+                playback_video_view.layoutParams = lp
             }
         }
         if (productId == " " || deviceName == " " || secretId == " " || secretKey == " ") {
@@ -69,50 +75,56 @@ class VideoActivity : BaseActivity(), View.OnClickListener, SurfaceHolder.Callba
             val ret = openP2PChannel(productId, deviceName, secretId, secretKey)
             if (ret == 0) {
                 isP2PChannelAvailable = true
-                val url = XP2P.delegateHttpFlv() + "ipc.flv?action=live"
-                mPlayer.dataSource = url
-                mPlayer.prepareAsync()
-                mPlayer.start()
+                val jsonArray = XP2P.getComandRequestWithSync("action=inner_define&cmd=get_record_index", 2*1000*1000)
+                if (!TextUtils.isEmpty(jsonArray)) {
+                    val list = JsonManager.parseJsonArray(jsonArray, PlaybackVideoEntity::class.java)
+                    videoList.addAll(list)
+                    adapter.notifyDataSetChanged()
+                } else {
+                    Toast.makeText(this, "录像列表为空", Toast.LENGTH_LONG).show()
+//                    val url = XP2P.delegateHttpFlv() + "ipc.flv?action=playback"
+//                    mPlayer.dataSource = url
+//                    mPlayer.prepareAsync()
+//                    mPlayer.start()
+                }
             } else {
                 isP2PChannelAvailable = false
-                speak.visibility = View.GONE
-                watch_monitor.visibility = View.GONE
+                watch_playback.visibility = View.GONE
                 Toast.makeText(this, "P2P通道建立失败，请检查设备是否上线", Toast.LENGTH_LONG).show()
             }
         }
     }
 
     override fun setListener() {
-        speak.setOnClickListener(this)
-        watch_monitor.setOnClickListener(this)
+        watch_playback.setOnClickListener(this)
+        adapter.setOnItemListener(object : OnItemListener {
+            override fun onItemClick(holder: BaseHolder<*>, clickView: View, position: Int) {
+                val videoEntity = videoList[position]
+                val startTime = date2TimeStamp(videoEntity.start_time, "yyyy-MM-dd_HH-mm-ss")
+                val endTime = date2TimeStamp(videoEntity.end_time, "yyyy-MM-dd_HH-mm-ss")
+                val url = XP2P.delegateHttpFlv() + "ipc.flv?action=playback&start_time=${startTime}&end_time=${endTime}"
+                if (mPlayer != null) {
+                    resetWatchState()
+                    mPlayer.reset()
+                    mPlayer.dataSource = url
+                    mPlayer.setSurface(playback_video_view.holder.surface)
+                    mPlayer.prepareAsync()
+                    mPlayer.start()
+                }
+            }
+        })
     }
 
     override fun onClick(v: View?) {
         when(v) {
-            speak -> {
-                if (isSpeaking) {
-                    stopSpeak()
-                    isSpeaking = false
-                    speak.text = "开始对讲"
-                } else {
-                    if (checkPermissions(permissions)) {
-                        startSpeak()
-                        isSpeaking = true
-                        speak.text = "停止对讲"
-                    } else {
-                        requestPermission(permissions)
-                    }
-                }
-            }
-            watch_monitor -> {
+            watch_playback -> {
                 if (isPlaying) {
                     mPlayer.pause()
                     isPlaying = false
-                    watch_monitor.text = "开始播放"
+                    watch_playback.text = "开始播放"
                 } else {
                     mPlayer.start()
-                    isPlaying = true
-                    watch_monitor.text = "停止播放"
+                    resetWatchState()
                 }
             }
         }
@@ -131,16 +143,6 @@ class VideoActivity : BaseActivity(), View.OnClickListener, SurfaceHolder.Callba
         }
     }
 
-    private fun startSpeak() {
-        XP2P.runSendService()
-        Thread.sleep(500)
-        audioRecordUtil.start()
-    }
-
-    private fun stopSpeak() {
-        audioRecordUtil.stop()
-    }
-
     private fun enableSaveLog() {
         val sdf = SimpleDateFormat("", Locale.SIMPLIFIED_CHINESE)
         sdf.applyPattern("yyyy-MM-dd-HH-mm-ss")
@@ -148,7 +150,8 @@ class VideoActivity : BaseActivity(), View.OnClickListener, SurfaceHolder.Callba
         Runtime.getRuntime().exec(arrayOf("logcat", "-f", filePath, "XP2P-LOG:V", "*:S"))
     }
 
-    override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) { }
+    override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+    }
 
     override fun surfaceDestroyed(holder: SurfaceHolder?) { }
 
@@ -160,10 +163,18 @@ class VideoActivity : BaseActivity(), View.OnClickListener, SurfaceHolder.Callba
         super.onDestroy()
         mPlayer.release()
         XP2P.stopService()
-        audioRecordUtil.release()
     }
 
     override fun fail(msg: String?, errorCode: Int) {
-        L.d("==============fail=============")
+    }
+
+    private fun date2TimeStamp(dateString: String?, format: String?): Long {
+        val sdf = SimpleDateFormat(format)
+        return sdf.parse(dateString).time / 1000
+    }
+
+    private fun resetWatchState() {
+        isPlaying = true
+        watch_playback.text = "停止播放"
     }
 }
