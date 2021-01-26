d53a18cef253b566829104430b511f26488683cf
heyawei <379135467@qq.com>
2021-01-26 15:24:40 +0800

    云存录像取消原生的进度条，自定义进度条控制播放的进度
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192861722531
    
    Change-Id: Id1cfb9ea566997bbd6fac06264b97e1abf0f7eee

--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/activity/DateIPCActivity.kt
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/activity/DateIPCActivity.kt
@@ -5,14 +5,13 @@ import android.net.Uri
 import android.os.Build
 import android.text.TextUtils
 import android.util.Log
+import android.view.View
 import android.view.View.OnTouchListener
 import android.widget.MediaController
 import android.widget.SeekBar
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.core.util.rangeTo
 import androidx.recyclerview.widget.GridLayoutManager
-import com.alibaba.fastjson.JSON
 import com.alibaba.fastjson.JSONObject
 import com.tencent.iot.explorer.link.core.demo.R
 import com.tencent.iot.explorer.link.core.demo.entity.BaseResponse
@@ -23,9 +22,11 @@ import com.tencent.iot.explorer.link.core.demo.view.TimeNum
 import com.tencent.iot.video.link.callback.VideoCallback
 import com.tencent.iot.video.link.service.VideoBaseService
 import kotlinx.android.synthetic.main.activity_date_ipc.*
+import kotlinx.android.synthetic.main.activity_date_ipc.view.*
 import kotlinx.android.synthetic.main.activity_ipc.vedio_player
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.concurrent.thread
 
 
 class DateIPCActivity : BaseActivity() {
@@ -41,6 +42,7 @@ class DateIPCActivity : BaseActivity() {
     var paramDateParamStr = ""
     var baseUrl = ""
     var playUrl = ""
+    var freshThread: Thread? = null
 
     override fun getContentView(): Int {
         return R.layout.activity_date_ipc
@@ -176,7 +178,6 @@ class DateIPCActivity : BaseActivity() {
                     }
                 }
             })
-
         }
     }
 
@@ -185,6 +186,8 @@ class DateIPCActivity : BaseActivity() {
             var h = progress /60
             var m = progress % 60
             tv_current_time.setText("" + h + "时" + m + "分")
+            if (!fromUser) return  // 程序逻辑造成的进度刷新，不触发重新获取 url 的过程
+
             if (TextUtils.isEmpty(paramDateStr)) {
                 Toast.makeText(this@DateIPCActivity, "请选择回放日期", Toast.LENGTH_SHORT).show()
             } else {
@@ -200,7 +203,7 @@ class DateIPCActivity : BaseActivity() {
                                     "&endtime=" + paramDateParamStr + String.format("%02d%02d%02d", progressItemList!!.get(j).endHour, progressItemList!!.get(j).endMin, progressItemList!!.get(j).endSec)
                             Log.e("XXX", "playUrl " + playUrl)
 
-                            playVideo(playUrl)
+                            playVideo(playUrl, tagA, (progress - tagA) * 60 * 1000)
                             return
                         }
                     }
@@ -214,16 +217,51 @@ class DateIPCActivity : BaseActivity() {
         override fun onStopTrackingTouch(seekBar: SeekBar?) {}
     }
 
-    private fun playVideo(url: String) {
+    /**
+     * offset : 录像偏移的毫秒数
+     */
+    private fun playVideo(url: String, start: Int, offset: Int) {
+        if (freshThread != null) {      // 已经处在播放过程中，拖拽进度条，清理上一次的刷新线程
+            freshThread?.interrupt()
+        }
         val uri: Uri = Uri.parse(url)
         vedio_player.setVideoURI(uri)
-        var mediaController = MediaController(this)
-        vedio_player.setMediaController(mediaController)
         vedio_player.start()
+        vedio_player.seekTo(offset) // 设置进度为毫秒级
+        freshThread = Thread(Runnable {
+            while (true) {
+                try {
+                    Thread.sleep(30000) // 半分钟检查一次进度值并刷新
+                } catch (e : InterruptedException) {
+                    e.printStackTrace()
+                    break
+                }
+
+                runOnUiThread(Runnable {
+                    seekbar.setProgress(start + vedio_player.currentPosition / (1000 * 60)) // 设置进度为分钟级
+                })
+            }
+        })
+        freshThread?.start()
+        vedio_player.setOnCompletionListener {
+            freshThread?.interrupt()
+        }
+
+        vedio_player.setOnErrorListener { mp, what, extra ->
+            freshThread?.interrupt()
+            false
+        }
     }
 
     private fun stopPlay() {
         vedio_player.stopPlayback()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (freshThread != null) {
+            freshThread?.interrupt()
+        }
     }
 
     companion object {
