1e221df3a37423043d9edae96a57f9319e92c0a2
archurtan <archurspace@gmail.com>
2021-02-23 18:11:21 +0800

    sdkdemo中增加是否保存裸流的开关
    Change-Id: Ibba3fda7a61773ace203c00a1877fc236a093c1c

--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/activity/InputAuthorizeActivity.kt
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/activity/InputAuthorizeActivity.kt
@@ -1,12 +1,15 @@
 package com.tencent.iot.explorer.link.core.demo.activity
 
+import android.R.id.toggle
 import android.text.TextUtils
+import android.widget.CompoundButton
 import android.widget.Toast
 import com.tencent.iot.explorer.link.core.demo.BuildConfig
 import com.tencent.iot.explorer.link.core.demo.R
 import com.tencent.iot.explorer.link.core.utils.SharePreferenceUtil
 import com.tencent.iot.video.link.consts.VideoConst
 import kotlinx.android.synthetic.main.activity_input_authorize.*
+
 
 class InputAuthorizeActivity : BaseActivity() {
 
@@ -18,6 +21,7 @@ class InputAuthorizeActivity : BaseActivity() {
         var secretId = SharePreferenceUtil.getString(this, VideoConst.VIDEO_CONFIG, VideoConst.VIDEO_SECRET_ID)
         var secretKey = SharePreferenceUtil.getString(this, VideoConst.VIDEO_CONFIG, VideoConst.VIDEO_SECRET_KEY)
         var productId = SharePreferenceUtil.getString(this, VideoConst.VIDEO_CONFIG, VideoConst.VIDEO_PRODUCT_ID)
+        SharePreferenceUtil.saveString(this, VideoConst.VIDEO_CONFIG, VideoConst.VIDEO_SAVE_RAW_AV, "false")
 
         // 没有对应的沙盒信息，导入配置项的内容
         if (TextUtils.isEmpty(secretId.trim())) {
@@ -51,6 +55,13 @@ class InputAuthorizeActivity : BaseActivity() {
             }
         }
 
+        tbtn_save_raw_video.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                SharePreferenceUtil.saveString(this, VideoConst.VIDEO_CONFIG, VideoConst.VIDEO_SAVE_RAW_AV, "true")
+            } else {
+                SharePreferenceUtil.saveString(this, VideoConst.VIDEO_CONFIG, VideoConst.VIDEO_SAVE_RAW_AV, "false")
+            }
+        }
     }
 
     private fun checkInput(): Boolean {
