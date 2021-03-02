6da87fea0f98a17312907eda1df2600b63c130cf
heyawei <379135467@qq.com>
2021-03-02 09:54:53 +0800

    [腾讯连连App]基于小程序的视觉重构-首页优化（家庭管理）
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192859911107?url_cache_key=a0791f381590f6254135cb9abb1f42a7&action_entry_type=stories
    
    Change-Id: Iefd57fd34bc59e84d827f7ed6c1b593fcaa1b346

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/activity/FamilyAddressActivity.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/activity/FamilyAddressActivity.kt
@@ -4,12 +4,23 @@ import android.Manifest.permission
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.text.TextUtils
-import com.alibaba.fastjson.JSON
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.alibaba.fastjson.JSONObject
+import com.google.android.material.appbar.AppBarLayout
+import com.scwang.smart.refresh.footer.ClassicsFooter
+import com.scwang.smart.refresh.header.ClassicsHeader
+import com.scwang.smart.refresh.layout.api.RefreshLayout
+import com.scwang.smart.refresh.layout.listener.OnRefreshLoadMoreListener
 import com.tencent.iot.explorer.link.R
 import com.tencent.iot.explorer.link.core.auth.http.HttpCallBack
 import com.tencent.iot.explorer.link.core.auth.http.HttpUtil
 import com.tencent.iot.explorer.link.core.auth.util.JsonManager
 import com.tencent.iot.explorer.link.core.log.L
+import com.tencent.iot.explorer.link.kitlink.adapter.PostionsAdapter
+import com.tencent.iot.explorer.link.kitlink.entity.LocationResp
+import com.tencent.iot.explorer.link.kitlink.entity.Postion
+import com.tencent.iot.explorer.link.kitlink.util.AppBarStateChangeListener
 import com.tencent.map.geolocation.TencentLocation
 import com.tencent.map.geolocation.TencentLocationListener
 import com.tencent.map.geolocation.TencentLocationManager
@@ -17,21 +28,24 @@ import com.tencent.map.geolocation.TencentLocationRequest
 import com.tencent.mapsdk.raster.model.*
 import com.tencent.tencentmap.mapsdk.map.TencentMap
 import kotlinx.android.synthetic.main.activity_family_address.*
-import kotlinx.android.synthetic.main.menu_cancel_layout.*
+import kotlinx.android.synthetic.main.activity_family_address.app_bar
+import kotlinx.android.synthetic.main.activity_family_address.smart_refreshLayout
+import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.menu_back_layout.*
+import kotlinx.android.synthetic.main.menu_cancel_layout.tv_title
 
-
-/**
- * 家庭位置
- */
 class FamilyAddressActivity : BaseActivity(), TencentLocationListener {
 
+    private var PAGE_SIZE = 20
     private lateinit var locationManager: TencentLocationManager
     private lateinit var tencentMap: TencentMap
     private var marker: Marker? = null
     private var addressName = ""
-
-    private val BEIJING = LatLng(39.906016, 116.397657)
     private var tencentMapKey = ""
+    private var postions: MutableList<Postion> = ArrayList()
+    private var adapter: PostionsAdapter? = null
+    private var pageIndex = 1
+    private var latLng: LatLng? = null
 
     var permissions = arrayOf(
         permission.ACCESS_COARSE_LOCATION,
@@ -45,27 +59,57 @@ class FamilyAddressActivity : BaseActivity(), TencentLocationListener {
 
     override fun initView() {
         tv_title.text = getString(R.string.family_address)
+        var linearLayoutManager = LinearLayoutManager(this@FamilyAddressActivity)
+        adapter = PostionsAdapter(postions)
+        lv_pos.layoutManager = linearLayoutManager
+        lv_pos.adapter = adapter
+
         initMap()
         if (checkPermissions(permissions)) {
             startLocation()
         } else {
             requestPermission(permissions)
         }
+
+        smart_refreshLayout.setEnableRefresh(false)
+        smart_refreshLayout.setEnableLoadMore(true)
+        smart_refreshLayout.setRefreshFooter(ClassicsFooter(this@FamilyAddressActivity))
     }
 
     override fun setListener() {
-        tv_back.setOnClickListener { finish() }
+        iv_reset_loaction.setOnClickListener { startLocation() }
+        iv_back.setOnClickListener { finish() }
         btn_add_family.setOnClickListener {
             val data = Intent()
             data.putExtra("address", addressName)
-            setResult(200, data)
+            setResult(RESULT_OK, data)
             finish()
         }
+        app_bar.addOnOffsetChangedListener(object : AppBarStateChangeListener() {
+            override fun onStateChanged(appBarLayout: AppBarLayout, state: State, percent: Float) {
+                // 动态调整 mapview 的尺寸，保持标记处于 view 中心
+                var space = mapView.width - toolbar.minimumHeight
+                var lp = mapView.layoutParams
+                lp.height = mapView.width - ((1 - percent) * space).toInt()
+                mapView.layoutParams = lp
+            }
+        })
+
+        smart_refreshLayout.setOnRefreshLoadMoreListener(object : OnRefreshLoadMoreListener {
+            override fun onLoadMore(refreshLayout: RefreshLayout) {
+                if (this@FamilyAddressActivity.latLng == null) {
+                    refreshLayout.finishLoadMore()
+                    return
+                }
+                requestAddress(this@FamilyAddressActivity.latLng!!, pageIndex)
+            }
+
+            override fun onRefresh(refreshLayout: RefreshLayout) {
+                refreshLayout.finishRefresh()
+            }
+        })
     }
 
-    /**
-     * 初始化地图设置
-     */
     private fun initMap() {
         locationManager = TencentLocationManager.getInstance(this)
         tencentMap = mapView.map
@@ -73,85 +117,90 @@ class FamilyAddressActivity : BaseActivity(), TencentLocationListener {
             .metaData.getString("TencentMapSDK") ?: ""
     }
 
-    /**
-     * 开始定位
-     */
+    // 开始定位
     private fun startLocation() {
         L.e("开始定位")
         val request = TencentLocationRequest.create()
         request.requestLevel = TencentLocationRequest.REQUEST_LEVEL_POI
-//        request.isAllowCache = false//.isAllowCache = true
         when (locationManager.requestLocationUpdates(request, this)) {
-            TencentLocation.ERROR_OK -> {
-                L.e("定位成功")
-            }
+            TencentLocation.ERROR_OK -> { L.e("定位成功") }
             else -> {
                 L.e("定位失败")
                 locationManager.removeUpdates(this)
             }
         }
+
         tencentMap.setOnMapCameraChangeListener(object : TencentMap.OnMapCameraChangeListener {
             override fun onCameraChangeFinish(cp: CameraPosition) {
                 L.e("onCameraChangeFinish=${cp.target.latitude},${cp.target.longitude}")
                 maskTag(cp.target, "")
-//                requestAddress(cp.target)
+                requestAddress(cp.target)
             }
 
-            override fun onCameraChange(cp: CameraPosition) {
-                L.e("onCameraChange=${cp.target.latitude},${cp.target.longitude}")
-                maskTag(cp.target, "")
-            }
+            override fun onCameraChange(cp: CameraPosition) {}
         })
     }
 
-    override fun onStatusUpdate(name: String, status: Int, reason: String) {
-    }
+    override fun onStatusUpdate(name: String, status: Int, reason: String) {}
 
     override fun onLocationChanged(location: TencentLocation, error: Int, desc: String) {
         L.e("location=${JsonManager.toJson(location)}")
         marker?.run {
             L.e("marker is visibly=$isInfoWindowShown")
         }
-        if (TencentLocation.ERROR_OK == error) {
-            // 定位成功
+        if (TencentLocation.ERROR_OK == error) {    // 定位成功
             val target = LatLng(location.latitude, location.longitude)
             tencentMap.setCenter(target)
             maskTag(target, location.name ?: "")
             locationManager.removeUpdates(this)
-        } else {
-            // 定位失败
-        }
+        } else { } // 定位失败
     }
 
     private fun requestAddress(latLng: LatLng) {
+        requestAddress(latLng, 1)
+    }
+
+    private fun requestAddress(latLng: LatLng, page: Int) {
         val sb = StringBuilder("https://apis.map.qq.com/ws/geocoder/v1/?location=")
-        sb.append(latLng.latitude).append(",").append(latLng.longitude).append("&key=")
-            .append(tencentMapKey)
+        sb.append(latLng.latitude).append(",").append(latLng.longitude).append("&key=").append(tencentMapKey)
+        sb.append("&get_poi=1").append("&poi_options=page_size=${PAGE_SIZE};page_index=${page}")
+        Log.e("XXX", "str " + sb.toString())
         HttpUtil.get(sb.toString(), object : HttpCallBack {
             override fun onSuccess(response: String) {
                 L.e("地址解析", "response=$response")
-                val result = JSON.parseObject(response)
-                if (result.getIntValue("status") == 0) {
-                    result.getJSONObject("result").run {
-                        getJSONObject("formatted_addresses").let {
-                            addressName = it.getString("recommend")
-                            if (!TextUtils.isEmpty(addressName)) {
-                                maskTag(latLng, addressName)
-                            }
-                        }
+                var locationResp = JSONObject.parseObject(response, LocationResp::class.java)
+                Log.e("XXX", "locationResp.status " + locationResp.status)
+                Log.e("XXX", "pageIndex " + pageIndex)
+                if (locationResp != null && locationResp.status == 0) {
+                    addressName = locationResp.result.formatted_addresses.recommend
+                    if (!TextUtils.isEmpty(addressName)) {
+                        maskTag(latLng, addressName)
                     }
+                    if (locationResp.result.pois.size > 0) {
+                        pageIndex++
+                    }
+                    postions.addAll(locationResp.result.pois)
+                    adapter?.notifyDataSetChanged()
+                }
+
+                if (smart_refreshLayout.isLoading) {
+                    smart_refreshLayout.finishLoadMore()
                 }
             }
 
             override fun onError(error: String) {
+                if (smart_refreshLayout.isLoading) {
+                    smart_refreshLayout.finishLoadMore()
+                }
             }
         })
     }
 
-    /**
-     * 标记地图
-     */
+    // 标记地图
     private fun maskTag(latLng: LatLng, title: String) {
+        postions.clear()
+        this@FamilyAddressActivity.latLng = latLng
+
         runOnUiThread {
             addressName = title
             if (tencentMap.zoomLevel < tencentMap.maxZoomLevel) {
