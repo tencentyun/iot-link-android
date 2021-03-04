64efb47d0cf669fff0c17de26a92085cc24576da
archurtan <archurspace@gmail.com>
2021-03-04 15:54:40 +0800

    配置git信息
    Change-Id: I6fb783cc34d59c461dd21d10ee4822848f4c9941

--- .github/scripts/updateVersionInIotVersionRepo.sh
+++ .github/scripts/updateVersionInIotVersionRepo.sh
@@ -4,6 +4,11 @@ rc=$(git rev-parse --short HEAD)
 
 cd iot-version
 
+echo "start commit..."
+
+git config --global user.email "archurspace@gmail.com"
+git config --global user.name "archurtan"
+
 git add .
 git commit -m "repositories@$rc"
 git push
