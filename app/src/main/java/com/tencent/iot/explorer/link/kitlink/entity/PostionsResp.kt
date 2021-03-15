7d1063288996b641123449e02adfabb2f78cc271
heyawei <379135467@qq.com>
2021-03-15 17:32:52 +0800

    地图页面支持地名搜索，历史搜索记录保存
    https://docs.qq.com/doc/DRWthcG12TE1QSElx
    
    Change-Id: I5260d922107029ae9b3e0a15f58effb928be7e7c

---
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/entity/PostionsResp.kt
@@ -0,0 +1,8 @@
+package com.tencent.iot.explorer.link.kitlink.entity
+
+class PostionsResp {
+    var status = 0
+    var message = ""
+    var count = 0
+    var data : MutableList<Postion> = ArrayList()
+}
