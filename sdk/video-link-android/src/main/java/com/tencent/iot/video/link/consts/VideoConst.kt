e8537209ed243a860bc1755c0598ac3cf796bf64
archurtan <archurspace@gmail.com>
2021-03-25 17:18:22 +0800

    添加多路直播演示场景
    Change-Id: Ibd90ae4c32ffe1efea712e7f21824fb5ad66d7f2

--- sdk/video-link-android/src/main/java/com/tencent/iot/video/link/consts/VideoConst.kt
+++ sdk/video-link-android/src/main/java/com/tencent/iot/video/link/consts/VideoConst.kt
@@ -9,4 +9,9 @@ object VideoConst {
     const val VIDEO_DEVICE_NAME = "VideoDeviceName"
     const val VIDEO_PLAYBACK= "VideoPlayback"
     const val VIDEO_SAVE_RAW_AV= "SaveRawAV"
+    const val MULTI_VIDEO_PROD_ID= "productId"
+    const val MULTI_VIDEO_SECRET_KEY= "secretKey"
+    const val MULTI_VIDEO_SECRET_ID= "secretId"
+    const val MULTI_VIDEO_DEVICE_NAME01= "deviceName01"
+    const val MULTI_VIDEO_DEVICE_NAME02= "deviceName02"
 }
