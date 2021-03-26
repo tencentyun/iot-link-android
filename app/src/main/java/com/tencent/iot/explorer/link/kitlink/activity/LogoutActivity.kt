24227b46cfdbc8f3f894d1e19e676c9c0b581a8f
heyawei <379135467@qq.com>
2021-03-26 16:05:01 +0800

    修复问题 —— 我已了解《腾讯连连账号注销协议》显示不全
    Change-Id: I161529d06845bcca6b4df96a772770d02bfd355a

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/activity/LogoutActivity.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/activity/LogoutActivity.kt
@@ -3,6 +3,11 @@ package com.tencent.iot.explorer.link.kitlink.activity
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.View
 import com.tencent.iot.explorer.link.App
 import com.tencent.iot.explorer.link.R
@@ -14,6 +19,7 @@ import com.tencent.iot.explorer.link.mvp.view.LogoutView
 import com.tencent.iot.explorer.link.T
 import com.tencent.iot.explorer.link.core.utils.Utils
 import kotlinx.android.synthetic.main.activity_logout.*
+import kotlinx.android.synthetic.main.activity_register.*
 import kotlinx.android.synthetic.main.menu_back_layout.*
 import java.util.*
 
@@ -38,12 +44,42 @@ class LogoutActivity  : PActivity(), LogoutView, View.OnClickListener{
         tv_logout_time.text =
             DateUtils.getFormatDateWithoutTime(DateUtils.getDateAfter(Date(), 7+1)) + " 00:00:00" // 7天后的24点
         presenter = LogoutPresenter(this)
+        formatTip()
+    }
+
+    private fun formatTip() {
+        val str = resources.getString(R.string.already_known)
+        val partStr1 = resources.getString(R.string.tencentll_account_logout_agreement)
+        var showStr = str + " " + partStr1
+        val spannable = SpannableStringBuilder(showStr)
+
+        spannable.setSpan(object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(this@LogoutActivity, WebActivity::class.java)
+                intent.putExtra(CommonField.EXTRA_TITLE, getString(R.string.tencentll_account_logout_agreement2))
+                var url = CommonField.POLICY_PREFIX
+                url += "?uin=$ANDROID_ID"
+                url += CommonField.CANCEL_POLICY_SUFFIX
+                intent.putExtra(CommonField.EXTRA_TEXT, url)
+                startActivity(intent)
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.color = resources.getColor(R.color.blue_0066FF)
+                ds.setUnderlineText(false);
+            }
+
+        },
+            showStr.length - partStr1.length, showStr.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        clear_account_tv.setMovementMethod(LinkMovementMethod.getInstance())
+        clear_account_tv.setText(spannable)
     }
 
     override fun setListener() {
         iv_back.setOnClickListener { finish() }
         btn_logout.setOnClickListener(this)
-        tv_logout_account_agreement.setOnClickListener(this)
         iv_logout_agreement.setOnClickListener(this)
     }
 
@@ -53,15 +89,6 @@ class LogoutActivity  : PActivity(), LogoutView, View.OnClickListener{
                 if (presenter.isAgreement()) {
                     showConfirmCancelDialog()
                 }
-            }
-            tv_logout_account_agreement -> {// 注销协议
-                val intent = Intent(this, WebActivity::class.java)
-                intent.putExtra(CommonField.EXTRA_TITLE, getString(R.string.tencentll_account_logout_agreement2))
-                var url = CommonField.POLICY_PREFIX
-                url += "?uin=$ANDROID_ID"
-                url += CommonField.CANCEL_POLICY_SUFFIX
-                intent.putExtra(CommonField.EXTRA_TEXT, url)
-                startActivity(intent)
             }
             iv_logout_agreement -> {
                 presenter.agreement()
@@ -89,7 +116,6 @@ class LogoutActivity  : PActivity(), LogoutView, View.OnClickListener{
     }
 
     override fun cancelAccountSuccess() {
-        // TODO 显示账号已申请注销Dialog
         showCancelReqSuccessDialog()
     }
 
