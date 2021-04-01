9a93c2d0f6e838437b6becf59f4e29ecfb2c5240
heyawei <379135467@qq.com>
2021-04-01 17:51:40 +0800

    智能 UI 调整
    Change-Id: I1668ba31b84874b73ebf80cc35f4b468059f0e6b

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/activity/AddTaskNameActivity.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/activity/AddTaskNameActivity.kt
@@ -1,6 +1,9 @@
 package com.tencent.iot.explorer.link.kitlink.activity
 
 import android.content.Intent
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import com.tencent.iot.explorer.link.R
 import com.tencent.iot.explorer.link.T
 import com.tencent.iot.explorer.link.kitlink.consts.CommonField
@@ -16,10 +19,23 @@ class AddTaskNameActivity : BaseActivity() {
 
     override fun initView() {
         tv_title.setText(R.string.add_smart_name)
+        ev_task_name.addTextChangedListener(object :TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s == null || s.toString().length <= 0) {
+                    layout_clear.visibility = View.GONE
+                } else {
+                    layout_clear.visibility = View.VISIBLE
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
         ev_task_name.setText(intent.getStringExtra(CommonField.EXYRA_TASK_NAME));
     }
 
     override fun setListener() {
+        layout_clear.setOnClickListener { ev_task_name.setText("") }
         iv_back.setOnClickListener { finish() }
         tv_ok.setOnClickListener {
             if (ev_task_name.text.toString().trim().length > 20) {
