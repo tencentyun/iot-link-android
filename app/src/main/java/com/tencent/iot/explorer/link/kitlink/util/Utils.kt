6b958e4da41a26212f23eb40e7f6508de1c757e0
heyawei <379135467@qq.com>
2021-04-08 16:12:48 +0800

    修复界面显示相关问题 1、地图过渡时间明显 2、快捷入口精度不够 3、忘记密码的界面尺寸
    Change-Id: I5c8775599ef073bf460789532d27429d9fef9d00

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/util/Utils.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/util/Utils.kt
@@ -15,5 +15,23 @@ class Utils {
             LocalBroadcastManager.getInstance(context).sendBroadcast(intent)
             context.sendBroadcast(intent)
         }
+
+        fun length(num: Float): Int {
+            var len = 1
+            val str = num.toString()
+            val parts = str.split(".")
+            if (parts != null && parts.size == 2) {
+                for (i in parts[1].length - 1 downTo 1) {
+                    if (parts[1][i].toString() != "0") {
+                        len = i + 1
+                        break
+                    }
+                }
+            }
+            if (len <= 0) {
+                len = 1
+            }
+            return len
+        }
     }
 }
