669bdd61df2bc0904959e310ed0952d4e06b14fe
heyawei <379135467@qq.com>
2021-04-09 16:22:42 +0800

    扫描网关设备，子设备未自动绑定
    Change-Id: Ib3e1adb3972420cc18ae9a06ef5632acce9680aa

---
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/entity/DeviceInfo.kt
@@ -0,0 +1,7 @@
+package com.tencent.iot.explorer.link.kitlink.entity
+
+import com.tencent.iot.explorer.link.core.auth.entity.DeviceEntity
+
+class DeviceInfo {
+    var AppDeviceInfo: DeviceEntity? = null
+}
