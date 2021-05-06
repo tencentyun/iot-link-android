c394158f025b1c953df70e389acd8cea85b00a61
archurtan <archurspace@gmail.com>
2021-05-06 13:26:44 +0800

    sdkdemo引入集成测试
    Change-Id: I393bc2f2466a62fffda5dd2bc0488e0efa2e3c68

---
+++ sdkdemo/src/test/java/com/tencent/iot/explorer/link/core/demo/ModuleActivityTest.java
@@ -0,0 +1,31 @@
+package com.tencent.iot.explorer.link.core.demo;
+
+import android.content.Intent;
+import android.widget.Button;
+
+import com.tencent.iot.explorer.link.core.demo.activity.LoginActivity;
+import com.tencent.iot.explorer.link.core.demo.activity.ModuleActivity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertEquals;
+import static org.robolectric.Shadows.shadowOf;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk=29)
+public class ModuleActivityTest {
+    @Test
+    public void testJump2Login() {
+        ModuleActivity activity = Robolectric.buildActivity(ModuleActivity.class).create().get();
+        Button btn = activity.findViewById(R.id.btn_1);
+        btn.performClick();
+        Intent expectedIntent = new Intent(activity, LoginActivity.class);
+        Intent actual = shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+        assertEquals(expectedIntent.getComponent(), actual.getComponent());
+    }
+}
