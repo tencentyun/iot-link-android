30b767d125717fdfdf1f4ec822117a86f187c06e
archurtan <archurspace@gmail.com>
2021-05-10 10:53:58 +0800

    移除三方verticaltab依赖，使用原生进行重构
    Change-Id: I5c36cb2b3b6c6a05d50f6964cfa8f2bc9aa34271

---
+++ app/src/main/java/com/tencent/iot/explorer/link/customview/verticaltab/VerticalTabLayout.java
@@ -0,0 +1,397 @@
+package com.tencent.iot.explorer.link.customview.verticaltab;
+
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.tencent.iot.explorer.link.R;
+import com.tencent.iot.explorer.link.core.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class VerticalTabLayout extends ScrollView {
+
+    public static int TAB_MODE_FIXED = 10;
+    public static int TAB_MODE_SCROLLABLE = 11;
+
+    private final Context mContext;
+    private TabStrip mTabStrip;
+    private TabView mSelectedTab;
+    private final int mColorIndicator;
+    private final int mTabMargin;
+    private final int mTabMode;
+    private final int mTabHeight;
+    private int mIndicatorWidth;
+    private int mIndicatorGravity;
+
+    private final List<OnTabSelectedListener> mTabSelectedListeners;
+
+    private TabFragmentManager mTabFragmentManager;
+
+    public VerticalTabLayout(Context context) {
+        this(context, null);
+    }
+
+    public VerticalTabLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public VerticalTabLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mContext = context;
+        mTabSelectedListeners = new ArrayList<>();
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.VerticalTabLayout);
+        mColorIndicator = typedArray.getColor(R.styleable.VerticalTabLayout_indicator_color,
+                context.getResources().getColor(R.color.colorAccent));
+        mIndicatorWidth = (int) typedArray.getDimension(R.styleable.VerticalTabLayout_indicator_width, Utils.INSTANCE.dp2px(context, 3));
+        mIndicatorGravity = typedArray.getInteger(R.styleable.VerticalTabLayout_indicator_gravity, Gravity.LEFT);
+        if (mIndicatorGravity == 3) {
+            mIndicatorGravity = Gravity.LEFT;
+        } else if (mIndicatorGravity == 5) {
+            mIndicatorGravity = Gravity.RIGHT;
+        } else if (mIndicatorGravity == 119) {
+            mIndicatorGravity = Gravity.FILL;
+        }
+        mTabMargin = (int) typedArray.getDimension(R.styleable.VerticalTabLayout_tab_margin, 0);
+        mTabMode = typedArray.getInteger(R.styleable.VerticalTabLayout_tab_mode, TAB_MODE_FIXED);
+        int defaultTabHeight = LinearLayout.LayoutParams.WRAP_CONTENT;
+        mTabHeight = (int) typedArray.getDimension(R.styleable.VerticalTabLayout_tab_height, defaultTabHeight);
+        typedArray.recycle();
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        if (getChildCount() > 0) {
+            removeAllViews();
+        }
+        initTabStrip();
+    }
+
+    private void initTabStrip() {
+        mTabStrip = new TabStrip(mContext);
+        addView(mTabStrip, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+    }
+
+    public void removeAllTabs() {
+        mTabStrip.removeAllViews();
+        mSelectedTab = null;
+    }
+
+    public TabView getTabAt(int position) {
+        return (TabView) mTabStrip.getChildAt(position);
+    }
+
+    public int getTabCount() {
+        return mTabStrip.getChildCount();
+    }
+
+    public int getSelectedTabPosition() {
+        int index = mTabStrip.indexOfChild(mSelectedTab);
+        return index == -1 ? 0 : index;
+    }
+
+    private void addTabWithMode(TabView tabView) {
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        initTabWithMode(params);
+        mTabStrip.addView(tabView, params);
+        if (mTabStrip.indexOfChild(tabView) == 0) {
+            tabView.setChecked(true);
+            params = (LinearLayout.LayoutParams) tabView.getLayoutParams();
+            params.setMargins(0, 0, 0, 0);
+            tabView.setLayoutParams(params);
+            mSelectedTab = tabView;
+            mTabStrip.post(new Runnable() {
+                @Override
+                public void run() {
+                    mTabStrip.moveIndicator(0);
+                }
+            });
+        }
+    }
+
+    private void initTabWithMode(LinearLayout.LayoutParams params) {
+        if (mTabMode == TAB_MODE_FIXED) {
+            params.height = 0;
+            params.weight = 1.0f;
+            params.setMargins(0, 0, 0, 0);
+            setFillViewport(true);
+        } else if (mTabMode == TAB_MODE_SCROLLABLE) {
+            params.height = mTabHeight;
+            params.weight = 0f;
+            params.setMargins(0, mTabMargin, 0, 0);
+            setFillViewport(false);
+        }
+    }
+
+    private void scrollToTab(int position) {
+        final TabView tabView = getTabAt(position);
+        int y = getScrollY();
+        int tabTop = tabView.getTop() + tabView.getHeight() / 2 - y;
+        int target = getHeight() / 2;
+        if (tabTop > target) {
+            smoothScrollBy(0, tabTop - target);
+        } else if (tabTop < target) {
+            smoothScrollBy(0, tabTop - target);
+        }
+    }
+
+    private void addTab(TabView tabView) {
+        if (tabView != null) {
+            addTabWithMode(tabView);
+            tabView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = mTabStrip.indexOfChild(view);
+                    setTabSelected(position);
+                }
+            });
+        } else {
+            throw new IllegalStateException("tabview can't be null");
+        }
+    }
+
+    public void setTabSelected(final int position) {
+        setTabSelected(position, true, true);
+    }
+
+    private void setTabSelected(final int position, final boolean updataIndicator, final boolean callListener) {
+        post(new Runnable() {
+            @Override
+            public void run() {
+                setTabSelectedImpl(position, updataIndicator, callListener);
+            }
+        });
+    }
+
+    private void setTabSelectedImpl(final int position, boolean updataIndicator, boolean callListener) {
+        TabView view = getTabAt(position);
+        boolean selected;
+        if (selected = (view != mSelectedTab)) {
+            if (mSelectedTab != null) {
+                mSelectedTab.setChecked(false);
+            }
+            view.setChecked(true);
+            if (updataIndicator) {
+                mTabStrip.moveIndicatorWithAnimator(position);
+            }
+            mSelectedTab = view;
+            scrollToTab(position);
+        }
+        if (callListener) {
+            for (int i = 0; i < mTabSelectedListeners.size(); i++) {
+                OnTabSelectedListener listener = mTabSelectedListeners.get(i);
+                if (listener != null) {
+                    if (selected) {
+                        listener.onTabSelected(view, position);
+                    } else {
+                        listener.onTabReselected(view, position);
+                    }
+                }
+            }
+        }
+    }
+
+    public void addOnTabSelectedListener(OnTabSelectedListener listener) {
+        if (listener != null) {
+            mTabSelectedListeners.add(listener);
+        }
+    }
+
+    public void removeOnTabSelectedListener(OnTabSelectedListener listener) {
+        if (listener != null) {
+            mTabSelectedListeners.remove(listener);
+        }
+    }
+
+    public void setTabAdapter(TabAdapter adapter) {
+        removeAllTabs();
+        if (adapter != null) {
+            for (int i = 0; i < adapter.getCount(); i++) {
+                addTab(new TabView(mContext)
+                        .setTitle(adapter.getTitle(i))
+                        .setBackground(adapter.getBackground(i)));
+            }
+        }
+    }
+
+    public void setupWithFragment(FragmentManager manager, List<Fragment> fragments) {
+        setupWithFragment(manager, 0, fragments);
+    }
+
+    public void setupWithFragment(FragmentManager manager, List<Fragment> fragments, TabAdapter adapter) {
+        setupWithFragment(manager, 0, fragments, adapter);
+    }
+
+    public void setupWithFragment(FragmentManager manager, int containerResid, List<Fragment> fragments) {
+        if (mTabFragmentManager != null) {
+            mTabFragmentManager.detach();
+        }
+        if (containerResid != 0) {
+            mTabFragmentManager = new TabFragmentManager(manager, containerResid, fragments, this);
+        } else {
+            mTabFragmentManager = new TabFragmentManager(manager, fragments, this);
+        }
+    }
+
+    public void setupWithFragment(FragmentManager manager, int containerResid, List<Fragment> fragments, TabAdapter adapter) {
+        setTabAdapter(adapter);
+        setupWithFragment(manager, containerResid, fragments);
+    }
+
+    private class TabStrip extends LinearLayout {
+        private float mIndicatorTopY;
+        private float mIndicatorX;
+        private float mIndicatorBottomY;
+        private int mLastWidth;
+        private Paint mIndicatorPaint;
+        private RectF mIndicatorRect;
+        private AnimatorSet mIndicatorAnimatorSet;
+
+        public TabStrip(Context context) {
+            super(context);
+            setWillNotDraw(false);
+            setOrientation(LinearLayout.VERTICAL);
+            mIndicatorPaint = new Paint();
+            mIndicatorPaint.setAntiAlias(true);
+            mIndicatorGravity = mIndicatorGravity == 0 ? Gravity.LEFT : mIndicatorGravity;
+            mIndicatorRect = new RectF();
+            setIndicatorGravity();
+        }
+
+        protected void setIndicatorGravity() {
+            if (mIndicatorGravity == Gravity.LEFT) {
+                mIndicatorX = 0;
+                if (mLastWidth != 0) mIndicatorWidth = mLastWidth;
+                setPadding(mIndicatorWidth, 0, 0, 0);
+            } else if (mIndicatorGravity == Gravity.RIGHT) {
+                if (mLastWidth != 0) mIndicatorWidth = mLastWidth;
+                setPadding(0, 0, mIndicatorWidth, 0);
+            } else if (mIndicatorGravity == Gravity.FILL) {
+                mIndicatorX = 0;
+                setPadding(0, 0, 0, 0);
+            }
+            post(new Runnable() {
+                @Override
+                public void run() {
+                    if (mIndicatorGravity == Gravity.RIGHT) {
+                        mIndicatorX = getWidth() - mIndicatorWidth;
+                    } else if (mIndicatorGravity == Gravity.FILL) {
+                        mLastWidth = mIndicatorWidth;
+                        mIndicatorWidth = getWidth();
+                    }
+                    invalidate();
+                }
+            });
+        }
+
+        private void calcIndicatorY(float offset) {
+            int index = (int) Math.floor(offset);
+            View childView = getChildAt(index);
+            if (Math.floor(offset) != getChildCount() - 1 && Math.ceil(offset) != 0) {
+                View nextView = getChildAt(index + 1);
+                mIndicatorTopY = childView.getTop() + (nextView.getTop() - childView.getTop()) * (offset - index);
+                mIndicatorBottomY = childView.getBottom() + (nextView.getBottom() -
+                        childView.getBottom()) * (offset - index);
+            } else {
+                mIndicatorTopY = childView.getTop();
+                mIndicatorBottomY = childView.getBottom();
+            }
+        }
+
+        protected void moveIndicator(float offset) {
+            calcIndicatorY(offset);
+            invalidate();
+        }
+
+        protected void moveIndicatorWithAnimator(int index) {
+            final int direction = index - getSelectedTabPosition();
+            View childView = getChildAt(index);
+            final float targetTop = childView.getTop();
+            final float targetBottom = childView.getBottom();
+            if (mIndicatorTopY == targetTop && mIndicatorBottomY == targetBottom) return;
+            if (mIndicatorAnimatorSet != null && mIndicatorAnimatorSet.isRunning()) {
+                mIndicatorAnimatorSet.end();
+            }
+            post(new Runnable() {
+                @Override
+                public void run() {
+                    ValueAnimator startAnime = null;
+                    ValueAnimator endAnime = null;
+                    if (direction > 0) {
+                        startAnime = ValueAnimator.ofFloat(mIndicatorBottomY, targetBottom)
+                                .setDuration(100);
+                        startAnime.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator animation) {
+                                mIndicatorBottomY = Float.parseFloat(animation.getAnimatedValue().toString());
+                                invalidate();
+                            }
+                        });
+                        endAnime = ValueAnimator.ofFloat(mIndicatorTopY, targetTop).setDuration(100);
+                        endAnime.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator animation) {
+                                mIndicatorTopY = Float.parseFloat(animation.getAnimatedValue().toString());
+                                invalidate();
+                            }
+                        });
+                    } else if (direction < 0) {
+                        startAnime = ValueAnimator.ofFloat(mIndicatorTopY, targetTop).setDuration(100);
+                        startAnime.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator animation) {
+                                mIndicatorTopY = Float.parseFloat(animation.getAnimatedValue().toString());
+                                invalidate();
+                            }
+                        });
+                        endAnime = ValueAnimator.ofFloat(mIndicatorBottomY, targetBottom).setDuration(100);
+                        endAnime.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator animation) {
+                                mIndicatorBottomY = Float.parseFloat(animation.getAnimatedValue().toString());
+                                invalidate();
+                            }
+                        });
+                    }
+                    if (startAnime != null) {
+                        mIndicatorAnimatorSet = new AnimatorSet();
+                        mIndicatorAnimatorSet.play(endAnime).after(startAnime);
+                        mIndicatorAnimatorSet.start();
+                    }
+                }
+            });
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            mIndicatorPaint.setColor(mColorIndicator);
+            mIndicatorRect.left = mIndicatorX;
+            mIndicatorRect.top = mIndicatorTopY;
+            mIndicatorRect.right = mIndicatorX + mIndicatorWidth;
+            mIndicatorRect.bottom = mIndicatorBottomY;
+            canvas.drawRect(mIndicatorRect, mIndicatorPaint);
+        }
+    }
+
+    public interface OnTabSelectedListener {
+        void onTabSelected(TabView tab, int position);
+
+        void onTabReselected(TabView tab, int position);
+    }
+}
