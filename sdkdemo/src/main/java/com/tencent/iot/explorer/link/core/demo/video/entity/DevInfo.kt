4a0ae0de478040e38676c0f8b56b92a3bfd07732
heyawei <379135467@qq.com>
2021-05-26 09:39:05 +0800

    仅支持音视频和事件展示（首页设备网格列表，承载页）
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192864412661
    
    Change-Id: I8f75df31b9ee5130f5dfcb04a1bb2e5c3c9cdc12

---
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/video/entity/DevInfo.kt
@@ -0,0 +1,12 @@
+package com.tencent.iot.explorer.link.core.demo.video.entity
+
+class DevInfo {
+    var deviceName = ""
+    var Status = 0
+    var DevicePsk = ""
+    var CreateTime = 0L
+    var FirstOnlineTime = 0L
+    var LoginTime = 0L
+    var LogLevel = 0
+    var Version = ""
+}
