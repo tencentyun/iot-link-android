73302353a4670ce1ed293d641c259ca080bab669
heyawei <379135467@qq.com>
2021-05-27 10:44:42 +0800

    网格设备列表支持多屏选择，视频横竖屏切换（不包含视频播放）
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192864412661
    
    Change-Id: I6ddaea55f259f281174afd997c07b523d90db142

---
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/video/fragment/VideoCloudPlaybackFragment.kt
@@ -0,0 +1,25 @@
+package com.tencent.iot.explorer.link.core.demo.video.fragment
+
+import android.view.View
+import com.tencent.iot.explorer.link.core.demo.R
+import com.tencent.iot.explorer.link.core.demo.fragment.BaseFragment
+import kotlinx.android.synthetic.main.fragment_video_cloud_playback.*
+
+class VideoCloudPlaybackFragment : BaseFragment() {
+
+    override fun getContentView(): Int {
+        return R.layout.fragment_video_cloud_playback
+    }
+
+    override fun startHere(view: View) {
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    private fun setListener() {
+    }
+
+}
