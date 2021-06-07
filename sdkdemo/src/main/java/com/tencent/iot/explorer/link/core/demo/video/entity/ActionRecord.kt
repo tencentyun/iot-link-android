c91ba679697b4895893e9a994b2526e31cf34d06
heyawei <379135467@qq.com>
2021-06-07 10:15:11 +0800

    预览页面获取时间云列表（支持加载 https 图片），支持云存录像播放，支持时间云播放
    Change-Id: I532ef29b1ce9d159e4b8dbe874f1cbcb44b2d3ab

--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/video/entity/ActionRecord.kt
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/video/entity/ActionRecord.kt
@@ -2,6 +2,8 @@ package com.tencent.iot.explorer.link.core.demo.video.entity
 
 class ActionRecord {
     var time = ""
+    var startTime = 0L
+    var endTime = 0L
     var action = ""
     var snapshotUrl = ""
     var actionUrl = ""
