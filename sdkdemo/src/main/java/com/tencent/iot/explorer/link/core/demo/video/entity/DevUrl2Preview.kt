48869cf04ffc79122684c6e7c4f8cfcd07429da9
heyawei <379135467@qq.com>
2021-06-10 10:01:08 +0800

    支持多屏预览
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192864412661
    
    Change-Id: I341664175cb3965d1906824de93770dc06c5fcc2

--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/video/entity/DevUrl2Preview.kt
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/video/entity/DevUrl2Preview.kt
@@ -1,7 +1,12 @@
 package com.tencent.iot.explorer.link.core.demo.video.entity
 
+import android.view.TextureView
+import tv.danmaku.ijk.media.player.IjkMediaPlayer
+
 class DevUrl2Preview {
     var devName = ""
     var url = ""
     var Status = 0  // 0 是不在线
+    var surfaceTextureListener : TextureView.SurfaceTextureListener? = null
+    var player: IjkMediaPlayer? = null
 }
