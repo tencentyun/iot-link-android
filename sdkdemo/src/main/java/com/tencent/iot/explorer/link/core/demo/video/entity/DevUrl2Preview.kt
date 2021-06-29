c5a7db6aca4507e983f6fb255e6b1506f4c24db0
heyawei <379135467@qq.com>
2021-06-29 11:49:05 +0800

    【ID865842231】 nvr 摄像头播放（ipc/nvr多屏播放）
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192865842231?url_cache_key=4f0d71c80d98812e25e7163db84b4e73
    
    Change-Id: I0d380a197263279f87083a6da2384c475ca71a89

--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/video/entity/DevUrl2Preview.kt
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/core/demo/video/entity/DevUrl2Preview.kt
@@ -8,6 +8,8 @@ class DevUrl2Preview {
     var devName = ""
     var url = ""
     var Status = 0  // 0 是不在线
+    var channel = 0
+    var channel2DevName = ""
     var surfaceTextureListener : TextureView.SurfaceTextureListener? = null
     var player: IjkMediaPlayer? = null
     var lock: Object = Object()
