d93d19ce874a5cb5f06787836278eec26e70b67c
heyawei <379135467@qq.com>
2021-07-05 16:51:04 +0800

    调整根目录，区分子模块目录
    Change-Id: Id2e8dd898262faa1288bbd8bda37e6e529a32715

---
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/demo/core/entity/RespData.kt
@@ -0,0 +1,6 @@
+package com.tencent.iot.explorer.link.demo.core.entity
+
+class RespData {
+    var requestId = ""
+    var data = ""
+}
---
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/demo/core/holder/ControlPanelHolder.kt
@@ -0,0 +1,34 @@
+package com.tencent.iot.explorer.link.demo.core.holder
+
+import android.content.Context
+import android.view.ViewGroup
+import com.tencent.iot.explorer.link.core.auth.entity.ControlPanel
+import kotlinx.android.synthetic.main.item_control_panel.view.*
+
+class ControlPanelHolder : BaseHolder<ControlPanel> {
+
+    constructor(context: Context, root: ViewGroup, resLayout: Int) : super(context, root, resLayout)
+
+    override fun show(holder: BaseHolder<*>, position: Int) {
+        data.run {
+            itemView.tv_template_name.text = name
+            when (id) {
+                "tp", "room", "share" -> {
+                    itemView.tv_btn_type.text = ""
+                    itemView.tv_template_value.text = ""
+                }
+                "alias" -> {
+                    itemView.tv_btn_type.text = ""
+                    itemView.tv_template_value.text = value
+                }
+                else -> {
+//                    itemView.tv_btn_type.text = "按钮类型：$type，图标：$icon"
+                    itemView.tv_template_value.text = define?.getText(value) ?: "0"
+                }
+            }
+        }
+        itemView.setOnClickListener {
+            clickItem(this, itemView, position)
+        }
+    }
+}
