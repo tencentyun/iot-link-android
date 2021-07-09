184aa012315de12e611060a89d988dccff88f506
heyawei <379135467@qq.com>
2021-07-09 16:47:31 +0800

    sdkdemo 内部按照功能模块划分，根据生命周期自动结束协程
    Change-Id: I2963ee748a770257c480007a55654666b7ce401d

--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/demo/video/response/SignedUrlResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.tencent.iot.explorer.link.demo.video.response
-
-class SignedUrlResponse {
-    var requestId = ""
-    var signedVideoURL = ""
-}
---
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/demo/video/utils/ToastDialog.java
@@ -0,0 +1,58 @@
+package com.tencent.iot.explorer.link.demo.video.utils;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.tencent.iot.explorer.link.demo.R;
+
+
+public class ToastDialog extends IosCenterStyleDialog {
+
+    public enum Type {
+        SUCCESS, WARNING
+    }
+
+    private Type type;
+    private String content;
+    private long duration;
+    private Handler handler = new Handler(Looper.getMainLooper());
+    private int[] icons = {
+            R.mipmap.icon_toast_dialog_success,
+            R.mipmap.icon_toast_dialog_warning
+    };
+
+    /**
+     * @param context 上下文
+     * @param type 对话框类型，SUCCESS/WARNING
+     * @param content 对话框内容
+     * @param duration 对话框展示时长，单位ms
+     */
+    public ToastDialog(Context context, Type type, String content, long duration) {
+        super(context, R.layout.popup_toast_layout);
+        this.type = type;
+        this.content = content;
+        this.duration = duration;
+    }
+
+    @Override
+    public void initView() {
+        handler.postDelayed(() -> {
+            if (isShowing()) {
+                super.dismiss();
+            }
+        }, duration);
+
+        ImageView iv = view.findViewById(R.id.iv_status);
+        TextView tv = view.findViewById(R.id.tv_content);
+        tv.setText(content);
+        iv.setImageResource(icons[type.ordinal()]);
+    }
+
+    @Override
+    public void show() {
+        super.show();
+    }
+}
