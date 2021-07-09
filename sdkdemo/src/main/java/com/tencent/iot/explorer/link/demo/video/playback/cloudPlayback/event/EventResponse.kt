f79cdc353e4589b6cca14ad42379336a226d9262
heyawei <379135467@qq.com>
2021-07-09 16:47:31 +0800

    sdkdemo 内部按照功能模块划分，根据生命周期自动结束协程
    Change-Id: I2963ee748a770257c480007a55654666b7ce401d

---
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/demo/video/playback/cloudPlayback/event/EventResponse.kt
@@ -0,0 +1,7 @@
+package com.tencent.iot.explorer.link.demo.video.playback.cloudPlayback.event
+
+class EventResponse {
+    var requestId = ""
+    var total = 0
+    var events : MutableList<Event> = ArrayList()
+}
