41e5dbbc6ee2d727c9fc2bc0da23f5de62e93c02
archurtan <archurtan@tencent.com>
2021-07-14 17:55:56 +0800

    调整workflow -- 同一仓库支持打不同tag
    Change-Id: Ifa448ee1339093cf6e743c2363c1de7d303c0f24

--- .github/scripts/updateVersionCodeForSnapshot.sh
+++ .github/scripts/updateVersionCodeForSnapshot.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+video_tag=$LATEST_VIDEO_TAG
+link_tag=$LATEST_LINK_TAG
+
 # 最新tag，例如：v1.0.0
-latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-echo ">>>latest tag: $latest_tag"
+echo ">>>latest video tag: $video_tag"
+echo ">>>latest link tag: $link_tag"
 
 # 最新tag，例如：1.0.0
-vtag=${latest_tag#*v}
-echo ">>>latest tag(no v): $vtag"
+vtag=${video_tag#*v}
+ltag=${link_tag#*v}
+echo ">>>latest video tag(no v): $vtag"
+echo ">>>latest link tag(no v): $ltag"
 
 # v1.0.0-beta.1 --> v1.0.0
 vtag_no_beta=(${vtag//-/ })
 vtag_no_beta=${vtag_no_beta[0]}
+
+ltag_no_beta=(${ltag//-/ })
+ltag_no_beta=${ltag_no_beta[0]}
 
 # snapshot版本自增（最新tag+1）
 vtaglist=(${vtag_no_beta//./ })
@@ -19,11 +27,27 @@ secondtag=${vtaglist[1]}
 thirdtag=${vtaglist[2]}
 thirdtag=`expr $thirdtag + 1`
 
-new_version=$firsttag.$secondtag.$thirdtag
+video_new_version=$firsttag.$secondtag.$thirdtag
 
-echo ">>>new version: $new_version"
+echo ">>>video new version: $video_new_version"
 
-sed -i 's#def sdkVersion.*#def sdkVersion = \"'$new_version'\"#g' config.gradle
+
+ltaglist=(${ltag_no_beta//./ })
+firsttag=${ltaglist[0]}
+secondtag=${ltaglist[1]}
+thirdtag=${ltaglist[2]}
+thirdtag=`expr $thirdtag + 1`
+
+link_new_version=$firsttag.$secondtag.$thirdtag
+
+echo ">>>link new version: $link_new_version"
+
+if [ -z "$video_new_version" ]; then
+    video_new_version=$link_new_version
+fi
+
+sed -i 's#def sdkVersion.*#def sdkVersion = \"'$link_new_version'\"#g' config.gradle
+sed -i 's#def videoSdkVersion.*#def videoSdkVersion = \"'$video_new_version'\"#g' config.gradle
 
 echo ">>>over!!!"
 
