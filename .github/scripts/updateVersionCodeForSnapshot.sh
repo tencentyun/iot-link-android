e09326ca60e716c7b1a1739333d05857e299ebf6
archurtan <archurtan@tencent.com>
2021-07-19 11:54:52 +0800

    调整video tag格式并适配新tag的解析逻辑
    Change-Id: I1740caa63d82521eaeb3c5eb9e3a0235985afcb9

--- .github/scripts/updateVersionCodeForSnapshot.sh
+++ .github/scripts/updateVersionCodeForSnapshot.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
+# ---------------- VIDEO BEGIN ----------------
 video_tag=$LATEST_VIDEO_TAG
-link_tag=$LATEST_LINK_TAG
 
-# 最新tag，例如：v1.0.0
+# 最新tag，例如：video-v2.2.0-beta
 echo ">>>latest video tag: $video_tag"
-echo ">>>latest link tag: $link_tag"
 
-# 最新tag，例如：1.0.0
-vtag=${video_tag#*v}
-ltag=${link_tag#*v}
-echo ">>>latest video tag(no v): $vtag"
-echo ">>>latest link tag(no v): $ltag"
+# video-v2.2.0-beta --> v2.2.0
+vtag_no_beta=(${video_tag//-/ })
+vtag_no_beta=${vtag_no_beta[1]}
 
-# v1.0.0-beta.1 --> v1.0.0
-vtag_no_beta=(${vtag//-/ })
-vtag_no_beta=${vtag_no_beta[0]}
+# v2.2.0 --> 2.2.0
+vtag_no_beta=${vtag_no_beta#*v}
+echo ">>>latest video tag(no v): $vtag_no_beta"
 
-ltag_no_beta=(${ltag//-/ })
-ltag_no_beta=${ltag_no_beta[0]}
 
 # snapshot版本自增（最新tag+1）
 vtaglist=(${vtag_no_beta//./ })
@@ -28,9 +23,22 @@ thirdtag=${vtaglist[2]}
 thirdtag=`expr $thirdtag + 1`
 
 video_new_version=$firsttag.$secondtag.$thirdtag
-
 echo ">>>video new version: $video_new_version"
+# ---------------- VIDEO END ----------------
 
+
+# ---------------- LINK BEGIN ----------------
+link_tag=$LATEST_LINK_TAG
+
+# 最新tag，例如：v1.0.0
+echo ">>>latest link tag: $link_tag"
+
+# 最新tag，例如：1.0.0
+ltag=${link_tag#*v}
+echo ">>>latest link tag(no v): $ltag"
+
+ltag_no_beta=(${ltag//-/ })
+ltag_no_beta=${ltag_no_beta[0]}
 
 ltaglist=(${ltag_no_beta//./ })
 firsttag=${ltaglist[0]}
@@ -39,8 +47,8 @@ thirdtag=${ltaglist[2]}
 thirdtag=`expr $thirdtag + 1`
 
 link_new_version=$firsttag.$secondtag.$thirdtag
-
 echo ">>>link new version: $link_new_version"
+# ---------------- LINK END ----------------
 
 if [ -z "$video_new_version" ]; then
     video_new_version=$link_new_version
