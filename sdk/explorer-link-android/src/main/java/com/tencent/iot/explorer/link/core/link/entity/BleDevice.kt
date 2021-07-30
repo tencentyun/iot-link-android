d659bfd46ba0486d0f11fc97cf9be5ccaf0fe0a2
heyawei <379135467@qq.com>
2021-07-30 11:54:58 +0800

    对接llsync标准蓝牙辅助配网协议
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192863935819#
    
    Change-Id: Id5e0894279cada756afb446076c07cf2bb8fced7

---
+++ sdk/explorer-link-android/src/main/java/com/tencent/iot/explorer/link/core/link/entity/BleDevice.kt
@@ -0,0 +1,10 @@
+package com.tencent.iot.explorer.link.core.link.entity
+
+import android.bluetooth.BluetoothDevice
+
+class BleDevice {
+    var url = ""
+    var productName = ""
+    var devName = ""
+    var blueDev : BluetoothDevice? = null
+}
