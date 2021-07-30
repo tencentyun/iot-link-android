0055b84ce454edfa370697a5eca36810266f2b2f
heyawei <379135467@qq.com>
2021-07-30 11:54:58 +0800

    对接llsync标准蓝牙辅助配网协议
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192863935819#
    
    Change-Id: Id5e0894279cada756afb446076c07cf2bb8fced7

---
+++ sdk/explorer-link-android/src/main/java/com/tencent/iot/explorer/link/core/link/entity/BleDeviceInfo.kt
@@ -0,0 +1,8 @@
+package com.tencent.iot.explorer.link.core.link.entity
+
+class BleDeviceInfo {
+    var llsyncVersion = "" // 协议版本号
+    var firmwareVersion = "" // 固件版本号
+    var mtuFlag = 0  // 是否设置 mtu
+    var mtuSize = 0  // mtu 大小
+}
