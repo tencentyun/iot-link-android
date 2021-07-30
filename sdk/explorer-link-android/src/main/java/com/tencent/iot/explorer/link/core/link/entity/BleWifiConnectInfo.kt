5813d2681b99b33fb793a6f27c9050f1e1e3c312
heyawei <379135467@qq.com>
2021-07-30 11:54:58 +0800

    对接llsync标准蓝牙辅助配网协议
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192863935819#
    
    Change-Id: Id5e0894279cada756afb446076c07cf2bb8fced7

---
+++ sdk/explorer-link-android/src/main/java/com/tencent/iot/explorer/link/core/link/entity/BleWifiConnectInfo.kt
@@ -0,0 +1,30 @@
+package com.tencent.iot.explorer.link.core.link.entity
+
+import android.util.Log
+import com.alibaba.fastjson.JSON
+
+class BleWifiConnectInfo {
+    var wifiMode: BleDeviceWifiMode = BleDeviceWifiMode.NULL
+    var connected: Boolean = false
+    var softAp = false
+    var ssid = ""
+    var ssidLen = 0
+
+    companion object {
+        fun byteArr2BleWifiConnectInfo(byteArray: ByteArray): BleWifiConnectInfo {
+            var ret = BleWifiConnectInfo()
+            if (byteArray.isEmpty()) return ret
+            if (byteArray.get(0) != 0xE2.toByte()) return ret
+
+            ret.wifiMode = BleDeviceWifiMode.valueOf(byteArray[3].toInt().toByte())
+            ret.connected = byteArray[4].toInt() == 0
+            ret.ssidLen = byteArray[6].toInt()
+            val ssidByteArr = ByteArray(ret.ssidLen)
+            System.arraycopy(byteArray, 7, ssidByteArr, 0, ret.ssidLen)
+            ret.ssid = String(ssidByteArr)
+
+            Log.e("XXX", "BleWifiConnectInfo ${JSON.toJSONString(ret)}")
+            return ret
+        }
+    }
+}
