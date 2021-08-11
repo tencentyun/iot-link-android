df83e3ebb9e38c8f6413e7a1a61c140d91cd43e4
archurtan <archurtan@tencent.com>
2021-08-11 11:01:41 +0800

    SDKDemo Release版的版本号使用tag
    Change-Id: I92f8c18efa8190876b6d9925cd1433278115de0e

--- .github/scripts/updateVideoSDKVersion.sh
+++ .github/scripts/updateVideoSDKVersion.sh
@@ -11,6 +11,7 @@ version=${version#*v}
 
 # replace videoSdkVersion with version
 sed -i 's#def videoSdkVersion.*#def videoSdkVersion = \"'$version'\"#g' config.gradle
+sed -i 's#def versionSDKDemoName.*#def versionSDKDemoName = \"'$version'\"#g' config.gradle
 
 # remove '-SNAPSHOT'
 sed -i '60,$s#-SNAPSHOT##g' sdk/video-link-android/build.gradle
