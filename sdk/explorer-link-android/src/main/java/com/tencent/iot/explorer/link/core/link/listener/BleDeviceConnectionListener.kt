93dd39bc0c875c373f5e0fd7ccd51060799b8106
heyawei <379135467@qq.com>
2021-08-13 17:56:33 +0800

    开发llsync标准蓝牙辅助配网协议对应的页面
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192863935819#
    
    Change-Id: I70291632f18845c9ebf99fb05e55fcac55357b32

--- sdk/explorer-link-android/src/main/java/com/tencent/iot/explorer/link/core/link/listener/BleDeviceConnectionListener.kt
+++ sdk/explorer-link-android/src/main/java/com/tencent/iot/explorer/link/core/link/listener/BleDeviceConnectionListener.kt
@@ -1,11 +1,13 @@
 package com.tencent.iot.explorer.link.core.link.listener
 
+import com.tencent.iot.explorer.link.core.link.entity.BleDevice
 import com.tencent.iot.explorer.link.core.link.entity.BleDeviceInfo
 import com.tencent.iot.explorer.link.core.link.entity.BleWifiConnectInfo
 import com.tencent.iot.explorer.link.core.link.exception.TCLinkException
 
 interface BleDeviceConnectionListener {
     fun onBleDeviceConnected()
+    fun onBleDeviceFounded(bleDevice: BleDevice)
     fun onBleDeviceDisconnected(exception : TCLinkException)
     fun onBleDeviceInfo(bleDeviceInfo: BleDeviceInfo)
     fun onBleSetWifiModeResult(success: Boolean)
