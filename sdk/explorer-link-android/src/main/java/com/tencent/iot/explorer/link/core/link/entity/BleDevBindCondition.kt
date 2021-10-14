e7859420259d2433776dfd9375170397fcc17c04
heyawei <379135467@qq.com>
2021-10-14 20:24:27 +0800

    纯蓝牙 LLSync APP 需求
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192863935819
    
    Change-Id: I8ed41e7d2ac6b64c3de0aa43f54ae658903a682f

---
+++ sdk/explorer-link-android/src/main/java/com/tencent/iot/explorer/link/core/link/entity/BleDevBindCondition.kt
@@ -0,0 +1,35 @@
+package com.tencent.iot.explorer.link.core.link.entity
+
+import android.util.Log
+import com.tencent.iot.explorer.link.core.link.service.BleConfigService
+
+open class BleDevBindCondition {
+    var signInfo = ""
+    var deviceName = ""
+    var data: ByteArray? = null
+        set(value) {
+            field = value
+            field?.let {
+//                if (it[0] != 0x05.toByte() && it[0] != 0x06.toByte()) return@let
+                Log.e("XXX", "it.value " + BleConfigService.bytesToHex(it))
+                val signInfoByteArr = ByteArray(20)
+                System.arraycopy(it, 3, signInfoByteArr, 0, 20)
+                BleConfigService.bytesToHex(signInfoByteArr)?.let { signStr ->
+                    signInfo = signStr.toLowerCase()
+                }
+
+                val devBytesLen = it.size - 23
+                val devNameByteArr = ByteArray(devBytesLen)
+                System.arraycopy(it, 23, devNameByteArr, 0, devBytesLen)
+                deviceName = String(devNameByteArr)
+            }
+        }
+
+    companion object {
+        fun data2BleDevBindCondition(data: ByteArray) : BleDevBindCondition{
+            var bleDevBindCondition = BleDevBindCondition()
+            bleDevBindCondition.data = data
+            return bleDevBindCondition
+        }
+    }
+}
