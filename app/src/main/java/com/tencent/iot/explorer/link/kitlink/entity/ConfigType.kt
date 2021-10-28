5088c9d1df346aa0d00bb0312b5d7d4b7e39e0e6
heyawei <379135467@qq.com>
2021-10-28 10:41:50 +0800

    纯蓝牙 LLSync APP 需求
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192863935819
    
    Change-Id: I8ed41e7d2ac6b64c3de0aa43f54ae658903a682f

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/entity/ConfigType.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/entity/ConfigType.kt
@@ -3,5 +3,6 @@ package com.tencent.iot.explorer.link.kitlink.entity
 enum class ConfigType (val id: Int) {
     SoftAp(1),
     SmartConfig(0),
-    BleConfig(2);
+    BleConfig(2),
+    BleBindConfig(3);
 }
