ba34ec214c947e3983ab02bcfbe4253cad50c8da
heyawei <379135467@qq.com>
2021-12-17 18:26:54 +0800

    局域网探测（UI 入口）
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192869350657
    
    Change-Id: Iff738760c1e451fd04aec8f13404ed2dbdf66abe

--- sdk/video-link-android/src/main/java/com/tencent/iot/video/link/consts/VideoConst.kt
+++ sdk/video-link-android/src/main/java/com/tencent/iot/video/link/consts/VideoConst.kt
@@ -3,6 +3,7 @@ package com.tencent.iot.video.link.consts
 object VideoConst {
     /***********video**********/
     const val VIDEO_CONFIG = "VideoConfig"
+    const val VIDEO_WLAN_CONFIG = "VideoWlanConfig"
     const val VIDEO_SECRET_ID = "VideoSecretId"
     const val VIDEO_SECRET_KEY = "VideoSecretKey"
     const val VIDEO_PRODUCT_ID = "VideoProductId"
