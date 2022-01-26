00897f85e9af7fe75b43baa9b798d95397c112fb
heyawei <379135467@qq.com>
2022-01-26 17:12:20 +0800

    sdk demo 支持视频采集和显示
    Change-Id: I46734b5299663e55c5e308ef0d9b6907975b5e9c

---
+++ sdk/video-link-android/src/main/java/com/tencent/iot/video/link/recorder/opengles/render/base/OnSurfaceTextureListener.java
@@ -0,0 +1,8 @@
+package com.tencent.iot.video.link.recorder.opengles.render.base;
+
+import android.graphics.SurfaceTexture;
+
+public interface OnSurfaceTextureListener {
+
+    void onSurfaceTexture(SurfaceTexture surfaceTexture); // SurfaceTexture 回调
+}
