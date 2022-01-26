e7ed0a1561bd0733d81ab0870972022eac97e96a
heyawei <379135467@qq.com>
2022-01-26 17:12:20 +0800

    sdk demo 支持视频采集和显示
    Change-Id: I46734b5299663e55c5e308ef0d9b6907975b5e9c

--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/demo/video/VideoOptionsActivity.kt
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/demo/video/VideoOptionsActivity.kt
@@ -3,6 +3,7 @@ package com.tencent.iot.explorer.link.demo.video
 import com.tencent.iot.explorer.link.demo.R
 import com.tencent.iot.explorer.link.demo.BaseActivity
 import com.tencent.iot.explorer.link.demo.VideoBaseActivity
+import com.tencent.iot.explorer.link.demo.video.preview.RecordVideoActivity
 import kotlinx.android.synthetic.main.activity_video_options.*
 
 class VideoOptionsActivity : VideoBaseActivity() {
@@ -15,6 +16,7 @@ class VideoOptionsActivity : VideoBaseActivity() {
     override fun setListener() {
         btn_video.setOnClickListener { jumpActivity(VideoInputAuthorizeActivity::class.java) }
         btn_video_wlan.setOnClickListener { jumpActivity(VideoWlanDetectActivity::class.java) }
+        btn_video_collect.setOnClickListener { jumpActivity(RecordVideoActivity::class.java) }
     }
 
 }
