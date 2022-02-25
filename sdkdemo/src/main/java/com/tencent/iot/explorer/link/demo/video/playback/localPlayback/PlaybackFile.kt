08ef00b85278639c34557e08d4894246086e8e59
archurtan <archurtan@tencent.com>
2022-02-25 17:20:17 +0800

    【讯美项目支持】支持P2P本地录像下载
    1、录像列表页面开发
    2、下载功能
    
    Change-Id: Ie88a3d730db33da6ce29bf7a0f2a4b12a5e9500c

---
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/demo/video/playback/localPlayback/PlaybackFile.kt
@@ -0,0 +1,10 @@
+package com.tencent.iot.explorer.link.demo.video.playback.localPlayback
+
+class PlaybackFile {
+    var start_time = 0L
+    var end_time = 0L
+    var file_type = 0
+    var file_name = ""
+    var file_size = 0L
+    var extra_info = ""
+}
