51213f7b65df25890ede28dc8bbfd166d749c2dc
archurtan <archurtan@tencent.com>
2022-05-20 16:56:23 +0800

    同步video-link-android sdk.
    Change-Id: I7e3d06be34f79f32a43d16a8b0f1e6c308298da9

--- sdk/video-link-android/src/main/java/com/tencent/iot/video/link/entity/DeviceStatus.java
+++ sdk/video-link-android/src/main/java/com/tencent/iot/video/link/entity/DeviceStatus.java
@@ -1,12 +1,12 @@
-package com.tencent.iot.video.link.entity
+package com.tencent.iot.video.link.entity;
 
-class DeviceStatus {
+public class DeviceStatus {
     // 0   接收请求
     // 1   拒绝请求
     // 404 error request message
     // 405 connect number too many
     // 406 current command don't support
     // 407 device process error
-    var status = 0
-    var appConnectNum = 0
+    public int status = 0;
+    public int appConnectNum = 0;
 }
