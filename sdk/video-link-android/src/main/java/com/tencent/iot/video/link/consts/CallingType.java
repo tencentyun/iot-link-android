12f4782ec2f505da51d959c99c75a903709daf78
archurtan <archurtan@tencent.com>
2022-05-20 16:56:23 +0800

    同步video-link-android sdk.
    Change-Id: I7e3d06be34f79f32a43d16a8b0f1e6c308298da9

---
+++ sdk/video-link-android/src/main/java/com/tencent/iot/video/link/consts/CallingType.java
@@ -0,0 +1,7 @@
+package com.tencent.iot.video.link.consts;
+
+public abstract class CallingType {
+    public static final int TYPE_UNKNOWN = 0;
+    public static final int TYPE_AUDIO_CALL = 1;
+    public static final int TYPE_VIDEO_CALL = 2;
+}
