d3dbb7499053b3e2104a7b96fede5d48978fcbcc
whalensun(孙伟) <532125505@qq.com>
2023-02-08 09:19:54 +0800

    添加判断播放器缓存大于5s时间继续了20s，重启P2P服务。
    Change-Id: I49a9ccf4786d89664c77ceb3e9ce139fcf99a114

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/util/VideoUtils.java
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/util/VideoUtils.java
@@ -7,10 +7,18 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 public class VideoUtils {
     public static String VIDEO_RESUME = "videoResume";
+    public static String VIDEO_RESET = "videoReset";
 
     public static void sendVideoBroadcast(Context context, int value) {
         Intent intent = new Intent("android.intent.action.CART_BROADCAST");
         intent.putExtra(VIDEO_RESUME, value);
+        LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+        context.sendBroadcast(intent);
+    }
+
+    public static void sendNeedResetP2PBroadcast(Context context, int value) {
+        Intent intent = new Intent("android.intent.action.CART_BROADCAST");
+        intent.putExtra(VIDEO_RESET, value);
         LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
         context.sendBroadcast(intent);
     }
