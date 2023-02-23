91a9df4c4a349d7aa3c1c7ae40b83b9dbad26ccb
whalensun(孙伟) <532125505@qq.com>
2023-02-23 20:18:45 +0800

    凯迪仕变声添加mode接口
    Change-Id: I795691a445bcbfecf1cc45c7b17004f4fa36619f

---
+++ sdk/video-link-android/src/main/java/com/tencent/iot/video/link/util/audio/VoiceChangerMode.kt
@@ -0,0 +1,7 @@
+package com.tencent.iot.video.link.util.audio
+
+enum class VoiceChangerMode (val value: Int) {
+    VOICE_CHANGER_MODE_NONE(0),
+    VOICE_CHANGER_MODE_WOMAN(2) ,
+    VOICE_CHANGER_MODE_MAN(3) ;
+}
