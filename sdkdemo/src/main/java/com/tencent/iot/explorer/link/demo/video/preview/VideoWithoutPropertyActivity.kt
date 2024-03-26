371d8a003d932777cdb456b10dd06584888c308b
whalensun(孙伟) <532125505@qq.com>
2024-03-26 15:31:30 +0800

    增加校验逻辑回调
    Change-Id: I1af040185a1f276635e44e1c04b0f79d7214a6b3

--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/demo/video/preview/VideoWithoutPropertyActivity.kt
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/demo/video/preview/VideoWithoutPropertyActivity.kt
@@ -406,6 +406,8 @@ class VideoWithoutPropertyActivity : VideoBaseActivity(), EventView, TextureView
             if (event == 1004) {
                 Log.e(tag, "====event === 1004")
             }
+        } else if (event == 1010) {
+            Log.e(tag, "====event === 1010, 校验失败，info撞库防止串流： $msg")
         }
     }
 
