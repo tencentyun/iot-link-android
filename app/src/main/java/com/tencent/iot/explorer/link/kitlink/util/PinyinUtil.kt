3654bc99898b77920fa71535b299a2d9d188a8f1
neilyhe <228429803@qq.com>
2025-07-14 17:49:52 +0800

    更换PinyinHelper库

--- app/src/main/java/com/tencent/iot/explorer/link/kitlink/util/PinyinUtil.kt
+++ app/src/main/java/com/tencent/iot/explorer/link/kitlink/util/PinyinUtil.kt
@@ -1,16 +1,25 @@
 package com.tencent.iot.explorer.link.kitlink.util
 
-import net.sourceforge.pinyin4j.PinyinHelper
+//import net.sourceforge.pinyin4jj.PinyinHelper
+import com.github.promeg.pinyinhelper.Pinyin
 
 object PinyinUtil {
 
     /**
      * 获取汉字首字母
      */
-    fun getFirstLetter(word: Char) : String? {
-        val letterArray = PinyinHelper.toHanyuPinyinStringArray(word)
-        return if (letterArray != null) {
-            letterArray[0][0].toString()
+//    fun getFirstLetter(word: Char) : String? {
+//        val letterArray = PinyinHelper.toHanyuPinyinStringArray(word)
+//        return if (letterArray != null) {
+//            letterArray[0][0].toString()
+//        } else {
+//            null
+//        }
+//    }
+
+    fun getFirstLetter(word: Char): String? {
+        return if (Pinyin.isChinese(word)) {
+            Pinyin.toPinyin(word)[0].toString() // TinyPinyin 返回大写字母
         } else {
             null
         }
