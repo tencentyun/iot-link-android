39c86b03a36c4246bf67eacd8a48bd631961dccd
neilyhe <228429803@qq.com>
2025-09-17 15:10:19 +0800

    发版脚本修改

--- .github/scripts/updateValuesForSign.sh
+++ .github/scripts/updateValuesForSign.sh
@@ -6,3 +6,6 @@ root_path=$(pwd)
 sed -i 's#MY_KEY_ID#'$key_id'#g' gradle.properties
 sed -i 's#MY_PASSWORD#'$password'#g' gradle.properties
 sed -i 's#MY_KEY_RING_FILE#'$root_path'/secring.gpg#g' gradle.properties
+
+sed -i 's#MY_MAVEN_USERNAME#'$IOT_SONATYPE_USERNAME'#g' gradle.properties
+sed -i 's#MY_MAVEN_PASSWORD#'$IOT_SONATYPE_PASSWORD'#g' gradle.properties
