918456f484240f58a11fb9f1774160d697f78009
neilyhe <228429803@qq.com>
2025-11-05 15:59:25 +0800

    拉取多设备实现

--- sdkdemo/src/main/java/com/tencent/iot/explorer/link/demo/video/VideoOptionsActivity.kt
+++ sdkdemo/src/main/java/com/tencent/iot/explorer/link/demo/video/VideoOptionsActivity.kt
@@ -1,6 +1,6 @@
 package com.tencent.iot.explorer.link.demo.video
 
-import com.tencent.iot.explorer.link.demo.R
+import android.app.AlertDialog
 import com.tencent.iot.explorer.link.demo.VideoBaseActivity
 import com.tencent.iot.explorer.link.demo.databinding.ActivityVideoOptionsBinding
 
@@ -13,8 +13,21 @@ class VideoOptionsActivity : VideoBaseActivity<ActivityVideoOptionsBinding>() {
         with(binding) {
             btnVideo.setOnClickListener { jumpActivity(VideoInputAuthorizeActivity::class.java) }
             btnVideoWlan.setOnClickListener { jumpActivity(VideoWlanDetectActivity::class.java) }
-            btnVideoLink.setOnClickListener { jumpActivity(VideoTestInputActivity::class.java) }
+            btnVideoLink.setOnClickListener { showConnectionTypeDialog() }
         }
     }
 
+    private fun showConnectionTypeDialog() {
+        val options = arrayOf("单设备直连", "多设备直连")
+        AlertDialog.Builder(this)
+            .setTitle("选择连接方式")
+            .setItems(options) { _, which ->
+                when (which) {
+                    0 -> jumpActivity(VideoTestInputActivity::class.java)
+                    1 -> jumpActivity(MultiVideoTestInputActivity::class.java)
+                }
+            }
+            .setNegativeButton("取消", null)
+            .show()
+    }
 }
